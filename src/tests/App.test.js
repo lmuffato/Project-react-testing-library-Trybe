8e6a98b92145d78b49c81c51896b670fd8ce62b4
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-05-25 15:00:20 -0300

    Initial commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,25 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Requisito 1 - Teste o componente <App.js />',
+  () => {
+    test('renders a reading with the text `Pokédex`', () => {
+      const { getByText } = render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+      const heading = getByText(/Pokédex/i);
+      expect(heading).toBeInTheDocument();
+    });
+
+    test('shows the Pokédex when the route is `/`', () => {
+      const { getByText } = render(
+        <MemoryRouter initialEntries={ ['/'] }>
+          <App />
+        </MemoryRouter>,
+      );
+
+      expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    });
+  });
