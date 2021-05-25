425c66ad50525709ef9e88ea8c5950363ba0ef6f
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-05-25 14:56:33 -0400

    Favorites component initial test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testes do componente Favorite Pokemons', () => {
+  test('testes gerais da página de favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavoritesText = screen.getByText('No favorite pokemon found');
+    expect(noFavoritesText).toBeInTheDocument();
+  });
+});
