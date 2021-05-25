057eb49c558a396a5502940109b7898fe9a21d33
Felipe Muller <felipeavmuller@gmail.com>
2021-05-25 16:28:34 -0300

    Testa o click no home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,32 +1,44 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Requisito 1', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('renders home, about and favorite pokémons links', () => {
-  const { getByRole } = renderWithRouter(<App />);
+  it('renders home, about and favorite pokémons links', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const homeLink = getByRole('link', {
+      name: /home/i,
+    });
+    const aboutLink = getByRole('link', {
+      name: /about/i,
+    });
+    const favoriteLink = getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
+  });
 
-  const homeLink = getByRole('link', {
-    name: /home/i,
+  it('Link home works', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const homeLink = getByRole('link', {
+      name: /home/i,
+    });
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
-  const aboutLink = getByRole('link', {
-    name: /about/i,
-  });
-  const favoriteLink = getByRole('link', {
-    name: /favorite pokémons/i,
-  });
-  expect(homeLink).toBeInTheDocument();
-  expect(aboutLink).toBeInTheDocument();
-  expect(favoriteLink).toBeInTheDocument();
-});
+}); // Describe
