45da13072a2925c18f7af0d328c8221b84bca603
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-05-25 19:37:45 -0300

    requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('contains an H2 heading with exact text', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const head = getByRole('heading', {
+    name: /Page requested not found/i,
+    level: 2,
+  });
+  expect(head).toBeInTheDocument();
+});
+
+test('contains the exact img', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+  const img = getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  expect(img).toBeInTheDocument();
+  expect(img).toHaveAttribute('src',
+    'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
