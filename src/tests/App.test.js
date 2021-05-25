9abfc7813a88311fe23531e6b9285a36b7cd17f9
Orlando Flores <orlando_flores@id.uff.br>
2021-05-25 15:20:58 -0300

    feat: requisito 1 e funcao helper renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Renders routes perfectly', () => {
+  test('renders /Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const homeBtn = getByText(/home/i);
+    userEvent.click(homeBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('renders /About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const aboutBtn = getByText(/about/i);
+    userEvent.click(aboutBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  test('renders /favorites', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const favoritesBtn = getByRole('link', {
+      name: /favorite pokémons/i,
+    });
+    userEvent.click(favoritesBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });
