2f52c582d196bba85ded6bc4d434c6368da19548
Lucas Pedroso <lepsk8@gmail.com>
2021-05-25 16:24:11 -0300

    test: :white_check_mark: About contains h2 with 'About Pokédex'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter.helper';
+import About from '../components/About';
+
+describe('Requisito 2', () => {
+  test('A página contém um heading h2 com o texto `About Pokédex`', () => {
+    renderWithRouter(<About />);
+
+    const aboutPokedex = screen.getByRole('heading');
+    expect(aboutPokedex).toBeInTheDocument();
+    expect(aboutPokedex).toHaveTextContent(/about pokédex/i);
+  });
+});
