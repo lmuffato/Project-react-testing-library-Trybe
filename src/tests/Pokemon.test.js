1d3fab47d92d9cccf55b99c3740715a167d40879
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-05-25 20:44:50 -0300

    Req 6, this one implementation is decent

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,73 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { Pokemon } from '../components';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Requisito 6 - Teste o componente <Pokemon.js />',
+  () => {
+    it('Teste se é renderizado um card com as informações de determinado pokémon',
+      () => {
+        const alakazam = pokemons[4];
+        const { getByTestId, getByRole } = renderWithRouter(
+          <Pokemon
+            pokemon={ alakazam }
+            isFavorite={ false }
+          />,
+        );
+        const name = getByTestId('pokemon-name');
+        expect(name).toHaveTextContent('Alakazam');
+        const type = getByTestId('pokemon-type');
+        expect(type).toHaveTextContent('Psychic');
+        const weight = getByTestId('pokemon-weight');
+        expect(weight).toHaveTextContent(/48\.0 kg/);
+        const sprite = getByRole('img', { name: /sprite/ });
+        expect(sprite).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png');
+      });
+    it(`Teste se o card do Pokémon indicado na Pokédex 
+    contém um link de navegação para exibir detalhes deste Pokémon.
+    O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido`,
+    () => {
+      const alakazam = pokemons[4];
+      const { getByRole } = renderWithRouter(
+        <Pokemon
+          pokemon={ alakazam }
+          isFavorite={ false }
+        />,
+      );
+      const moreDetails = getByRole('link', { name: 'More details' });
+      expect(moreDetails).toHaveAttribute('href', '/pokemons/65');
+    });
+    it(`Teste se ao clicar no link de navegação do Pokémon, 
+    é feito o redirecionamento da aplicação para a página de detalhes de Pokémon`,
+    () => {
+
+    });
+    it(`Teste também se a URL exibida no navegador muda para /pokemon/<id>,
+    onde <id> é o id do Pokémon cujos detalhes se deseja ver`,
+    () => {
+      const alakazam = pokemons[4];
+      const { getByRole, history } = renderWithRouter(
+        <Pokemon
+          pokemon={ alakazam }
+          isFavorite={ false }
+        />,
+      );
+      const moreDetails = getByRole('link', { name: 'More details' });
+      userEvent.click(moreDetails);
+      expect(history.location.pathname).toBe('/pokemons/65');
+    });
+    it('Teste se existe um ícone de estrela nos Pokémons favoritados',
+      () => {
+        const alakazam = pokemons[4];
+        const { getByRole } = renderWithRouter(
+          <Pokemon
+            pokemon={ alakazam }
+            isFavorite
+          />,
+        );
+        const star = getByRole('img', { name: /marked as favorite/ });
+        expect(star).toBeInTheDocument();
+        expect(star).toHaveAttribute('src', '/star-icon.svg');
+      });
+  });
