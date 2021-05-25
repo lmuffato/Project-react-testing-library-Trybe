5e3673cdeba86a6f7a4a5f0b692b640ebe6919f1
Lucas Pedroso <lepsk8@gmail.com>
2021-05-25 17:56:09 -0300

    test: :white_check_mark: Page NotFound have a h2 with text Page requested not found 😭

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter.helper';
+import { NotFound } from '../components';
+
+describe('Requisito 4', () => {
+  test('A página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const h2NotFound = screen.getByRole('heading');
+    expect(h2NotFound).toBeInTheDocument();
+    expect(h2NotFound).toHaveTextContent(/Page requested not found 😭/i);
+  });
+
+  test('A página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText(/requested was not found/i);
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
