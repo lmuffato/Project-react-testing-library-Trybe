da34fd3e4950ebbbc20696b9cc4120cd26f8b725
Rafael Cardoso <rafael.vimur@gmail.com>
2021-05-25 16:05:28 -0300

    adiciona requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -15,10 +15,40 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Testando se o primeiro link possui o texto Home', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Home')).toBeInTheDocument();
+});
+
+test('Testando se o segundo link possui o texto About', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('About')).toBeInTheDocument();
+});
+
+test('Testando se o terceiro link possui o texto Favorite Pokémons', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
 });
