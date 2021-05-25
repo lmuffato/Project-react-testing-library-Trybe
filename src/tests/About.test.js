b42c4dad92e9a96e736ee1a2dcb16b94cba58a3c
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-05-25 16:42:50 -0300

    :rocket: Requisito 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { About } from '../components';
+
+describe('tests for the <About> component', () => {
+  test('renders a reading with the text `About Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <About />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/About Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('renders the image correctly', () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <About />
+      </MemoryRouter>,
+    );
+    const img = getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,52 +3,54 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Tests for the <App> component', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
-});
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 
-test('shows the link with the value "Favorite Pokémons"', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  const linkFavorite = getByText(/Favorite Pokémons/i);
-  expect(linkFavorite).toBeInTheDocument();
-});
+  test('shows the link with the value "Favorite Pokémons"', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkFavorite = getByText(/Favorite Pokémons/i);
+    expect(linkFavorite).toBeInTheDocument();
+  });
 
-test('shows the link with the value "Home"', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  const linkHome = getByText(/Home/i);
-  expect(linkHome).toBeInTheDocument();
-});
+  test('shows the link with the value "Home"', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+  });
 
-test('shows the link with the value "About"', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  const linkAbout = getByText(/About/i);
-  expect(linkAbout).toBeInTheDocument();
+  test('shows the link with the value "About"', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkAbout = getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+  });
 });
