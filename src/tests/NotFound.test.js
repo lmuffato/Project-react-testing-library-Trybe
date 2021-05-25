21315ce353ec981a26317bff12ba7e4eddd60607
Jodiel Briesemeister <jodielll29@gmail.com>
2021-05-25 17:38:42 -0300

    feat: requirement 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+it('contains h2 with the following text "Page requested not found"', () => {
+  const { getByRole } = render(<NotFound />);
+
+  const h2 = getByRole('heading', {
+    name: /Page requested not found/i,
+    level: 2,
+  });
+
+  expect(h2).toBeInTheDocument();
+});
+
+it('contains a img', () => {
+  const { getByAltText } = render(<NotFound />);
+
+  const img = getByAltText('Pikachu crying because the page requested was not found');
+  const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  expect(img.src).toContain(url);
+});
