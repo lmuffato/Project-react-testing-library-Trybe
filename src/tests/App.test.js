5fc17a6c7d09088c6329aaa155e884234fbb4075
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-25 16:47:19 -0300

    fix: lint apos criar testes sobre links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
