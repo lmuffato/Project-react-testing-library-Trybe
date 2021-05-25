1554418873045b8351278dc52f40f63d004b826d
Sergio Martins <sergiomos2010@gmail.com>
2021-05-25 14:57:40 -0300

    tenting "/" route

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('tenting App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('links are in the home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const firstLink = getByText(/home/i);
+    const secondLink = getByText(/about/i);
+    const thirdLink = getByText(/favorite pokémons/i);
+
+    expect(firstLink).toBeInTheDocument();
+    expect(secondLink).toBeInTheDocument();
+    expect(thirdLink).toBeInTheDocument();
+  });
+
+  test('Home renders app', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const link = getByText(/home/i);
+    userEvent.click(link);
+    const pokedex = getByText(/encountered pokémons/i);
+    expect(pokedex).toBeInTheDocument();
+  });
 });
