22d96fdc63b155f144b1723b466ddb59a1ee4043
Lucas Pedroso <lepsk8@gmail.com>
2021-05-25 16:25:21 -0300

    test: :white_check_mark: Contains 2 <p> about pokédex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -12,4 +12,13 @@ describe('Requisito 2', () => {
     expect(aboutPokedex).toBeInTheDocument();
     expect(aboutPokedex).toHaveTextContent(/about pokédex/i);
   });
+
+  test('A página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const tagP = screen.getAllByText(/pokémons/i);
+    expect(tagP).toHaveLength(2);
+    expect(tagP[0]).toHaveTextContent(/digital encyclopedia/i);
+    expect(tagP[1]).toHaveTextContent(/see more details for each/i);
+  });
 });
