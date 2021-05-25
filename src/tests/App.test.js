b07d29524d19ce5e0a3c424c693f3201e5ba607f
Patrick Morais <ppternunes@gmail.com>
2021-05-25 18:05:44 -0300

    fixes e requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -47,4 +49,48 @@ test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
     </MemoryRouter>,
   );
   expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+});
+
+test('aplicação é redirecionada para URL / ao clicar no link Home', () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  fireEvent.click(getByText(/Home/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('aplicação é redirecionada para URL /about ao clicar no link About', () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  fireEvent.click(getByText(/About/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('redireciona para URL /favorites ao clicar no link Favorite Pokémons ', () => {
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+  fireEvent.click(getByText(/Favorite Pokémons/i));
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('pathname aleatório', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  const route = '/blablublé';
+  history.push(route);
+  expect(getByText(/Page requested not found/i)).toBeInTheDocument();
 });
