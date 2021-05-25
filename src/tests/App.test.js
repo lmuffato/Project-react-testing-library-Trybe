91ad6e0464bbe070dc228eb2e4828349f8fd3b47
madruga665 <madruga665@hotmail.com>
2021-05-25 18:03:26 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './RenderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +22,33 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+// Meus teste começam aqui
+
+describe('Testa se contém um conjunto fixo de links de navegação.', () => {
+  test('O primeiro link deve possuir o texto "Home"', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', {
+      name: 'Home',
+    });
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('O segundo link deve possuir o texto "About"', () => {
+    renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', {
+      name: 'About',
+    });
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('O terceiro link deve possuir o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const favoritePokemonLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favoritePokemonLink).toBeInTheDocument();
+  });
 });
