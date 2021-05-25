bb87381b7fd54d5d96f849f94b70c88a3268d08a
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-25 15:30:26 -0300

    feat: req1 - testando links da rota '/'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +13,24 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test(`Teste se a aplicação é redirecionada para a página inicial, na URL "/" 
+ao clicar no link "Home" da barra de navegação.`, () => {
+
+  const { getByRole } = renderWithRouter(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkHome = getByRole('link', {
+    name: /home/i,
+  });
+  userEvent.click(linkHome);
+
+  const linkAbout = getByRole('link', {
+    name: /about/i,
+  });
+  expect(linkAbout).toBeInTheDocument();
 });
