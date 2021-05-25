d4f277c1d22a4e01dc8502b02921370f5868581b
w-cypriano <profwellington17@gmail.com>
2021-05-25 20:08:51 -0300

    requisito2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from "../renderWithRouter";
+import About from '../components/About'
+
+describe('este o componente <About.js />', () => {
+  test('Se a página contém as informações sobre a Pokédex.', () => {
+    const { getByText, history } = renderWithRouter(<About />)
+    history.push('/about')
+    const about = getByText(/This application/i);
+    expect(about).toBeInTheDocument();
+  });
+  test('Se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole, history } = renderWithRouter(<About />);
+    const titlePage = getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(titlePage).toBeInTheDocument();
+  });
+  test('Se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<About />);
+    history.push('/about');
+    const p = document.getElementsByTagName("p");
+    expect(p).toHaveLength(2);
+  });
+  test('Se a página contém a seguinte imagem de uma Pokédex:', () => {
+    const { getByRole, history } = renderWithRouter(<About />);
+    history.push('/about');
+    const src = "https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png";
+    const searchImg = getByRole('img');
+    expect(searchImg.src).toBe(src);
+  });
+});
+
