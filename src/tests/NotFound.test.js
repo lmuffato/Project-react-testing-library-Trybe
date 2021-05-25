a570a159dd038686fcdd46150ae842cb75c5b8ac
defauth98 <neto.daniribeiro@gmail.com>
2021-05-25 18:26:17 -0300

    testando a imagem da pagina not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
+const altImage = 'Pikachu crying because the page requested was not found';
+const srcImage = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
 describe('not found page', () => {
   it('render a not found page', () => {
-    const { getByText, history } = renderWithRouter(<App />)
+    const { getByText, history } = renderWithRouter(<App />);
 
     history.push('/not-found');
 
-    const heading = getByText(/Page requested not found/g);
+    const heading = getByText(/Page requested not found/i);
     expect(heading).toBeInTheDocument();
-  })
-})
+  });
+
+  it('render a image', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+
+    history.push('/not-found');
+
+    const image = getByAltText(altImage);
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(srcImage);
+  });
+});
