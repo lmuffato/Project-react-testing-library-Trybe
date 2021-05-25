30523f3450de06bce2c039cecdc6db7ce5ea2a96
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-25 17:53:45 -0300

    feat: Verifica se o texto home e renderizado na pagina.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { getByRole } from '@testing-library/dom';
+import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+  const home = getByText(/home/i);
+  expect(home).toBeInTheDocument();
 });
