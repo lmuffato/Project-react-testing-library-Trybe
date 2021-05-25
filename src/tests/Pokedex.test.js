3bc8e3021b15ead5d74a820c2e1c930400721ff9
Felipe Muller <felipeavmuller@gmail.com>
2021-05-25 19:01:59 -0300

    Alguns testes da pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Pokedex test', () => {
+  it('encountered pokemons text', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const text = getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+    expect(text).toBeInTheDocument();
+  });
+
+  it('next pokemon button is working correctly', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const nextBtn = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    userEvent.click(nextBtn);
+    expect(getByText('Charmander')).toBeInTheDocument();
+    for (let index = 0; index < (pokemons.length - 1); index += 1) {
+      userEvent.click(nextBtn);
+    }
+    expect(getByText('Pikachu')).toBeInTheDocument();
+  });
+
+  it('only one pokemon is showed', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    expect(getAllByTestId('pokemon-name').length).toBe(1);
+  });
+
+  it('pokemon type button', () => {
+    const { getByRole, getByText, getAllByTestId } = renderWithRouter(<App />);
+    const typeBtn = getAllByTestId('pokemon-type-button');
+    expect(typeBtn.length).toBe(7);
+    const poison = getByRole('button', {
+      name: 'Poison',
+    });
+    userEvent.click(poison);
+    const nextBtn = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(nextBtn.disabled).toBe(true);
+    expect(getByText('Ekans')).toBeInTheDocument();
+  });
+});
