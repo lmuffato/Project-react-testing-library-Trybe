0c3972980a6b12b962d5ce50054ac0bc04ff7c97
Rafael Cardoso <rafael.vimur@gmail.com>
2021-05-25 17:23:50 -0300

    adiciona requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { About } from '../components';
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <About />
+    </MemoryRouter>,
+  );
+
+  const image = getByRole('img');
+  expect(image).toHaveAttribute('src',
+    expect.stringMatching('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'));
+});
