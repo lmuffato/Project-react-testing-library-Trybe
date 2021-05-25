26f79c645676f409c445da9c443d24b83903bb6a
guidugaich <guidugaich.dev@gmail.com>
2021-05-25 17:02:30 -0300

    montando a estrutura para os testes do requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,6 @@
-test('', () => {});
+describe('2. Testando componente <About />', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {});
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {});
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {});
+  test('Teste se a página contém a imagem de uma Pokédex:', () => {});
+});
