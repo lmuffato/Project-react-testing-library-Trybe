527760f1baf494d77da75c6a655d9c11f9f42735
hheloisa <hhackenhaar@gmail.com>
2021-05-25 19:39:20 -0300

    Testa <About />, testa heading

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About', () => {
+  test('Testa se a página contém as infos sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+});
