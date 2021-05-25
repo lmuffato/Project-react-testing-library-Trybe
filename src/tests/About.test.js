f6833971aa5af504b13f9d6ae731b6897b2761c2
Ana Ventura <anaventura1811@gmail.com>
2021-05-25 14:48:33 -0300

    inclui partes 1 e 2 do teste do componente about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+test('page contains information about Pokédex', () => {
+  const { history } = renderWithRouter(<About />);
+
+  history.push('/about');
+
+  const paragraphs = screen.getAllByText(
+    (content, element) => element.tagName.toLowerCase() === 'p'
+    && content.includes('Pokémons'),
+  );
+  expect(paragraphs).toHaveLength(2);
+});
+
+test('page contains a h2 heading with text `About Pokédex`', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const aboutPage = getByRole('heading', {
+    name: 'About Pokédex',
+    level: 2,
+  });
+  expect(aboutPage).toBeInTheDocument();
+});
+
+test('', () => {
+  
+})
