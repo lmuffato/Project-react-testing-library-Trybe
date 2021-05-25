e6bc7e98ba4bfdf0c0dd2529b3e058eea490cdfc
Sergio Martins <sergiomos2010@gmail.com>
2021-05-25 19:23:11 -0300

    testing: show all favorites pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
-import pokemons from '../data';
+import data from '../data';
 
 describe('Favorite Pokemons', () => {
   test('No Favorite pokémon message, if there is none', () => {
     render(<FavoritePokemons />);
     const notFound = screen.getByText(/No favorite pokemon found/i);
     expect(notFound).toBeInTheDocument();
+  });
+
+  test('show all pokémons', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ data } />);
+    const pokemons = screen.getAllByAltText(/is marked as favorite/i);
+    expect(pokemons.length).toBe(data.length);
   });
 });
