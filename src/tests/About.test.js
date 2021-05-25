95874e560d9f1e033062de0f93c90b1dd4e36cbd
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 15:51:35 -0300

    test: renders a reading with the text about pokedex (requisito 2.1)

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { About } from '../components';
+
+describe('test mutations for about page', () => {
+  test('renders a reading with the text `About Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+
+    const headingTwo = getByText(/About Pokédex/i);
+    expect(headingTwo).toBeInTheDocument();
+  });
+});
