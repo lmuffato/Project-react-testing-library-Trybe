cef4b5620cf68d403859b8eddfe40c158e04c4a4
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 15:33:58 -0300

    test: links redirect to correct route (requisito 1.3)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
@@ -26,5 +27,32 @@ describe('tests whether statistical elements are on the screen', () => {
     expect(linkHome).toBeInTheDocument();
     expect(linkPokemon).toBeInTheDocument();
     expect(linkAbout).toBeInTheDocument();
+  });
+  test('test if links redirect to correct route', () => {
+    let testLocation;
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+        <Route
+          path="/"
+          render={ ({ location }) => {
+            testLocation = location;
+            return null;
+          } }
+        />
+      </MemoryRouter>,
+    );
+
+    const linkHome = getByText(/home/i);
+    userEvent.click(linkHome);
+    expect(testLocation.pathname).toBe('/');
+
+    const linkAbout = getByText(/about/i);
+    userEvent.click(linkAbout);
+    expect(testLocation.pathname).toBe('/about');
+
+    const linkPokemon = getByText(/Favorite Pokémons/i);
+    userEvent.click(linkPokemon);
+    expect(testLocation.pathname).toBe('/favorites');
   });
 });
