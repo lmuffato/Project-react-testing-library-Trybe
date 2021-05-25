fddd43ee3e2bec8348ed8550d7b05727373d45ca
Ana Ventura <anaventura1811@gmail.com>
2021-05-25 15:16:58 -0300

    partes 2-4 do teste do componente pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -15,4 +15,53 @@ test('page renders a h2 heading with text `Encountered pokémons`', () => {
   expect(encounteredPkms).toBeInTheDocument();
 });
 
+test('click on button `next-pokémon` renders the next pokémon of the list'
+ + ' and only one Pokémon is shown each time', () => {
+  const { history } = renderWithRouter(<App />);
 
+  history.push('/');
+
+  userEvent.click(screen.getByTestId('next-pokemon'));
+  expect(screen.getByText('Próximo pokémon')).toBeInTheDocument();
+
+  const pkmName = screen.getByTestId('pokemon-name');
+  expect(pkmName).toBeInTheDocument();
+  const pokemonName = screen.getAllByTestId('pokemon-name');
+  expect(pokemonName).toHaveLength(1);
+
+  const pkmType = screen.getByTestId('pokemon-type');
+  expect(pkmType).toBeInTheDocument();
+  const pokemonType = screen.getAllByTestId('pokemon-type');
+  expect(pokemonType).toHaveLength(1);
+
+  const pkmWeight = screen.getByTestId('pokemon-weight');
+  expect(pkmWeight).toBeInTheDocument();
+  const pokemonWeight = screen.getAllByTestId('pokemon-weight');
+  expect(pokemonWeight).toHaveLength(1);
+});
+
+test('pokédex has filter buttons', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/');
+
+  const pkmListOfTypes = [...new Set(pokemons.map(({ type }) => type))];
+  const buttons = screen.getAllByTestId('pokemon-type-button');
+  expect(buttons).toHaveLength(pkmListOfTypes.length);
+
+  pkmListOfTypes.forEach((type, index) => {
+    expect(buttons[index]).toHaveTextContent(type);
+  });
+});
+
+test('pokédex has a button to reset filters', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/');
+
+  const btnReset = getByRole('button', {
+    name: /all/i,
+  });
+  expect(btnReset).toBeInTheDocument();
+  userEvent.click(btnReset);
+});
