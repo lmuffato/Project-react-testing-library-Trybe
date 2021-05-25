9ba4a4d2da99611aa6e7709b1a21231176f935bc
Sergio Martins <sergiomos2010@gmail.com>
2021-05-25 18:05:49 -0300

    fix: changing getByText to getByRole

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('tenting App', () => {
@@ -16,15 +17,15 @@ describe('tenting App', () => {
   });
 
   test('links are in the home', () => {
-    const { getByText } = render(
+    const { getByRole } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
 
-    const firstLink = getByText(/home/i);
-    const secondLink = getByText(/about/i);
-    const thirdLink = getByText(/favorite pokémons/i);
+    const firstLink = getByRole('link', { name: /home/i });
+    const secondLink = getByRole('link', { name: /about/i });
+    const thirdLink = getByRole('link', { name: /favorite/i });
 
     expect(firstLink).toBeInTheDocument();
     expect(secondLink).toBeInTheDocument();
@@ -32,15 +33,37 @@ describe('tenting App', () => {
   });
 
   test('Home renders app', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { history } = renderWithRouter(<App />);
 
-    const link = getByText(/home/i);
+    const link = screen.getByRole('link', { name: /home/i });
     userEvent.click(link);
-    const pokedex = getByText(/encountered pokémons/i);
-    expect(pokedex).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('About go to /about', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /about/i });
+    userEvent.click(link);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('Favorite Pokémons go to /favorites', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const link = screen.getByRole('link', { name: /favorite/i });
+    userEvent.click(link);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('NotFound when not have a know url', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+
+    history.push('/noFound/');
+    const noMatch = getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
   });
 });
