d2fac29de24a2cb317b374a6e1db5bfd82c2e91b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-05-25 18:57:02 -0300

    Req1: App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
+
+function renderWithRouter(
+  ui,
+  { route = '/', history = createMemoryHistory({ initialEntries: [route] }) } = {},
+) {
+  return {
+    ...render(<Router history={ history }>{ui}</Router>),
+    history,
+  };
+}
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +32,43 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('O topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  test('O primeiro link deve possuir o texto Home redirecionado para /', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    fireEvent.click(getByText(/Home/));
+    const homeLink = getByText(/Home/);
+    expect(homeLink).toBeInTheDocument();
+  });
+  test('O segundo link deve possuir o texto About redirecionado para /about', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const aboutLink = getByText(/About/);
+    expect(aboutLink).toBeInTheDocument();
+  });
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    fireEvent.click(getByText(/Favorite Pokémons/));
+    const favoritePokeLink = getByText(/Favorite Pokémons/);
+    expect(favoritePokeLink).toBeInTheDocument();
+  });
+});
+
+test('Redireciona para a página Not Found ao entrar em uma URL desconhecida', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  history.push('/pagina/nao-encontrada');
+  const notFound = getByText(/Page requested not found/);
+  expect(notFound).toBeInTheDocument();
 });
