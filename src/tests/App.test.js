2adbee4063eda4b7f0d9a6af8b6e8cfca49361cf
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-25 14:45:10 -0300

    iniciando o projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -11,4 +13,22 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+it('testa se o primeiro link contém o texto "Home"', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/Home/i);
+  expect(home).toBeInTheDocument();
+});
+
+it('testa se o primeiro link contém o texto "About"', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/About/i);
+  expect(home).toBeInTheDocument();
+});
+
+it('testa se o primeiro link contém o texto "Favorite Pokémons"', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const home = getByText(/Favorite Pokémons/i);
+  expect(home).toBeInTheDocument();
 });
