d77c5c8fddc0d66c5de5b16f697ea881be24041c
defauth98 <neto.daniribeiro@gmail.com>
2021-05-25 17:25:34 -0300

    testando os links no header

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('App', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders a links with texts Home, About, and favorite pokemons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const HomeLink = getByText(/Home/i);
+    const AboutLink = getByText(/About/i);
+    const FavoritePokemonsLink = getByText(/Favorite Pokémons/i);
+
+    expect(HomeLink).toBeInTheDocument();
+    expect(AboutLink).toBeInTheDocument();
+    expect(FavoritePokemonsLink).toBeInTheDocument();
+  });
+}
+)
