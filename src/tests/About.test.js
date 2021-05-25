3aef1f99d09b8f40a2ac332bc44f994e3e9ec478
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-25 16:02:46 -0300

    initial commit

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+const { render } = require("@testing-library/react");
+import App from '../App'
+import about from  '../components/About';
+
+describe('about tests', () => {
+  test('Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /.', () => {
+    const { getByRole } = render(<About />);
+  });
+})
+
