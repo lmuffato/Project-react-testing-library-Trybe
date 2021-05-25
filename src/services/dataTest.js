3fc8121db75fd1d6b4003afb9f08a591fbda0baf
Renzo Sevilha <sevilharenzo@gmail.com>
2021-05-25 13:08:07 -0300

    feat: req1

---
+++ src/services/dataTest.js
@@ -0,0 +1,36 @@
+const getRegex = (pattern) => new RegExp(pattern, 'i');
+
+export const appData = {
+  links: [
+    {
+      linkName: getRegex('home'),
+      linkPath: '/',
+    },
+    {
+      linkName: getRegex('about'),
+      linkPath: '/about',
+    },
+    {
+      linkName: getRegex('favorite pokémons'),
+      linkPath: '/favorites',
+    },
+  ],
+};
+
+export const aboutData = {
+  pokedexInfos: [
+    getRegex('this application simulates a pokédex'),
+    getRegex('One can filter Pokémons by type'),
+  ],
+  imgSrc:
+    'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+};
+
+export const favoritedPokemonsData = {
+  favoritedPokemon: [
+    getRegex('Pikachu'), 
+    getRegex('Electric'), 
+    getRegex('Average weight: 6.0 kg')
+  ],
+  imgSrc: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+}
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helper/renderWithRouter';
+
+import App from '../App';
+import { appData } from '../services/dataTest';
+
+describe('Requirement 1 - renders the App', () => {
+  const { links } = appData;
+
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders the right path when user click in the link', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    links.forEach(({ linkName, linkPath }) => {
+      const getLink = getByRole('link', { name: linkName });
+      expect(getLink).toBeInTheDocument();
+
+      userEvent.click(getLink);
+      const { pathname } = history.location;
+      expect(pathname).toBe(linkPath);
+    });
+  });
+
+  it('render the right component when it gets an inexistent path', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/not-found-page');
+
+    const headingNotFound = getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(headingNotFound).toBeInTheDocument();
+
+    const gifNotFound = getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(gifNotFound).toBeInTheDocument();
+  });
 });
