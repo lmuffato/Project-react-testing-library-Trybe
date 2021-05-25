feceded9a31d2ebe43689cef9242680d6226856e
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-25 18:41:40 -0300

    refactor(App.test.js): Muda o teste do link home de getByText para getByRole

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { getByRole } from '@testing-library/dom';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-  const home = getByText(/home/i);
-  expect(home).toBeInTheDocument();
+describe('Tests the <App> component', () => {
+  it('renders a heading with the text `Pokédex`', () => {
+    renderWithRouter(<App />);
+    const heading = screen.getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders a heading with the text `Pokédex`', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: /home/i });
+    expect(home).toBeInTheDocument();
+  });
 });
