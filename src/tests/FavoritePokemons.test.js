b4f3775d98ece6c6f9bf04843999afcda65cfe4d
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 16:40:19 -0300

    test: whether the correct text is displayed when there is no favorite pokemon (requisito 3.1)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { FavoritePokemons } from '../components';
+
+describe('with render correct results to favorites pokemons', () => {
+  test('whether the correct text is displayed when there is no favorite pokemon', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ [] } />
+      </MemoryRouter>,
+    );
+    const notFoundPokemon = getByText(/No favorite pokemon found/i);
+    expect(notFoundPokemon).toBeInTheDocument();
+  });
+});
