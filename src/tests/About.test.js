00e6b448a52c9a79c88b0856305507c4418eee6b
guidugaich <guidugaich.dev@gmail.com>
2021-05-25 17:05:26 -0300

    importanto bibliotecas no teste do about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+
 describe('2. Testando componente <About />', () => {
   test('Teste se a página contém as informações sobre a Pokédex.', () => {});
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {});
