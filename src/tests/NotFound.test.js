504ce21230da451e6d91a8f4d7a2cdde2d226752
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 17:36:36 -0300

    test: whether the heading is rendered with the correct text (requisito 4.1)

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { NotFound } from '../components';
+
+describe('', () => {
+  test('', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const headingTwo = getByRole('heading',
+      { name: 'Page requested not found Crying emoji', level: 2 });
+    expect(headingTwo).toBeInTheDocument();
+  });
+});
