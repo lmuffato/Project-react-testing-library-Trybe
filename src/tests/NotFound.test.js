2b644819162aa4b04cbbf9ea1aeec117a43f248a
nascjoao <oi@joaonasc.dev>
2021-05-25 17:55:23 -0300

    feat: (req 4) Testes para o componente "NotFound.js"

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import alias from './utils/alias';
+
+import NotFound from '../components/NotFound';
+
+describe('Testes para o componente "NotFound.js"', () => {
+  const { expectToBeInTheDocument } = alias;
+
+  test('A página contém um heading "h2" com o texto "Page requested not found 😭"', () => {
+    render(<NotFound />);
+
+    expectToBeInTheDocument(
+      screen.getByRole('heading', {
+        name: 'Page requested not found Crying emoji',
+        level: 2,
+      }),
+    );
+  });
+
+  test('A página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    render(<NotFound />);
+
+    const img = screen.getByRole('img', { name: /pikachu crying/i });
+
+    expectToBeInTheDocument(img);
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
