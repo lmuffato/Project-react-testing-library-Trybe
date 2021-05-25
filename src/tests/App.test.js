98cf5828fd597378e32cde04a87a49204cecd066
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 15:32:08 -0300

    feat: renders a links main menu (requisito 1.2)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -13,4 +13,18 @@ describe('tests whether statistical elements are on the screen', () => {
     const heading = getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
+  test('renders a links main menu', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = getByText(/home/i);
+    const linkPokemon = getByText(/Favorite Pokémons/i);
+    const linkAbout = getByText(/about/i);
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkPokemon).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+  });
 });
