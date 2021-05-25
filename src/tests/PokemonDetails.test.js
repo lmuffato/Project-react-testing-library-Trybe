b8ca11c072e2c5761789c016f4449d2e8ac107ac
Renzo Sevilha <sevilharenzo@gmail.com>
2021-05-25 18:06:37 -0300

    fix: req7-lint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -8,11 +8,12 @@ import App from '../App';
 
 describe('Requirement 7 - renders Pokemon', () => {
   const { headings, resume, maps } = pokemonDetailsData;
+  const pathPokemon = '/pokemons/25';
 
   it('renders pokemon headings and paragraphs', () => {
     const { history, getByRole, getByText } = renderWithRouter(<App />);
 
-    history.push('/pokemons/25');
+    history.push(pathPokemon);
 
     headings.forEach((heading) => {
       const header = getByRole('heading', { name: heading });
@@ -26,7 +27,7 @@ describe('Requirement 7 - renders Pokemon', () => {
   it('renders pokemon maps', () => {
     const { history, getAllByRole, getByText } = renderWithRouter(<App />);
 
-    history.push('/pokemons/25');
+    history.push(pathPokemon);
 
     maps.forEach((map, index) => {
       const { alt, src, mapName } = map;
@@ -44,7 +45,7 @@ describe('Requirement 7 - renders Pokemon', () => {
   it('renders a star icon when it clicks in the checkbox', () => {
     const { history, getByLabelText, getByRole } = renderWithRouter(<App />);
 
-    history.push('/pokemons/25');
+    history.push(pathPokemon);
 
     const checkBoxFavorite = getByLabelText(/pokémon favoritado\?/i);
     userEvent.click(checkBoxFavorite);
