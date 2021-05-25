b9b9c03d99196e2b60d8366e3ba482e09edde084
Ana Ventura <anaventura1811@gmail.com>
2021-05-25 15:09:50 -0300

    Parte 1 do teste do componente Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+test('page renders a h2 heading with text `Encountered pokémons`', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+  history.push('/');
+  const encounteredPkms = getByRole('heading', {
+    name: 'Encountered pokémons',
+    level: 2,
+  });
+  expect(encounteredPkms).toBeInTheDocument();
+});
+
+
