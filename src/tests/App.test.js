15ab00d91a5397926a104a86539c16d5f839df6a
Viegas <mvrdgs@gmail.com>
2021-05-25 16:51:59 -0300

    implement App.js tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -15,10 +16,37 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Testing Home link', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const homeLink = getByText(/home/i);
+  expect(homeLink).toBeInTheDocument();
+  fireEvent.click(homeLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('Testing About link', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const homeLink = getByText(/About/i);
+  expect(homeLink).toBeInTheDocument();
+  fireEvent.click(homeLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('Testing `Favorite Pokémon` link', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+  const homeLink = getByText(/Favorite Pokémon/i);
+  expect(homeLink).toBeInTheDocument();
+  fireEvent.click(homeLink);
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
 });
