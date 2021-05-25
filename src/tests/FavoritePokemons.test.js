dca19d451693a204badef51a1f30bfaf87908f76
Maria Baeta <m.martinsdg@gmail.com>
2021-05-25 20:37:15 -0300

    Resquest 3: test Favorite Pokémon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+// import { fireEvent } from '@testing-library/dom';
+import React from 'react';
+import FavoritePokemon from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Request 3: Test Favorite', () => {
+  it('renders text `No favorite pokemon found`', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemon />);
+    const notFavorite = getByText('No favorite pokemon found');
+    expect(notFavorite).toBeInTheDocument();
+  });
+});
