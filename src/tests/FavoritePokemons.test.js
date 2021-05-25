4f0cbc334c0a7b6821dfb8ee6609db4db11a6fb7
Viegas <mvrdgs@gmail.com>
2021-05-25 20:31:02 -0300

    refactor: FavoritePokemons test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 import renderWithRouter from '../renderWithRouter';
-import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Test \'Favorite Pokémons\'', () => {
   it('Test if \'Favorite Pokémons\' is rendered', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
-    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
-  });
-
-  it('Test if \'Favorite Pokémons\' is rendered', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
-    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+    const { getByText, getAllByText } = renderWithRouter(<App />);
+    const favoritesBtn = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favoritesBtn);
+    expect(getAllByText(/Favorite Pokémons/i).length).toBe(2);
   });
 
   it('Test if \'Favorite Pokémons\' if no cards are rendered if no Pokemon is favorited',
     () => {
-      const { getByText } = renderWithRouter(<FavoritePokemons />);
+      const { getByText } = renderWithRouter(<App />);
+      const favoritesBtn = getByText(/Favorite Pokémons/i);
+      fireEvent.click(favoritesBtn);
       expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
     });
 });
