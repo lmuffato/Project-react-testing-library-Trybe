e145d20a267f93928d0c97c5e569ed09268ee35f
Sergio Martins <sergiomos2010@gmail.com>
2021-05-25 18:28:57 -0300

    testing about pokedex info

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('About Component', () => {
+  test('Show pokedéx info', () => {
+    render(<About />);
+    const textInfo = screen.getByText(/this application simulates a pokédex/i);
+    expect(textInfo).toBeInTheDocument();
+  });
+});
