1e3108b70a930a4b26bc95fb42c7d7c2dff6fac1
Rafael Cardoso <rafael.vimur@gmail.com>
2021-05-25 19:10:47 -0300

    adiciona requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+test('Teste se página contém um texto Page requested not found', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem', () => {
+  const { getAllByRole } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+
+  const images = getAllByRole('img');
+  expect(images[0]).toHaveAttribute('aria-label', expect.stringMatching('Crying emoji'));
+  expect(images[1]).toHaveAttribute('src',
+    expect.stringMatching('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif'));
+});
