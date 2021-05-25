84fb30d0b0a4f2884a9afbad3240e1f558738c35
Orlando Flores <orlando_flores@id.uff.br>
2021-05-25 19:05:34 -0300

    feat: requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,64 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from './mockPokemons';
+
+describe('test the pokemon component', () => {
+  test('if the pokemon name is pikachu', () => {
+    const { getByTestId } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ false }
+    />);
+    const pokemonName = getByTestId('pokemon-name');
+    expect(pokemonName).toHaveTextContent(/pikachu/i);
+  });
+  test('if the pokemon type is Electric', () => {
+    const { getByTestId } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ false }
+    />);
+    const pokemonType = getByTestId('pokemon-type');
+    expect(pokemonType).toHaveTextContent(/electric/i);
+  });
+  test('if the pokemon has the right weight', () => {
+    const { getByTestId } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ false }
+    />);
+    const pokemonType = getByTestId('pokemon-weight');
+    expect(pokemonType).toHaveTextContent(/6.0 kg/i);
+  });
+  test('if the image of the pokemon is in the document', () => {
+    const { getByRole } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ false }
+    />);
+    const image = getByRole('img', {
+      name: /pikachu sprite/i,
+    });
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(pokemons[0].image);
+  });
+  test('if when favorite the star image is in the document', () => {
+    const { getByRole } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite
+    />);
+    const image = getByRole('img', {
+      name: /pikachu is marked as favorite/i,
+    });
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('http://localhost/star-icon.svg');
+  });
+  test('if more details get clicked the PokemonDetails is rendered', () => {
+    const { getByText, history } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+      isFavorite={ false }
+    />);
+    const moreDetailsLink = getByText(/more details/i);
+    userEvent.click(moreDetailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+});
