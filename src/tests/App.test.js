e8f5b8f4ee3fbfca566d715083d6b33e8362cc56
luanlsr <luanfswd@gmail.com>
2021-05-25 18:01:50 -0300

    requisito 1 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,11 +4,26 @@ import { render } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
+  const { getByText, getByRole } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+
+  const linkHome = getByRole('link', {
+    name: 'Home',
+  });
+  expect(linkHome).toBeInTheDocument();
+
+  const linkAbout = getByRole('link', {
+    name: 'About',
+  });
+  expect(linkAbout).toBeInTheDocument();
+
+  const linkFavoritePokemons = getByRole('link', {
+    name: 'Favorite Pokémons',
+  });
+  expect(linkFavoritePokemons).toBeInTheDocument();
 });
