e2c31246273ff70d7e3e4892e9068633af4937e5
guidugaich <guidugaich.dev@gmail.com>
2021-05-25 17:33:59 -0300

    subrequisito 1 - req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import About from '../components/About';
 
 describe('2. Testando componente <About />', () => {
-  test('Teste se a página contém as informações sobre a Pokédex.', () => {});
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+
+    const aboutHeading = getByText('About Pokédex');
+    expect(aboutHeading).toBeInTheDocument();
+  });
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {});
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {});
   test('Teste se a página contém a imagem de uma Pokédex:', () => {});
