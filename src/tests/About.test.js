5376abac3a5e86945a24838e003430c9e744809f
Viegas <mvrdgs@gmail.com>
2021-05-25 20:20:18 -0300

    refactor: About test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import renderWithRouter from '../renderWithRouter';
-import About from '../components/About';
+import App from '../App';
+import { fireEvent } from '@testing-library/dom';
 
 describe('`About` component', () => {
   it('Test if \'About\'\'s title is rendered', () => {
-    const { getByText, getByRole } = renderWithRouter(<About />);
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const aboutLink = getByText(/About/i);
+    fireEvent.click(aboutLink);
     const title = getByRole('heading', { level: 2 });
     expect(title).toBeInTheDocument();
     expect(getByText(/About Pokédex/i)).toBeInTheDocument();
   });
 
   it('Test \'About\'\'s image is rendered', () => {
-    const { getByRole } = renderWithRouter(<About />);
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const aboutLink = getByText(/About/i);
+    fireEvent.click(aboutLink);
     const pokedexImg = getByRole('img');
     expect(pokedexImg).toBeInTheDocument();
     expect(pokedexImg.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Test \'Pokedex\' component', () => {
+  const pokemons = [{
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+    summary: 'This intelligent Pokémon roasts hard berries'
+      + 'with electricity to make them tender enough to eat.',
+  }];
+  const isPokemonFavoriteById = { 25: false };
+  
+  it('Test heading \'Encountered pokémons\'', async () => {
+    const { getByText, getByRole } = await renderWithRouter(
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />,
+    );
+    expect(getByRole('heading', { level: 2 })).toBeInTheDocument();
+    expect(getByText(/Encountered pokémons/i)).toBeInTheDocument();
+  });
+});
