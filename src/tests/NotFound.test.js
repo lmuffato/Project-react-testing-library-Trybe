fcbe57ad05610c13cdefdf5c037ef67a15e348a7
Ana Ventura <anaventura1811@gmail.com>
2021-05-25 15:07:54 -0300

    Implementa requisito 4, testa componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('page renders a heading with text `Page requested not found 😭`'
++ 'when the route is invalid', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  history.push('/rota-que-não-existe');
+
+  const notFoundTest = getByRole('heading', {
+    name: /page requested not found/i,
+    level: 2,
+  });
+  expect(notFoundTest).toBeInTheDocument();
+});
+
+test('page renders the image Pikachu crying', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/rota-que-não-existe');
+
+  const altText = 'Pikachu crying because the page requested was not found';
+  const imagePath = screen.getByAltText(altText);
+  expect(imagePath.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
