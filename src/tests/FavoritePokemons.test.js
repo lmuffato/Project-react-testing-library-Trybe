22e0288991eaa601a844052d709d195f0bd598e5
Orlando Flores <orlando_flores@id.uff.br>
2021-05-25 15:41:21 -0300

    feat: requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('test the component favorites Pokemons', () => {
+  test('has the "Favorites Pokémons" text', () => {
+    const { getByRole } = render(<FavoritePokemons />);
+    const heading = getByRole('heading', {
+      name: /favorite pokémons/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('has the "No favorite pokemon found" text', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const heading = getByText('No favorite pokemon found');
+    expect(heading).toBeInTheDocument();
+  });
+});
