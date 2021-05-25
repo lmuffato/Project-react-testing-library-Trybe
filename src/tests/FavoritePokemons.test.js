6160707ee8e0fc70d0b2404b75514598f8bb7c60
Felipe Muller <felipeavmuller@gmail.com>
2021-05-25 18:20:23 -0300

    Testa se pikachu e favoritado corretamente

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
@@ -7,5 +8,17 @@ describe('Favorites pokemons tests', () => {
     const { history, getByText } = renderWithRouter(<App />);
     history.push('/favorites');
     expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+  it('PIkachu card if pikachu is favorite', () => {
+    const { history, getByRole, getByText } = renderWithRouter(<App />);
+    const pikachuDetail = getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(pikachuDetail);
+
+    const favoriteBtn = getByText('Pokémon favoritado?');
+    userEvent.click(favoriteBtn);
+    history.push('/favorites');
+    expect(getByText('Pikachu')).toBeInTheDocument();
   });
 });
