c33908b5daecabde81a4e939bc3d83fe3abff001
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-05-25 14:35:05 -0400

    fix requirement 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import About from '../components/About';
+
+describe('testing all screen application of the About', () => {
+  it('check the title screen application', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const about = getByRole('heading', {
+      name: /About Pokédex/i,
+    });
+    expect(about).toBeInTheDocument();
+  });
+  it('check image Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
