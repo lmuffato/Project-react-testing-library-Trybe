9ef2d5b9cb6d1981e052225f9c6f557f18948aec
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-05-25 19:22:06 -0300

    requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+test('if no favorite pokémon, show message', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const notFound = getByText(/No Favorite Pokemon found/i);
+  expect(notFound).toBeInTheDocument();
+});
+
+test('it is shown all the favorite pokémon cards', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const favorite = [pokemons[0]];
+  renderWithRouter(<FavoritePokemons pokemons={ favorite } />);
+  expect(getByText('Pikachu')).toBeInTheDocument();
+});
+
+test('it is shown none pokémon cards if not favorite', () => {
+  const { queryByText } = renderWithRouter(<FavoritePokemons />);
+  const favorite = [];
+  renderWithRouter(<FavoritePokemons pokemons={ favorite } />);
+  expect(queryByText('Pikachu')).not.toBeInTheDocument();
+});
