0f03c35117f58a9565d2b332678aed0adceb51ea
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-05-25 18:27:37 -0300

    Desafio 2OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
+// import React from 'react';
+// import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it(' No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
-    
+    // const { notFound } = render(<FavoritePokemons />);
+
   });
 
   it('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
