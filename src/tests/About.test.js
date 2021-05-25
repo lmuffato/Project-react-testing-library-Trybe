9a5f66cfe59ed4118af812570b8af1c5e818e8a4
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-25 16:34:06 -0300

    creating req2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+it('renders a reading with the text `About Pokedex`', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const aboutPage = getByRole('heading', { name: /about pokédex/i, level: 2 });
+  expect(aboutPage).toBeInTheDocument();
+});
