4b27d9910d1477da2f5fb4d619e89f602aafc65d
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-25 20:36:32 -0300

    test: three tests added for <App />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Testing the <App.js /> component', () => {
+  test('renders a reading with the text \'Pokédex\'', () => {
+    const { getByText } = renderWithRouter(<App />);
+
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('shows the home page when the route is \'/\'', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const { location: { pathname } } = history;
+    const homePageText = getByText(/Encountered pokémons/i);
+
+    expect(pathname).toBe('/');
+    expect(homePageText).toBeInTheDocument();
+  });
+
+  test('the application contains a fixed set of nagivation links', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const navigationLinks = getAllByRole('link');
+    const firstLink = navigationLinks[0];
+    const secondLink = navigationLinks[1];
+    const thirdLink = navigationLinks[2];
+
+    expect(firstLink.textContent).toBe('Home');
+    expect(secondLink.textContent).toBe('About');
+    expect(thirdLink.textContent).toBe('Favorite Pokémons');
+  });
 });
