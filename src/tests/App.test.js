76dfc50ed9523701b231686d746eda72379bbe57
rafael <rafittu@outlook.com>
2021-05-25 17:25:37 -0300

    renderiza a pagina pokedex

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Testando o componente App.js', () => {
+  it('A página Pokédex é renderizada no caminho de URL /', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>
+    );
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+    const heading = screen.getByRole('heading', {
+      name: /Pokédex/i,
+      level: 1,
+    });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('A pagina contém links de navegação Home, About e Favorite Pokémons', () => {
+    // Escreva o teste aqui
+  });
+
+  it('Ao clicar no link Home, é redirecionado para pagina inicial URL /', () => {
+    // Escreva o teste aqui
+  });
+
+  it('Ao clicar no link About, é redirecionado para URL /about', () => {
+    // Escreva o teste aqui
+  });
+
+  it('Ao clicar no link Favorite Pokémons, é redirecionado para URL /favorites', () => {
+    // Escreva o teste aqui
+  });
+
+  it('URL desconhecida redireciona para página Not Found', () => {
+    // Escreva o teste aqui
+  });
 });
