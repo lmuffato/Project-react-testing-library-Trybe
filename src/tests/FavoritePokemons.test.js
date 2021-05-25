f48d1b26483318960ef354c5d1874603885876b0
guidugaich <guidugaich.dev@gmail.com>
2021-05-25 18:19:22 -0300

    monstando estrutura para testes do requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,5 @@
-test('', () => {});
+describe('3. Testando componente FavoritePokemon', () => {
+  test('Verificar se a mensagem correta é mostrada quando não há favoritos', () => {});
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
+  test('Nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {});
+});
