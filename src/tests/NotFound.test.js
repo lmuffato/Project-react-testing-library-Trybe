d5ca7be51e32b74bbb48d2e7f5cf46c7c11294c0
Orlando Flores <orlando_flores@id.uff.br>
2021-05-25 15:50:23 -0300

    feat: requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('test if the component NotFound renders perfectly', () => {
+  test('render a text with "Page requested not found"', () => {
+    const { getByRole } = render(<NotFound />);
+    const heading = getByRole('heading', {
+      name: /Page requested not found/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('the correct url for the image', () => {
+    const { getByRole } = render(<NotFound />);
+    const image = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
