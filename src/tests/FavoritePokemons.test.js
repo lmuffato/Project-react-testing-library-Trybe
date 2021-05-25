3eed062f05c0856a6dbe93e9b7d20a1e76b5d635
Eduardo Seije <eduardoseije@gmail.com>
2021-05-25 17:15:22 -0300

    Requisito 03 - teste

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testes do componente FavoritePokemons', () => {
+  test('Testa se a mensagem No Favorite Pokemon é exibida na tela', () => {
+    render(<FavoritePokemons />);
+    const noFavPokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavPokemon).toBeInTheDocument();
+  });
+});
