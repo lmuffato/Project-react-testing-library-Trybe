505a71e1b9156cd49214a02d348e5113705dafae
defauth98 <neto.daniribeiro@gmail.com>
2021-05-25 18:21:35 -0300

    testando o texto da pagina not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('not found page', () => {
+  it('render a not found page', () => {
+    const { getByText, history } = renderWithRouter(<App />)
+
+    history.push('/not-found');
+
+    const heading = getByText(/Page requested not found/g);
+    expect(heading).toBeInTheDocument();
+  })
+})
