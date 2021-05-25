5d0f1ff823e8b67f6a2c1d457597840c9d529c21
Hernanne <hernannedomiciano@gmail.com>
2021-05-25 19:31:15 -0300

    feat: req04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Testing NotFound page', () => {
+  it('testing heading', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const heading = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    const image = getByRole('img', {
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+    expect(image.src).toBe(imgSrc);
+  });
+});
