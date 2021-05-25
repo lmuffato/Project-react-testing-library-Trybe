a4608e0283affed6efb0d2a14edfca2db59dd601
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-25 19:34:49 -0300

    feat: requirement02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import { About } from '../components';
+
+describe('Test the component <About.js>', () => {
+  it('Test if the page contains information about Pokédex', () => {
+    const { getByRole, getByText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+
+    expect(getByRole('heading', {
+      name: /about pokédex/i,
+    })).toBeInTheDocument();
+
+    expect(getByText(/this application simulates a pokédex,/i)).toBeInTheDocument();
+
+    expect(getByText(/one can filter pokémons by type/i)).toBeInTheDocument();
+
+    const imagem = getByRole('img', {
+      name: /pokédex/i,
+    });
+
+    expect(imagem).toBeInTheDocument();
+
+    expect(imagem.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
