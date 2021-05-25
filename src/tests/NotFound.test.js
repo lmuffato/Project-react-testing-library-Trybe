c1e47c241f6344e1ff2692ad508657131cbf640e
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-05-25 19:15:35 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('show the not found page with all the elements', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  history.push('/rota-que-não-existe');
+
+  const notFoundText = getByRole('heading', {
+    name: /Page requested not found/i,
+    level: 2,
+  });
+
+  const notFoundImg = getByRole('img', {
+    name: /Pikachu crying because the page requested was not found/,
+  });
+  const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(notFoundImg).toHaveAttribute('src', src);
+  expect(notFoundText).toBeInTheDocument();
+});
