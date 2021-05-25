596a9824312eead912878ca06c1187fe1b59d03d
Giovanni <giovannitadeu08@gmail.com>
2021-05-25 16:55:30 -0300

    [Requisito 3] - Feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Requirement 3', () => {
+  it('"No favorite pokemon found" is displayed on the screen', () => {
+    const { queryByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavoritePokemonMessage = queryByText(/No favorite pokemon found/i);
+    expect(noFavoritePokemonMessage).toBeInTheDocument();
+  });
+
+  it('Favorite Pokémon cards are displayed.', () => {
+    const { queryAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ pokemons } />,
+    );
+    const pokemonList = queryAllByTestId('pokemon-name');
+    expect(pokemonList.length).toBe(pokemons.length);
+  });
+
+  it('Pokémon card is displayed, is not favored', () => {
+    const { queryAllByTestId } = renderWithRouter(
+      <FavoritePokemons pokemons={ [] } />,
+    );
+    const pokemonList = queryAllByTestId('pokemon-name');
+    expect(pokemonList.length).toBe(0);
+  });
+});
