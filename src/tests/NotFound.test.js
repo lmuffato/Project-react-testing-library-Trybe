88d1e7926173d88c731b6829827231448878d09c
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 17:45:03 -0300

    test: tests whether the image is rendered with the correct src (requisito 4.2)

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -14,4 +14,14 @@ describe('tests the notFound component', () => {
       { name: 'Page requested not found Crying emoji', level: 2 });
     expect(headingTwo).toBeInTheDocument();
   });
+  test('tests whether the image is rendered with the correct src', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const image = getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
 });
