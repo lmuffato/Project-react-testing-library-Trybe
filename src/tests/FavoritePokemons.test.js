aa8e080615bec2568d99bbf4cf712579564f397c
luanlsr <luanfswd@gmail.com>
2021-05-25 19:38:42 -0300

    requisito 3 finalizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -3,30 +3,30 @@ import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
 
 describe('Verificar a página de Pokemons Favoritos', () => {
-  const pokemons = [
-    {
-      id: 25,
-      name: 'Pikachu',
-      type: 'Electric',
-      averageWeight: {
-        value: '6.0',
-        measurementUnit: 'kg',
-      },
-      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-      foundAt: [
-        {
-          location: 'Kanto Viridian Forest',
-          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-        },
-        {
-          location: 'Kanto Power Plant',
-          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-        },
-      ],
-      summary: 'This intelligent Pokémon roasts hard berries with'
-      + ' electricity to make them tender enough to eat.',
-    }];
+  // const pokemons = [
+  //   {
+  //     id: 25,
+  //     name: 'Pikachu',
+  //     type: 'Electric',
+  //     averageWeight: {
+  //       value: '6.0',
+  //       measurementUnit: 'kg',
+  //     },
+  //     image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+  //     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+  //     foundAt: [
+  //       {
+  //         location: 'Kanto Viridian Forest',
+  //         map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+  //       },
+  //       {
+  //         location: 'Kanto Power Plant',
+  //         map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+  //       },
+  //     ],
+  //     summary: 'This intelligent Pokémon roasts hard berries with'
+  //     + ' electricity to make them tender enough to eat.',
+  //   }];
   it('deve renderizar a pagina de favoritos vazia', () => {
     const { getByRole, getByText } = renderWithRouter(<FavoritePokemons />);
     const h2 = getByRole('heading', {
