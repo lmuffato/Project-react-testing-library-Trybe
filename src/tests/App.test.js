f4936dbf72c10bd8f69a4fe36b8d1e4ec27dc915
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-05-25 14:14:33 -0400

    component App test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste do componente App', () => {
+  test('teste da página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const heading = screen.getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+    let { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const navTexts = ['Home', 'About', 'Favorite Pokémons'];
+    const navMenu = screen.getAllByRole('link');
+    for (let i = 0; i < 3; i +=1) {
+      expect(navMenu[i].innerHTML).toBe(navTexts[i]);
+    }
+    const homeLink = screen.getByText('Home');
+    userEvent.click(homeLink);
+    pathname = history.location.pathname;
+    expect(pathname).toBe('/');
+    const aboutLink = screen.getByText('About');
+    userEvent.click(aboutLink);
+    pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+    const favoritesLink = screen.getByText('Favorite Pokémons');
+    userEvent.click(favoritesLink);
+    pathname = history.location.pathname;
+    expect(pathname).toBe('/favorites');
+    history.push('/invalido');
+    const errorText = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i
+    });
+    expect(errorText).toBeInTheDocument();
+  });
 });
