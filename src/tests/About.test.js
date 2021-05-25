00cbeeb9d051d47ce0b6976b348bd91511825b34
Orlando Flores <orlando_flores@id.uff.br>
2021-05-25 15:33:11 -0300

    feat: requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('test if the component About renders perfectly', () => {
+  test('render a text with "About Pokedex"', () => {
+    const { getByRole } = render(<About />);
+    const heading = getByRole('heading', {
+      name: /about pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('the correct url for the image', () => {
+    const { getByRole } = render(<About />);
+    const image = getByRole('img');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
