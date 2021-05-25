547ca69caab3feeb421744479c44b7f38c1631b1
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-25 17:42:46 -0300

    req 4 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
-it('renders a reading with the text `About Pokedex`', () => {
+it('renders a reading with the text `No favorite pokemon found`', () => {
   const { getByText } = renderWithRouter(<FavoritePokemons />);
   const favoriteText = getByText(/No favorite pokemon found/i);
   expect(favoriteText).toBeInTheDocument();
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+const notFoundSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+it('should render the component "not found"', () => {
+  const { history, getByRole } = renderWithRouter(<NotFound />);
+  history.push('/route-not-found');
+  const notFoundText = getByRole('heading',
+    { name: /page requested not found/i, level: 2 });
+  expect(notFoundText).toBeInTheDocument();
+});
+
+it('should contains an img with src "notFoundSrc"', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const image = getAllByRole('img');
+  expect(image[1]).toHaveAttribute('src', notFoundSrc);
+});
