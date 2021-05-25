afb90a467c7d9c7f68a36a06fcfaf7d911042fd1
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-05-25 20:11:21 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('NotFound.js', () => {
+  test('tensting page', () => {
+    const { getAllByRole, getByText } = renderWithRouter(<NotFound />);
+    const textoPrincipal = getByText('Page requested not found');
+    expect(textoPrincipal.tagName).toBe('H2');
+    const image = getAllByRole('img');
+    expect(image[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
