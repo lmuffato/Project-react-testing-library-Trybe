8977d07f138a1d69b6eb60086150e27df8f3a3ab
Edrei <edreimateus@gmail.com>
2021-05-25 17:51:17 -0300

    feat: 3 requirement

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testing favorite pokemon page', () => {
+  test('see the message No favorite Pokémon found on the screen', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFoundMessage = getByText(/No favorite pokemon found/i);
+    expect(notFoundMessage).toBeInTheDocument();
+  });
+});
