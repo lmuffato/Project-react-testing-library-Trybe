0ea134bfca4b9c3e0e86c10ccf38c1cfa69b6f9c
Debora Passos <deborinhapassos@gmail.com>
2021-05-25 19:35:27 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,11 +3,28 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+describe('App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText, getByRole } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+
+    const home = getByRole('link', {
+      name: 'Home',
+    });
+    expect(home).toBeInTheDocument();
+
+    const about = getByRole('link', {
+      name: 'About',
+    });
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
 });
