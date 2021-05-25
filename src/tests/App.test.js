bafb9f445c5dc90e642bc13bf6c72f5241bbb9ec
jonathan <jonathanfernandesjf98@gmail.com>
2021-05-25 15:39:44 -0300

    criaçao da funçao helper e requisito 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('renderiza três links de navegação no topo da página', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const link1 = getByRole('link', { name: 'Home' });
+  expect(link1).toBeInTheDocument();
+  const link2 = getByRole('link', { name: 'About' });
+  expect(link2).toBeInTheDocument();
+  const link3 = getByRole('link', { name: 'Favorite Pokémons' });
+  expect(link3).toBeInTheDocument();
+});
+
+test('ao clicar no botão home, a aplicação é redirecionada para a home', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+  const pathName = history.location.pathname;
+  const buttonHome = getByRole('link', { name: 'Home' });
+  userEvent.click(buttonHome);
+  expect(pathName).toBe('/');
 });
