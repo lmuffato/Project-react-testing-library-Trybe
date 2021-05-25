2ab7ea856b6d315ecfb73395a13348ec7b34c644
Eduardo Seije <eduardoseije@gmail.com>
2021-05-25 17:54:46 -0300

    Requisito 03 - complementação

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('Testes do componente FavoritePokemons', () => {
   test('Testa se a mensagem No Favorite Pokemon é exibida na tela', () => {
     render(<FavoritePokemons />);
     const noFavPokemon = screen.getByText(/No favorite pokemon found/i);
     expect(noFavPokemon).toBeInTheDocument();
+  });
+  test('Testa se todos os Pokemons favoritados aparecem', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const moreDetailsLink = screen.getByText(/More details/i);
+    // expect(moreDetailsLink).toBeInTheDocument();
+    userEvent.click(moreDetailsLink);
+    // const favPokemon = screen.getByText(/Pokémon favoritado?/i);
+    // expect(favPokemon).toBeInTheDocument();
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    const favPokemonComponent = screen.getByText(/Favorite Pokémon/i);
+    userEvent.click(favPokemonComponent);
+    const pokemonDetail = screen.getByText(/More details/i);
+    expect(pokemonDetail).toBeInTheDocument();
   });
 });
