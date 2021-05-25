c5d1e6219d6c48bf8b68100d8e965bb7a03272b9
Renzo Sevilha <sevilharenzo@gmail.com>
2021-05-25 14:18:25 -0300

    feat: req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+
+import renderWithRouter from '../helper/renderWithRouter';
+
+import NotFound from '../components/NotFound';
+
+describe('Requirement 4 - renders the NotFound', () => {
+  it('renders notFound heading', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const headingNotFound = getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(headingNotFound).toBeInTheDocument();
+
+    const imageNotFound = getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(imageNotFound).toBeInTheDocument();
+  });
+});
