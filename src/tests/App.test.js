ae6f7857bb08e4ca4d4e6602b0f49c98d3965c85
w-cypriano <profwellington17@gmail.com>
2021-05-25 17:37:14 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -11,4 +11,26 @@ describe('Testando o componente App', () => {
     });
     expect(heading).toBeInTheDocument();
   });
+  test('O primeiro link deve possuir o texto Home', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const link = getAllByRole('link', {
+      name: /Home/i,
+    });
+    expect(link).toHaveLength(1);
+  });
+  test('O Segundo link deve possuir o texto About', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const link = getAllByRole('link', {
+      name: /About/i,
+    });
+    expect(link).toHaveLength(1);
+  });
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const link = getAllByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(link).toHaveLength(1);
+  });
 });
+
