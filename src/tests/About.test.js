cad92f2b2060c2d9d666316112089767925f52d5
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 16:12:02 -0300

    test: renders a paragraphs with the correct text (requisito 2.2)

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -14,4 +14,22 @@ describe('test mutations for about page', () => {
     const headingTwo = getByText(/About Pokédex/i);
     expect(headingTwo).toBeInTheDocument();
   });
+  test('renders paragraphs with the correct text', () => {
+    const paragraphOne = 'This application simulates a Pokédex, '
+        + 'a digital encyclopedia containing all Pokémons';
+    const paragraphTwo = 'One can filter Pokémons by type, '
+        + 'and see more details for each one of them';
+
+    const { getByText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+
+    const pOne = getByText(paragraphOne);
+    const pTwo = getByText(paragraphTwo);
+
+    expect(pOne).toBeInTheDocument();
+    expect(pTwo).toBeInTheDocument();
+  });
 });
