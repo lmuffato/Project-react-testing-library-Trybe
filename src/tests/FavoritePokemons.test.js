530c40b848f4bd4b91948aa66b324383ed82cc07
Sergio Martins <sergiomos2010@gmail.com>
2021-05-25 19:11:08 -0300

    Testing: show notfound pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('Favorite Pokemons', () => {
+  test('No Favorite pokémon message, if there is none', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});
