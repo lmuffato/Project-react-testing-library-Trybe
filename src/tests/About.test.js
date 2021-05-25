03108b8d62e22f0b17499e27beefc9d983f8969a
Jodiel Briesemeister <jodielll29@gmail.com>
2021-05-25 16:01:00 -0300

    feat: requirement 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+it('contains info about pokédex', () => {
+  render(<About />);
+
+  const info = screen.getByText(/can filter Pokémons by type, and see more details/i);
+  expect(info).toBeInTheDocument();
+});
+
+it('contains h2 with text About pokédex', () => {
+  const { getByRole } = render(<About />);
+
+  const h2 = getByRole('heading', {
+    name: 'About Pokédex',
+    level: 2,
+  });
+
+  expect(h2).toBeInTheDocument();
+});
+
+it('contains a pokédex img', () => {
+  const { getByAltText } = render(<About />);
+
+  const img = getByAltText('Pokédex');
+  const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/'
+  + 'Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+  expect(img.src).toContain(url);
+});
