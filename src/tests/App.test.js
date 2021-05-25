e7374d2a988aea84afe992343575c134a919b475
Felipe Muller <felipeavmuller@gmail.com>
2021-05-25 15:50:45 -0300

    Correçao no lint dos primeiros testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter, Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
