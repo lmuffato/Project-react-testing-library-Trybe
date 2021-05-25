3e6218ff3d3f990c1be5f6b0dfcbf1369736ed7a
Bruno Mendes <mendessbruno@gmail.com>
2021-05-25 18:24:15 -0300

    resolvido requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+
+const isPokemonFavoriteById = {
+  4: false,
+  10: false,
+  23: false,
+  25: true,
+  65: false,
+  78: false,
+  143: false,
+  148: false,
+  151: false,
+};
+
+test('renders Pokedex and execute all functions', () => {
+  const { getByRole, getByText, getAllByRole, getAllByTestId } = render(
+    <MemoryRouter>
+      <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ isPokemonFavoriteById } />
+    </MemoryRouter>,
+  );
+
+  const heading = getByRole('heading', { name: 'Encountered pokémons' });
+  expect(heading).toBeInTheDocument();
+
+  const pikachu = getByText(/Pikachu/i);
+  expect(pikachu).toBeInTheDocument();
+
+  const buttons = getAllByRole('button');
+  const count = 9;
+  expect(buttons.length).toBe(count);
+
+  const nextBtn = getByRole('button', { name: 'Próximo pokémon' });
+  userEvent.click(nextBtn);
+  const charmander = getByText(/Charmander/i);
+  expect(charmander).toBeInTheDocument();
+
+  const allBtn = getByRole('button', { name: /All/i });
+  userEvent.click(allBtn);
+  expect(pikachu).toBeInTheDocument();
+  expect(nextBtn.disabled).toBe(false);
+
+  const typebtns = getAllByTestId('pokemon-type-button');
+  const typeArr = typebtns.map((button) => button.innerHTML);
+  expect(typeArr).toEqual([
+    'Electric',
+    'Fire',
+    'Bug',
+    'Poison',
+    'Psychic',
+    'Normal',
+    'Dragon',
+  ]);
+});
