7f46b5a79ec22f8b1be3eb347b75a3f412c6d99f
michael-petterson-06 <michaelpetterson06l@gmailo.com>
2021-05-25 18:36:38 -0300

    requisito 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('About.js', () => {
+  test('Infomation about Pokedex', () => {
+    const { container, getByRole } = renderWithRouter(<About />);
+    const textoPrincipal = getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(textoPrincipal).toBeInTheDocument();
+    const paragrafos = container.querySelectorAll('p');
+    expect(paragrafos.length).toBe(2);
+
+    const fotoPokedex = getByRole('img');
+    expect(fotoPokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+const quantidadeBotoes = 9;
+
+describe('App.js', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { history, getByText, getAllByRole } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+    const linkHome = getByText('Home');
+    expect(linkHome).toBeInTheDocument(linkHome);
+    userEvent.click(linkHome);
+    const botoes = getAllByRole('button');
+    expect(botoes.length).toBe(quantidadeBotoes);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('testing link about', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const linkAbout = getByText('About');
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+    const textoAbout = getByText(/This application/i);
+    expect(textoAbout).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('testing link favorite', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const linkFavorite = getByText('Favorite Pokémons');
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+    const textoFavorite = getByText('Favorite Pokémons');
+    expect(textoFavorite).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('testing not found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/Qualquer pagina/');
+
+    const notFound = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+
+    expect(notFound).toBeInTheDocument();
+  });
 });
