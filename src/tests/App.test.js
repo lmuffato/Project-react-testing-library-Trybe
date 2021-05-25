d814cd2dccabc3777a5995a99cd4374625d5139f
guidugaich <guidugaich.dev@gmail.com>
2021-05-25 16:04:22 -0300

    refatorando para item 1 do requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,22 +3,12 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
+test('página principal da Pokédex é renderizada ao carregar a aplicação em /`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-  const heading = getByText(/Pokédex/i);
+  const heading = getByText(/Encountered pokémons/i);
   expect(heading).toBeInTheDocument();
-});
-
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
