b2046195c6f9a2cedf5e3829a89e451123ebbedd
defauth98 <neto.daniribeiro@gmail.com>
2021-05-25 18:02:23 -0300

    testando se a pagina about é exibida

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('about', () => {
+  it('render a about page', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const AboutLink = getByText(/About/i);
+
+    fireEvent.click(AboutLink);
+
+    const AboutPageHeader = getByText(/About Pokédex/i);
+    expect(AboutPageHeader).toBeInTheDocument();
+  });
+})
