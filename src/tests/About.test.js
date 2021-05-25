0cb8141be027c91c545c1e6862f858998b2a4d5c
defauth98 <neto.daniribeiro@gmail.com>
2021-05-25 18:08:41 -0300

    testando a imagem da pagina sobre

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
-import renderWithRouter from '../renderWithRouter';
 
 describe('about', () => {
   it('render a about page', () => {
@@ -19,4 +18,19 @@ describe('about', () => {
     const AboutPageHeader = getByText(/About Pokédex/i);
     expect(AboutPageHeader).toBeInTheDocument();
   });
-})
+
+  it('render a about image', () => {
+    const { getByText, getByAltText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const AboutLink = getByText(/About/i);
+    fireEvent.click(AboutLink);
+
+    const image = getByAltText('Pokédex');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
