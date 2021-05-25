6c836441a401388722246966dbff429084246d03
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 15:30:38 -0300

    feat: renders a reading with the text (requisito 1.1)

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,14 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('tests whether statistical elements are on the screen', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 });
