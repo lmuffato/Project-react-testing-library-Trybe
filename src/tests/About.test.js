3733c1d3f2b1d61beb7cc97b1d4d5641a874910e
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-05-25 17:29:13 -0300

    missing semicolon

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -25,7 +25,7 @@ test('Teste se a página contém as informações sobre a Pokédex.', () => {
   const aboutImg = getByRole('img', {
     name: /Pokédex/,
   });
-  const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
+  const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
   expect(aboutImg).toHaveAttribute('src', src);
 });
