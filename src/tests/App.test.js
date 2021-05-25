0893d48025bff40c517d16f6fd0a6b6fc9c754a2
w-cypriano <profwellington17@gmail.com>
2021-05-25 16:44:19 -0300

    fix lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter'
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-describe('Testes componente app', ()=> {
+describe('Testando o componente App', () => {
   test('renderiza o titulo  `Pokédex`', () => {
     const { getByRole } = renderWithRouter(<App />);
     const heading = getByRole('heading', {
-      name:/Pokédex/i,
-      level:1
+      name: /Pokédex/i,
+      level: 1,
     });
     expect(heading).toBeInTheDocument();
   });
