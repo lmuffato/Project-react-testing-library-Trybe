5c8de0fed95c1b8ccbcf722fa5cab90449acd743
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-05-25 18:54:52 -0300

    :rocket: Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { NotFound } from '../components';
+
+describe('Tests for the <NotFound> component', () => {
+  test('render an h2 heading with the text Page requested not found 😭', () => {
+    const { getByRole, getByLabelText } = render(
+      <MemoryRouter initialEntries={ ['/page-that-doesnt-exist'] }>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const heading = getByRole('heading', { level: 2, name: /Page requested not found/i });
+    expect(heading).toBeInTheDocument();
+
+    const img = getByLabelText(/Crying emoji/i);
+    expect(img).toBeDefined();
+
+    const gifContainer = document.querySelector('.not-found-image');
+    expect(gifContainer.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
