0ffff42525b774cd6576f125b7724d35f5374d53
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-05-25 15:08:38 -0400

    notFound component test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('testes gerais do componente', () => {
+  renderWithRouter(<NotFound />);
+
+  const notFoundText = screen.getByRole('heading', {
+    level: 2,
+  });
+  expect(notFoundText).toBeInTheDocument();
+  const gifUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const pageGifUrl = screen.getByAltText(/pikachu/i);
+  expect(pageGifUrl.src).toEqual(gifUrl);
+});
