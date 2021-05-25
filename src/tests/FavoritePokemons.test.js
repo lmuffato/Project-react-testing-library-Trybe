ac74137ec5265f616b0b60a76c4ac9dc926ff06b
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-25 17:18:23 -0300

    req 3 done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+it('renders a reading with the text `About Pokedex`', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const favoriteText = getByText(/No favorite pokemon found/i);
+  expect(favoriteText).toBeInTheDocument();
+});
