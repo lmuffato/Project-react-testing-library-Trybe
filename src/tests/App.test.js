edf7d6f8151ca30eb6546801db3b83fa81ccf25c
Eduardo Seije <eduardoseije@gmail.com>
2021-05-25 15:27:27 -0300

    Requisito 01 - implementado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -13,3 +13,33 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+describe('Testa se o topo da aplicação contém um conjunto fixo de links', () => {
+  it('Testa se o primeiro link possui o texto Home', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const homeLink = getByText(/Home/i);
+    expect(homeLink).toBeInTheDocument();
+  });
+  it('Testa se o segundo link possui o texto About', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const aboutLink = getByText(/About/i);
+    expect(aboutLink).toBeInTheDocument();
+  });
+  it('Testa se o terceiro link possui o texto Favorite Pokémons', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const favLink = getByText(/Favorite Pokémons/i);
+    expect(favLink).toBeInTheDocument();
+  });
+});
