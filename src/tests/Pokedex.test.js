2e7136895d88025f2f3498adb757a50bf81d3789
Alexandre Oliveira <alexandremolive@gmail.com>
2021-05-25 18:19:29 -0300

    creating req5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('renders a "home" page with the text "encountered pokémons"', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const home = getByRole('heading', { name: /encountered pokémons/i, level: 2 });
+  expect(home).toBeInTheDocument();
+});
+
+it('should render the next pokemon when click on button "Próximo pokémon"', () => {
+  const { getByRole, getByText } = renderWithRouter(<App />);
+  const buttonNext = getByRole('button', { name: /próximo pokémon/i });
+  userEvent.click(buttonNext);
+  const charmander = getByText('Charmander');
+
+  expect(buttonNext).toBeInTheDocument();
+  expect(charmander).toBeInTheDocument();
+});
