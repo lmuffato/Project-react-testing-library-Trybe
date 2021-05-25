69ba1e4809a8bea2d711959d7e653905e990a5de
Willian Mendes <willianmendesbr@gmail.com>
2021-05-25 17:54:54 -0300

    test: whether the heading is rendered with the correct tex (requisito 5.1)

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Pokedex } from '../components';
+import data from '../data';
+
+describe('tests the pokedex component', () => {
+  const pokemonsFavorites = {
+    4: false,
+    10: false,
+    23: false,
+    25: true,
+    65: false,
+    78: false,
+    143: false,
+    148: false,
+    151: false,
+  };
+  const pokemons = data;
+
+  test('tests whether the heading is rendered with the correct text', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <Pokedex pokemons={ pokemons } isPokemonFavoriteById={ pokemonsFavorites } />
+      </MemoryRouter>,
+    );
+    const headingTwo = getByRole('heading',
+      { name: 'Encountered pokémons', level: 2 });
+    expect(headingTwo).toBeInTheDocument();
+  });
+});
