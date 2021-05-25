8ffa9304098935d2ff5dd4190ca4bc7fd1770095
lucas-paz <lucas.hpaz@outlook.com>
2021-05-25 15:08:01 -0300

    Create function renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../components/helper';
+
+test('A página principal é exibida pra a url "/"', () => {
+  const { getByRole } = renderWithRouter(<App />);
+});
