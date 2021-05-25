dd17a0b58b6fb89b2f79e05ed957e336140bc309
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-05-25 17:40:20 -0400

    fix requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('test all screen application of the FavoritePokemons', () => {
+  it('"No favorite pokemon" found msg screen,if a person has no favorite pokemon',
+    () => {
+      const { getByText } = renderWithRouter(<FavoritePokemons />);
+      const notFavorite = getByText(/No favorite pokemon found/i);
+      expect(notFavorite).toBeInTheDocument();
+    });
+});
