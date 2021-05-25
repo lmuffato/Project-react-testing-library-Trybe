97ffe2a4ab7807799f408211e3b6b715372531dd
biazidioti <bzferreira.bio@gmail.com>
2021-05-25 15:25:48 -0300

    Start project.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -11,4 +11,14 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+
+  /*  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  }); */
 });
