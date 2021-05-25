de55f0b79cdd84344cb230b21fb171a2fc8b8dff
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-05-25 19:36:37 -0300

    Desafio 3 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,48 @@
-// import React from 'react';
-// import FavoritePokemons from '../components/FavoritePokemons';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(
+      <Router history={ history }>
+        { component }
+      </Router>,
+    ),
+    history,
+  });
+};
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it(' No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => {
-    // const { notFound } = render(<FavoritePokemons />);
-
+    const { getByText } = render(<FavoritePokemons />);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
   });
 
-  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText, getByRole, history } = renderWithRouter(<App />);
+    const moreDetailsLink = getByText('More details');
 
-  it('Se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {});
+    userEvent.click(moreDetailsLink);
+
+    const checkPikachu = getByRole('checkbox');
+    userEvent.click(checkPikachu);
+
+    history.push('/pokemons/25');
+    const doesExistPikachu = getByText('Pikachu');
+    expect(doesExistPikachu).toBeInTheDocument();
+  });
+
+  it('Se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    global.localStorage.clear();
+    const { getByText } = render(<FavoritePokemons />);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
 });
