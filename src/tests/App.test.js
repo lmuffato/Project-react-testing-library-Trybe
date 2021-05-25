35fa6e353d533583fe2928159a61910d10cf3f1f
rafael <rafittu@outlook.com>
2021-05-25 18:02:46 -0300

    teste container de links

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testando o componente App.js', () => {
@@ -20,7 +21,25 @@ describe('Testando o componente App.js', () => {
   });
 
   it('A pagina contém links de navegação Home, About e Favorite Pokémons', () => {
-    // Escreva o teste aqui
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    const homeLink = screen.getByRole('link', {
+      name: /Home/i,
+    });
+    const aboutLink = screen.getByRole('link', {
+      name: /About/i,
+    });
+    const favoriteLink = screen.getByRole('link', {
+      name: /Favorite Pokémon/i,
+    });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoriteLink).toBeInTheDocument();
   });
 
   it('Ao clicar no link Home, é redirecionado para pagina inicial URL /', () => {
