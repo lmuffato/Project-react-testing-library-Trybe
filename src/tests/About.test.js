b89f79e189da03f6d3cd6e791e7cf939688883ee
Viegas <mvrdgs@gmail.com>
2021-05-25 17:08:13 -0300

    test 'About' component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('`About` component', () => {
+  it('Test if \'About\'\'s title is rendered', () => {
+    const { getByText } = renderWithRouter(<About />);
+    expect(getByText(/About Pokédex/i)).toBeInTheDocument();
+  });
+  it('Test \'About\'\'s image is rendered', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const pokedexImg = getByRole('img');
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
