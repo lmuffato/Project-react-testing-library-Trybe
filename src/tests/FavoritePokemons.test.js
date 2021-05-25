1a13247495a8673451c5938d39f30e1413e1844c
Lucas André <lucas.cancado89@gmail.com>
2021-05-25 16:22:46 -0300

    Requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testa FavoritePokemons component', () => {
+  it('testa a mensagem quando não ha pokemons favoritados', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const noFavorites = getByText('No favorite pokemon found');
+    expect(noFavorites).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('test component NotFound', () => {
+  it('testa a mensagem da pagina', () => {
+    const { getByRole } = render(<NotFound />);
+    const heading = getByRole('heading', {
+      name: /Page requested not found/,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('testa se a página contem a imagem', () => {
+    const { getByRole } = render(<NotFound />);
+    const img = getByRole('img', {
+      name: 'Pikachu crying because the page requested was not found',
+    });
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
