3801e8c8acb2b07061780fd071cf78056a2da00a
Felipe Muller <felipeavmuller@gmail.com>
2021-05-25 17:11:49 -0300

    Testa os textos na pagina about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Component About tests', () => {
+  it('pokedex info?', () => {
+    const { history, getByRole, getByText } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const aboutText = getByRole('heading', {
+      name: /about pokédex/i,
+    });
+    expect(aboutText).toBeInTheDocument();
+
+    const firstParag = getByText('This application simulates a Pokédex, '
+     + 'a digital encyclopedia containing all Pokémons');
+    expect(firstParag).toBeInTheDocument();
+    const secondParag = getByText('One can filter Pokémons by type, '
+    + 'and see more details for each one of them');
+    expect(secondParag).toBeInTheDocument();
+  });
+}); // describe
