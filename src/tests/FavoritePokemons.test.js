3ccf48a64929c1b26f6389d10d3ea060ea2223e8
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-05-25 17:22:34 -0300

    :rocket: Requisito 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { render } from '@testing-library/react';
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { FavoritePokemons } from '../components';
+
+describe('tests for the <FavoritePokemons> component', () => {
+  test('renders a reading with the text `Favorite pokémons`', () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={ ['/favorites'] }>
+        <FavoritePokemons />
+      </MemoryRouter>,
+    );
+    const heading = getByRole('heading', { level: 2, name: /Favorite pokémons/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('renders a paragraph with the text `No favorite pokemon found`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/favorites'] }>
+        <FavoritePokemons />
+      </MemoryRouter>,
+    );
+    const text = getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+});
