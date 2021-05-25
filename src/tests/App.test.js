66055d1d387eb96bba384d5b24cdba33292cd209
hheloisa <hhackenhaar@gmail.com>
2021-05-25 16:03:11 -0300

    Cria renderWithRouter e primeiro teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { render, screen, getByRole } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('test of the app', () => {
+  test('Rendeniza na tela', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByText('Pokédex');
+    expect(title).toBeInTheDocument();
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+/*   test('renders a reading with the text `Pokédex`', () => {
+    const { getByRole } = render(<App />);
+    const h1 = getByRole(heading);
+    expect(screen.getByRole(heading)).toBeInTheDocument();
+  }); */
+  /* test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  }); */
 });
