0ebb31bf44cabbe7f756a9cd8c40b06cf355fe50
Orlando Flores <orlando_flores@id.uff.br>
2021-05-25 17:37:13 -0300

    feat: requisito 5 e mock de pokemons

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,83 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import pokemons from './mockPokemons';
+
+describe('test the component Pokedex', () => {
+  test('has the "Encountered Pokémons" text', () => {
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const heading = getByRole('heading', {
+      name: /encountered pokémons/i,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('the first pokemon is Pikachu', () => {
+    const { getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const pikachu = getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+  test('when the Fire button is clicked only fire pokemons are rendered', () => {
+    const { getByRole, getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const fireBtn = getByRole('button', {
+      name: /fire/i,
+    });
+    userEvent.click(fireBtn);
+    const charmander = getByText(/charmander/i);
+    expect(charmander).toBeInTheDocument();
+
+    const nextPokemon = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    userEvent.click(nextPokemon);
+    const rapidash = getByText(/rapidash/i);
+    expect(rapidash).toBeInTheDocument();
+
+    userEvent.click(nextPokemon);
+    expect(charmander).toBeInTheDocument();
+  });
+  test('if the all Button is rendered', () => {
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const allBtn = getByRole('button', {
+      name: /all/i,
+    });
+    expect(allBtn).toBeInTheDocument();
+  });
+  test('if has 3 pokemon types', () => {
+    const { getAllByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const numberOfTypes = 3;
+    const types = getAllByTestId('pokemon-type-button');
+    expect(types.length).toBe(numberOfTypes);
+  });
+  test('when the all Button is clicked pikachu is rendered', () => {
+    const { getByRole, getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const allBtn = getByRole('button', {
+      name: /all/i,
+    });
+    const fireBtn = getByRole('button', {
+      name: /fire/i,
+    });
+    userEvent.click(fireBtn);
+    userEvent.click(allBtn);
+    const pikachu = getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
---
+++ src/tests/mockPokemons.js
@@ -0,0 +1,109 @@
+const pokemons = [
+  {
+    id: 25,
+    name: 'Pikachu',
+    type: 'Electric',
+    averageWeight: {
+      value: '6.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Viridian Forest',
+        map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+      },
+      {
+        location: 'Kanto Power Plant',
+        map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+  },
+  {
+    id: 65,
+    name: 'Alakazam',
+    type: 'Psychic',
+    averageWeight: {
+      value: '48.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/8/88/Spr_5b_065_m.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Alakazam_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Unova Accumula Town',
+        map: 'https://cdn2.bulbagarden.net/upload/4/44/Unova_Accumula_Town_Map.png',
+      },
+    ],
+  },
+  {
+    id: 151,
+    name: 'Mew',
+    type: 'Psychic',
+    averageWeight: {
+      value: '4.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/4/43/Spr_5b_151.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Mew_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Faraway Island',
+        map: 'https://cdn2.bulbagarden.net/upload/e/e4/Hoenn_Faraway_Island_Map.png',
+      },
+    ],
+  },
+  {
+    id: 78,
+    name: 'Rapidash',
+    type: 'Fire',
+    averageWeight: {
+      value: '95.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Route 28',
+        map: 'https://cdn2.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png',
+      },
+      {
+        location: 'Johto Mount Silver',
+        map: 'https://cdn2.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png',
+      },
+    ],
+  },
+];
+
+export default pokemons;
