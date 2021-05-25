8941b798c7eb2d1f14b1adc41c2ef1f339c7491c
Viegas <mvrdgs@gmail.com>
2021-05-25 18:00:01 -0300

    implement 'Favorite Pokemons' test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Test \'Favorite Pokémons\'', () => {
+  it('Test if \'Favorite Pokémons\' is rendered', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+  });
+
+  it('Test if \'Favorite Pokémons\' is rendered', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+  });
+
+  it('Test if \'Favorite Pokémons\' if no cards are rendered if no Pokemon is favorited',
+    () => {
+      const { getByText } = renderWithRouter(<FavoritePokemons />);
+      expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+    });
+});
