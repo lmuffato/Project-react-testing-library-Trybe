ec14777090de409b6c9caa4bad4833a4af98533f
Bruno Mendes <mendessbruno@gmail.com>
2021-05-25 16:58:19 -0300

    resolvido requisio 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('renders NotFound', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const heading = getByRole('heading', { name: /Page requested not found/i });
+  const img = getByRole('img', { name: /Pikachu crying/i });
+
+  expect(heading).toBeInTheDocument();
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
