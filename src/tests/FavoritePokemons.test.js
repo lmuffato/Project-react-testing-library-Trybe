23fa160a0bfd22bed0765d5e8d46d6a7dd791815
Felipe Muller <felipeavmuller@gmail.com>
2021-05-25 18:10:18 -0300

    Testa se aparece mensagem se nao tiver pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Favorites pokemons tests', () => {
+  it('Favorite pokemons with no favorite pokemon', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    history.push('/favorites');
+    expect(getByText('No favorite pokemon found')).toBeInTheDocument();
+  });
+});
