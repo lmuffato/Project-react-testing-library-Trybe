2ce7979f10c7418dc12f76962645e9afd53e410b
guidugaich <guidugaich.dev@gmail.com>
2021-05-25 18:25:00 -0300

    subrequisito 1 - req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,7 +4,16 @@ import { render } from '@testing-library/react';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('3. Testando componente FavoritePokemon', () => {
-  test('Verificar se a mensagem correta é mostrada quando não há favoritos', () => {});
+  test('Verificar se a mensagem correta é mostrada quando não há favoritos', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <FavoritePokemons />
+      </MemoryRouter>,
+    );
+
+    const notFoundMsg = getByText('No favorite pokemon found');
+    expect(notFoundMsg).toBeInTheDocument();
+  });
   test('Teste se é exibido todos os cards de pokémons favoritados.', () => {});
   test('Nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {});
 });
