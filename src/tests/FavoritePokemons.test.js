b76eaef774c4582ebd3fc236b670f934d403087f
defauth98 <neto.daniribeiro@gmail.com>
2021-05-25 18:12:32 -0300

    verificando o texto quando nao ha pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+describe('favorite pokemon', () => {
+  it('render a favorite pokemon', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const FarovitesLink = getByText(/Favorite Pokémons/i);
+    fireEvent.click(FarovitesLink);
+
+    const NotFoundPokemons = getByText(/No favorite pokemon found/i);
+    expect(NotFoundPokemons).toBeInTheDocument();
+  });
+});
