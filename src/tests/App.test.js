077d496b749256c18252dbfd2610f67f00f9bc52
hheloisa <hhackenhaar@gmail.com>
2021-05-25 18:08:38 -0300

    Testes em <App />, inicia testes de redirecionamento

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -16,5 +16,31 @@ describe('Test of app, render and nav', () => {
     expect(links[0]).toHaveTextContent('Home');
     expect(links[1]).toHaveTextContent('About');
     expect(links[2]).toHaveTextContent('Favorite Pokémons');
+  });
+});
+
+describe('Test of redirec', () => {
+  test('Teste se a aplicação é redirecionada para a página inicial, na URL /'
+  + 'ao clicar no link Home da barra de navegação.', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const about = getByText('Home');
+    userEvent.click(about);
+    expect(pathname).toBe('/');
+  });
+  test(`Teste se a aplicação é redirecionada para a página de About, na URL /about,
+  ao clicar no link About da barra de navegação.`, () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const favPokemons = getByText('Favorite Pokémons');
+    userEvent.click(favPokemons);
+    expect(pathname).toBe('/favorites');
+  });
+  test(`Teste se a aplicação é redirecionada para a página Not Found
+  ao entrar em uma URL desconhecida.`, () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const notFound = getByText('Not Found');
+    history.push('/xablau');
+    expect(notFound).toBeInTheDocument();
   });
 });
