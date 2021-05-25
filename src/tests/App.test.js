6ebf9313de557b300f7f9bcb2ab9705590bf8f13
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-25 16:45:44 -0300

    feat: cria testes para verificar se a pagina contem links com os textos requisitados

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +22,30 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('Teste  de redenrização da página principal da Pokédex /', () => {
+  test('vErifica se redenrizou o App', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const text = getByText('Pokédex');
+    expect(text).toBeInTheDocument();
+  });
+
+  test('O primeiro link deve possuir o texto Home', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link1 = getByText('Home');
+    expect(link1).toBeInTheDocument();
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link1 = getByText('About');
+    expect(link1).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const link1 = getByText('Favorite Pokémons');
+    expect(link1).toBeInTheDocument();
+  });
 });
