de77bad6d7e16e61554a2f570fd10832939c15a0
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-05-25 20:52:33 -0300

    feat: Requirement 01 and 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render } from 'react-dom';
+import renderWithRouter from '../../renderWithRouter';
+import App from '../App';
+import { About } from '../components';
+
+describe('renders infos about the pokedex', () => {
+  const { container, getByText, getByRole, getByAltText } = renderWithRouter(<App />);
+  userEvent.click(getByText('About'));
+
+  test('checks if h2 is rendered correctly', () => {
+    const h2 = getByRole('heading', { level: 2 });
+    expect(h2).toHaveTextContent('About Pokédex');
+
+    const img = getByAltText('Pokédex');
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+
+    const paragraphs = container.querySelectorAll('p');
+    paragraphs.forEach((parag) => expect(parag).toBeInTheDocument());
+  });
+
+/*   test('checks if the image is rendered correctly', () => {
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(img).toBeInTheDocument();
+  }); */
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -14,11 +16,23 @@ test('renders a reading with the text `Pokédex`', () => {
 });
 
 test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  const { getByText } = renderWithRouter(<App />);
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(getByText('Home')).toBeInTheDocument();
+  expect(getByText('About')).toBeInTheDocument();
+  expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+});
+
+test('redirects correctly when the links are clicked', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByText('Home'));
+  expect(history.location.pathname).toBe('/');
+  userEvent.click(getByText('About'));
+  expect(history.location.pathname).toBe('/about');
+  userEvent.click(getByText('Favorite Pokémons'));
+  expect(history.location.pathname).toBe('/favorites');
+  history.push('anything');
+  expect(getByText('Page requested not found')).toBeInTheDocument();
 });
