7ac7bd826fe22df6d55e00749eb9cb6ff30908fc
Debora Passos <deborinhapassos@gmail.com>
2021-05-25 14:35:12 -0300

    Iniciando Projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,10 +5,9 @@ import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
