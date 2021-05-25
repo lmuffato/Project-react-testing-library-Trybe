6d9818b0f88000c7092261f27852482476d81fa7
Lucas Pedroso <lepsk8@gmail.com>
2021-05-25 15:13:07 -0300

    feat: :sparkles: 'Initial Commit'
    Adding renderWithRouter.helper.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter.helper';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Requisite 1', () => {
+  test('Renders a hearding with the text `Pokédex`', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const headingPokedex = screen.getByRole('heading', { level: 1 });
+    expect(headingPokedex).toBeInTheDocument();
+    expect(headingPokedex).toHaveTextContent(/pokédex/i);
+  });
 });
