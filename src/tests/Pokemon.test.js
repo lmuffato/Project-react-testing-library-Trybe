7f6d27436edcccd3568d3196bc3bfcc21380da38
Lucas André <lucas.cancado89@gmail.com>
2021-05-26 09:53:26 -0300

    Requisito 6 FIM

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 import Pokemon from '../components/Pokemon';
 import pokemons from '../data';
 
 describe('Testing Pokemon Component', () => {
-  it('', () => {
+  it('Testa a renderização do pokemon', () => {
     const { getByText, getByRole } = render(
       <BrowserRouter>
         <Pokemon pokemon={ pokemons[0] } />
@@ -19,5 +21,26 @@ describe('Testing Pokemon Component', () => {
     expect(weight).toBeInTheDocument();
     const pokemonImg = getByRole('img', { name: 'Pikachu sprite' });
     expect(pokemonImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  it('testa se o componente possui um link para a URL pokemons/:id', () => {
+    const history = createMemoryHistory();
+    const { getByRole } = render(
+      <Router history={ history }>
+        <Pokemon pokemon={ pokemons[0] } />
+      </Router>,
+    );
+    const detailsBtn = getByRole('link', { name: 'More details' });
+    userEvent.click(detailsBtn);
+    const pathName = history.location.pathname;
+    expect(pathName).toBe('/pokemons/25');
+  });
+  it('testa se existe o icone de estrela para os pokemons favoritados', () => {
+    const { getByRole } = render(
+      <BrowserRouter>
+        <Pokemon pokemon={ pokemons[0] } isFavorite />
+      </BrowserRouter>,
+    );
+    const star = getByRole('img', { name: 'Pikachu is marked as favorite' });
+    expect(star.src).toBe('http://localhost/star-icon.svg');
   });
 });
