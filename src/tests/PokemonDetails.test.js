26299b382abc27ae5e7baa7cf50c3a21a7e15b55
Viegas <mvrdgs@gmail.com>
2021-05-26 05:51:02 -0300

    implement: Test 'Pokemon Details' page

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Test \'Pokemon Details\' page', () => {
+  it('Test if page is rendered', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', { name: /more details/i });
+    fireEvent.click(detailsLink);
+    const pageTitle = getByRole('heading', { level: 2, name: /Pikachu Details/i });
+    expect(pageTitle).toBeInTheDocument();
+    const locationsTitle = getByRole('heading', { level: 2, name: /Game Locations of Pikachu/i });
+    expect(locationsTitle).toBeInTheDocument();
+    const favoriteCheckbox = getByRole('checkbox', { name: /Pokémon favoritado\?/i });
+    expect(favoriteCheckbox.checked).toBe(false);
+  });
+});
