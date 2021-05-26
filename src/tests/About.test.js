ef70834894664d915d4e1b4de6719ea0afc218bf
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-05-26 10:39:39 -0300

    corrigindo o segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { About } from '../components';
 import renderWithRouter from './renderWithRouter';
 
-const p1 = 'This application simulates a Pokédex, a '
-+ 'digital encyclopedia containing all Pokémons';
+const p1 = 'This application simulates a Pokédex, a digital encyclopedia containing '
++ 'all Pokémons';
 const p2 = 'One can filter Pokémons by type, and see more details for each one of them';
 const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
@@ -16,12 +16,11 @@ describe('Verifica se o componente About ', () => {
   });
 
   it('contém dois parágrafos com as informações sobre a Pokédex', () => {
-    renderWithRouter(<About />);
-    const info1 = screen.getByTestId('about-paragraph1');
-    const info2 = screen.getByTestId('about-paragraph2');
-    expect(info1.tagName === 'P' && info2.tagName === 'P').toBe(true);
-    expect(info1).toHaveTextContent(p1);
-    expect(info2).toHaveTextContent(p2);
+    const { container } = renderWithRouter(<About />);
+    const paragraphs = container.querySelectorAll('p');
+    expect(paragraphs).toHaveLength(2);
+    expect(paragraphs[0]).toHaveTextContent(p1);
+    expect(paragraphs[1]).toHaveTextContent(p2);
   });
 
   it('contém informações sobre a Pokédex', () => {
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verifica se o componente FavoritePokemons ', () => {
+  it('contém um heading h2 com o texto "About Pokédex"', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const element = screen.getByRole('heading', { level: 2 });
+    expect(element).toHaveTextContent('About Pokédex');
+  });
+});
