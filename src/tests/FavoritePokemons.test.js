f1ae69c5feb51e29d4f7a79340b825f897dd94f4
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-05-25 22:57:02 -0300

    Useless call of renderWithRouter removed in FavoritePokemons.test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import { FavoritePokemons } from '../components';
 import App from '../App';
@@ -27,7 +28,7 @@ describe('Requisito 3 - Teste o componente <FavoritePokemons.js />',
     it(`Teste se é exibido na tela a mensagem No favorite pokemon found,
     se a pessoa não tiver pokémons favoritos`,
     () => {
-      const { getByText } = renderWithRouter(<FavoritePokemons />);
+      const { getByText } = render(<FavoritePokemons />);
       const message = getByText('No favorite pokemon found');
       expect(message).toBeInTheDocument();
     });
