dc1a8cd9e221119661c337b4f9cefd9e5ccb4dd2
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 16:44:50 -0300

    importando userEvent

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import Pokedex from '../components/Pokedex';
 import renderWithRouter from '../helpers/renderWithRouter';
 import pokemons from '../data';
@@ -42,8 +43,12 @@ describe('5. Testando componente <Pokedex />', () => {
   });
 
   test('é mostrado apenas um Pokémon por vez', () => {});
+
   test('a Pokédex tem os botões de filtro', () => {});
+
   test('a Pokédex contém um botão para resetar o filtro', () => {});
+
   test('é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {});
+
   test('Botão Próximo deve ser desabilitado quando a lista tiver só um pokémon', () => {});
 });
