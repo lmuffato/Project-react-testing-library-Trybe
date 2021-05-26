5ff185c215f42bb7c7dee9cdc08b93e829e87215
defauth98 <neto.daniribeiro@gmail.com>
2021-05-26 12:38:22 -0300

    testando o fluxo de favoritar

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 describe('pokemon', () => {
@@ -22,5 +22,44 @@ describe('pokemon', () => {
     expect(pokemonName).toHaveTextContent('Pikachu');
     expect(pokemonType).toHaveTextContent('Electric');
     expect(pokemonWeigth).toHaveTextContent('Average weight: 6.0 kg');
+  });
+
+  it('move do details page', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const detailsButton = getByText('More details');
+    fireEvent.click(detailsButton);
+
+    const detailHeading = getByText('Pikachu Details');
+    expect(detailHeading).toBeInTheDocument();
+  });
+
+  it('favorite a pokemon', () => {
+    const { getByText, getByAltText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const detailsButton = getByText('More details');
+    fireEvent.click(detailsButton);
+
+    const favoriteButton = getByText('Pokémon favoritado?');
+    fireEvent.click(favoriteButton);
+
+    const homeLink = getByText('Home');
+    fireEvent.click(homeLink);
+
+    const favoriteImage = getByAltText('Pikachu is marked as favorite');
+    expect(favoriteImage).toBeInTheDocument();
+    expect(favoriteImage.src).toBe('http://localhost/star-icon.svg');
+
+    const pokemonImage = getByAltText('Pikachu sprite');
+    expect(pokemonImage).toBeInTheDocument();
+    expect(pokemonImage.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
 });
