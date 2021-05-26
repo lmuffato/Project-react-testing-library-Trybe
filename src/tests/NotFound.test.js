5c05f068eb8a6575571a244712ec5872e77df4dd
Mateus Alencar <mtauws@gmail.com>
2021-05-26 16:02:19 -0300

    fix:lint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -14,14 +14,14 @@ describe('Testa a página "Not Found"', () => {
     expect(h2).toBeInTheDocument();
   });
 
-  //Source: https://github.com/tryber/sd-010-a-project-react-testing-library/blob/a4d2962898a0c0ea03dbdc83bc8aa7b28eae4b28/src/tests/NotFound.test.js
+  // Source: https://github.com/tryber/sd-010-a-project-react-testing-library/blob/a4d2962898a0c0ea03dbdc83bc8aa7b28eae4b28/src/tests/NotFound.test.js
   it('Testa se renderiza uma img', () => {
     const { getByAltText } = renderWithRouter(<NotFound />);
     const image = getByAltText('Pikachu crying because the page requested was not found');
 
     expect(image.src)
-    .toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+      .toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 
     expect(image).toBeInTheDocument();
   });
-})
+});
