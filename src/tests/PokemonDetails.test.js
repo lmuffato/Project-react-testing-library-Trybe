09f57ed0275df0703b6a8d6185eb069fb25cc4f6
Viegas <mvrdgs@gmail.com>
2021-05-26 06:15:54 -0300

    test PokemonDetails map section

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -10,9 +10,21 @@ describe('Test \'Pokemon Details\' page', () => {
     fireEvent.click(detailsLink);
     const pageTitle = getByRole('heading', { level: 2, name: /Pikachu Details/i });
     expect(pageTitle).toBeInTheDocument();
-    const locationsTitle = getByRole('heading', { level: 2, name: /Game Locations of Pikachu/i });
+    const locationsTitle = getByRole('heading', { level: 2, name: /Summary/i });
     expect(locationsTitle).toBeInTheDocument();
     const favoriteCheckbox = getByRole('checkbox', { name: /Pokémon favoritado\?/i });
     expect(favoriteCheckbox.checked).toBe(false);
+  });
+
+  it('Test if there is a map section', () => {
+    const { getByRole, getAllByAltText } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', { name: /more details/i });
+    fireEvent.click(detailsLink);
+    const locationsTitle = getByRole('heading',
+      { level: 2, name: /Game Locations of Pikachu/i });
+    expect(locationsTitle).toBeInTheDocument();
+    const locations = getAllByAltText(/Pikachu location/i);
+    expect(locations[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(locations[1].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
   });
 });
