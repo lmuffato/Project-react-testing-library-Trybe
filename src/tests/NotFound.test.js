36487094636ea0b124dc4f969eaf58cba1c92c3f
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 15:32:19 -0300

    desenvolvidos testes do notfound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,6 +3,16 @@ import NotFound from '../components/NotFound';
 import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('4. Testando componente <NotFound />', () => {
-  test('Testar se a página contém um h2 com um texto', () => {});
-  test('Testar se a página contém uma imagem', () => {});
+  test('Testar se a página contém um h2 com um texto', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const notFoundHeading = getByRole('heading', { level: 2 });
+    expect(notFoundHeading).toHaveTextContent('Page requested not found 😭');
+  });
+
+  test('Testar se a página contém uma imagem', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imgPath = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toContain(imgPath);
+  });
 });
