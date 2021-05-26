a04c883f8ab1477017d0fea53bf9f5b33de88b2b
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-05-26 17:23:07 -0300

    Requirement 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +23,85 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByRole } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const myNav = getByRole('navigation');
+  expect(myNav).toBeInTheDocument();
+});
+
+test('Testa se existe um link com o texto Home', () => {
+  const { getByRole } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const myLink = getByRole('link', {
+    name: /Home/i,
+  });
+  userEvent.click(myLink);
+
+  const homeText = getByRole('heading', {
+    name: /Encountered pokémons/i,
+    level: 2,
+  });
+
+  expect(homeText).toBeInTheDocument();
+});
+
+test('Testa se existe um link com o texto About', () => {
+  const { getByRole } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+  const myLink = getByRole('link', {
+    name: /About/i,
+  });
+  userEvent.click(myLink);
+
+  const aboutText = getByRole('heading', {
+    name: /About Pokédex/i,
+    level: 2,
+  });
+
+  expect(aboutText).toBeInTheDocument();
+});
+
+test('Testa se existe um link com o texto Favorite Pokémons', () => {
+  const { getByRole } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  const myLink = getByRole('link', {
+    name: /Favorite Pokémons/i,
+  });
+  userEvent.click(myLink);
+
+  const favoriteText = getByRole('heading', {
+    name: /Favorite pokémons/i,
+    level: 2,
+  });
+
+  expect(favoriteText).toBeInTheDocument();
+});
+
+test('Testa se o usuario e direcionado para notFound `/desconhecido`', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  history.push('/desconhecido');
+
+  const notFoundText = getByRole('heading', {
+    name: /Page requested not found Crying emoji/i,
+    level: 2,
+  });
+
+  expect(notFoundText).toBeInTheDocument();
 });
