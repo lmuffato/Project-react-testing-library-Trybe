a366e0139f5532b84096e0b22dce35e339dd85c8
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-25 22:29:04 -0300

    requisito 3 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+// import renderWithRouter from './renderWithRouter';
+
+test('Testa se há pokemons favoritos e se aparece mensagem caso não tenha', () => {
+  render(<FavoritePokemons />);
+  const noFavorite = screen.getByText('No favorite pokemon found');
+  expect(noFavorite).toBeInTheDocument();
+});
