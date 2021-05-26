0719f6657acec39f1977083bbb78c7806afe383d
MaxRudim <max.hermsdorff@gmail.com>
2021-05-26 17:00:13 -0300

    Req. 03 - talvez esteja pronto

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './RenderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Favorite Pokémmons component test', () => {
+  test('test if the "No favorite pokémon found"'
++ ' message is displayed if nothing is selected', () => {
+    const { getByText, history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/favorites');
+    const paragraph = getByText(/No favorite pokemon found/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph.tagName).toBe('P');
+  });
+
+  test('test if the all the favorite pokémon cards are displayed', () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
+    const fakePokemon = '/pokemons/25';
+    history.push(`${fakePokemon}`);
+    const favcheckbox = getByRole('checkbox');
+    userEvent.click(favcheckbox);
+    history.push('/favorites');
+    const paragraph = getByText(/Pikachu/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph.tagName).toBe('P');
+  });
+});
