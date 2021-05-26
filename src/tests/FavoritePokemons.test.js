acf3d82b5d8c7fb573b4ecaca0516ea0167b03ec
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 14:51:28 -0300

    mudando estrutura do FavoritePokemons para usar renderWithRouter

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,43 +1,32 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
 import App from '../App';
 import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('3. Testando componente FavoritePokemon', () => {
   test('Verificar se a mensagem correta é mostrada quando não há favoritos', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <FavoritePokemons />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
 
     const notFoundMsg = getByText('No favorite pokemon found');
     expect(notFoundMsg).toBeInTheDocument();
   });
 
   test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
-    const history = createMemoryHistory();
-
-    const { getByText, getByRole, getByTestId } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText, getByRole, getByTestId, history } = renderWithRouter(<App />);
 
     const moreDetails = getByText('More details');
     expect(moreDetails).toBeInTheDocument();
     userEvent.click(moreDetails);
+
+    expect(history.location.pathname).toBe('/pokemons/25');
 
     const favoriteCheckbox = getByRole('checkbox');
     expect(favoriteCheckbox).toBeInTheDocument();
     userEvent.click(favoriteCheckbox);
 
     history.push('/favorites');
-    const { pathname } = history.location;
-    expect(pathname).toBe('/favorites');
+    expect(history.location.pathname).toBe('/favorites');
 
     expect(getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
     expect(getByTestId('pokemon-type')).toHaveTextContent('Electric');
