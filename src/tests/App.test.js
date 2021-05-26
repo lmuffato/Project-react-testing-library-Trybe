e0cf038475dc63fd9a8ddf4edc03eb556148d57e
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-25 21:19:49 -0300

    Testado home

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+describe('Test App', () => {
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText('Encountered pokémons');
+    expect(home).toBeInTheDocument();
+  });
 });
