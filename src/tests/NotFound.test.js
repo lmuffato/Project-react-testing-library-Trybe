a4d2962898a0c0ea03dbdc83bc8aa7b28eae4b28
luanlsr <luanfswd@gmail.com>
2021-05-25 23:36:49 -0300

    requisito 4 finalizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verificar a página NotFound', () => {
+  it('deve renderizar um h2', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const h2 = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found Crying emoji/i,
+    });
+
+    expect(h2).toBeInTheDocument();
+  });
+  it('deve renderizar uma imagem', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(image.src)
+      .toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(image).toBeInTheDocument();
+  });
+});
