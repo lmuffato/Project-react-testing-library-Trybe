79d02e48ca1c2b6105f84fd5f29b87cae8fe5754
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-26 17:02:22 -0300

    Req 1 - Test component App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,72 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('App.js tests', () => {
+  test('Renders a heading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+    const heading = getByText('Pokédex');
+    expect(heading).toBeInTheDocument();
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('Application is redirected to Home when its Link is clicked', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const homeLink = getByText('Home');
+    fireEvent.click(homeLink);
+
+    const heading = getByText('Pokédex');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Application is redirected to About page when its Link is clicked', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const aboutLink = getByText('About');
+    fireEvent.click(aboutLink);
+
+    const heading = getByText('About Pokédex');
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Application is redirected to Favorites page when its Link is clicked', () => {
+    const { getByText, getByRole } = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const favoritesLink = getByText('Favorite Pokémons');
+    fireEvent.click(favoritesLink);
+
+    const heading = getByRole('heading', {
+      name: 'Favorite pokémons',
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
 });
