196a81ba8bbfa0dfa6407c8294c97e146c275907
Sergio Martins <sergiomos2010@gmail.com>
2021-05-26 04:46:54 -0300

    Testing: show only favorites pokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
-import data from '../data';
+import App from '../App';
 
 describe('Favorite Pokemons', () => {
-  test('No Favorite pokémon message, if there is none', () => {
+  test('Show no favorite pokémon message, if there is none', () => {
     render(<FavoritePokemons />);
     const notFound = screen.getByText(/No favorite pokemon found/i);
     expect(notFound).toBeInTheDocument();
   });
 
-  test('show all pokémons', () => {
-    renderWithRouter(<FavoritePokemons pokemons={ data } />);
-    const pokemons = screen.getAllByAltText(/is marked as favorite/i);
-    expect(pokemons.length).toBe(data.length);
+  test('show all and only favorites pokémons', () => {
+    const { history, getByRole, getAllByTestId } = renderWithRouter(<App />);
+    const addNewFavoritePokemom = (pokemonID) => {
+      history.push(`pokemons/${pokemonID}`);
+      const favoriteCheckbox = getByRole('checkbox');
+      userEvent.click(favoriteCheckbox);
+      history.push('/');
+    }; // Função tirada do projeto do Murrilo Golçalves https://github.com/tryber/sd-010-a-project-react-testing-library/pull/79/files
+    addNewFavoritePokemom('25');
+    addNewFavoritePokemom('4');
+
+    history.push('/favorites');
+    const pokemons = getAllByTestId(/pokemon-name/i);
+    expect(pokemons.length).toBe(2);
+    expect(pokemons[0].innerHTML).toBe('Pikachu');
+    expect(pokemons[1].innerHTML).toBe('Charmander');
   });
 });
