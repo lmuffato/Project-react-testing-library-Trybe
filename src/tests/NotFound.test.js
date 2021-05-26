971790d2e8237b36cfa089c2a92c9aa0ebccbafb
Sergio Martins <sergiomos2010@gmail.com>
2021-05-26 04:57:08 -0300

    Testing: Show not found message

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFount from '../components/NotFound';
+
+describe('NotFound Component', () => {
+  test('Heading is a h2 and de text is Page requested not found', () => {
+    render(<NotFount />);
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found crying emoji/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+});
