9ec90b61b7c8e423333642eb7ffd1fc5219015b7
Elisa <elisafranca.carvalho@gmail.com>
2021-05-26 18:56:16 -0300

    refactory requirement

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { within } from '@testing-library/dom';
-import { Pokedex } from '../components';
-import pokemons from '../data';
+import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Test if Pokedex component', () => {
-  const isPokemonFavoriteById = {
-    4: false,
-    10: false,
-    23: false,
-    25: false,
-    65: false,
-    78: false,
-    143: false,
-    148: false,
-    151: false,
-  };
   it('renders the text Encountered pokémons', () => {
-    const { getByRole } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getByRole } = renderWithRouter(<App />);
     const heading = getByRole('heading', {
       name: /encountered pokémons/i,
       level: 2,
@@ -29,10 +14,8 @@ describe('Test if Pokedex component', () => {
     expect(heading).toBeInTheDocument();
   });
   it('changes the page when the next pokemon button is on click', () => {
-    const { getByTestId } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getByTestId } = renderWithRouter(<App />);
+
     const button = getByTestId('next-pokemon');
     userEvent.click(button);
     const charmanderNameElement = getByTestId('pokemon-name');
@@ -42,18 +25,12 @@ describe('Test if Pokedex component', () => {
     expect(charmanderNameText).toBeInTheDocument();
   });
   it('renders one pokemon at a time', () => {
-    const { getAllByTestId } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getAllByTestId } = renderWithRouter(<App />);
     const pokemonsInThePage = getAllByTestId('pokemon-name');
     expect(pokemonsInThePage.length).toBe(1);
   });
   it('has filter buttons', () => {
-    const { getByRole, getByTestId } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getByRole, getByTestId } = renderWithRouter(<App />);
     const filterButton = getByRole('button', { name: /fire/i });
     userEvent.click(filterButton);
     const typeInThePage = getByTestId('pokemon-type');
@@ -61,10 +38,7 @@ describe('Test if Pokedex component', () => {
     expect(typeInThePageText).toBeInTheDocument();
   });
   it('cleans the filter when all button is on click', () => {
-    const { getByRole, getByTestId } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getByRole, getByTestId } = renderWithRouter(<App />);
     const allButton = getByRole('button', { name: /all/i });
     userEvent.click(allButton);
     const typeInTheDocument = getByTestId('pokemon-type');
@@ -72,20 +46,14 @@ describe('Test if Pokedex component', () => {
     expect(typeInTheDocumentText).toBeInTheDocument();
   });
   it('has one button to each type of pokemon', () => {
-    const { getAllByTestId } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getAllByTestId } = renderWithRouter(<App />);
     const typesInTheScreen = getAllByTestId('pokemon-type-button');
     const length = 7;
     expect(typesInTheScreen.length).toBe(length);
   });
   it('has the next Pokémon button disabled when'
   + 'a filtered list of Pokémon has only one Pokémon', () => {
-    const { getByRole } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ isPokemonFavoriteById }
-    />);
+    const { getByRole } = renderWithRouter(<App />);
     const nextPokemonButton = getByRole('button', { name: /Próximo pokémon/i });
     const eletricButton = getByRole('button', { name: /Electric/i });
     userEvent.click(eletricButton);
