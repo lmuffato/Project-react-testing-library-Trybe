aa8cb9b7c6c650eecdb69bef570689903b2dd775
Guilherme de Prais <guilherme.prais@gmail.com>
2021-05-26 16:08:25 -0300

    req 01: first and second test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,15 +3,33 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+describe('Requisito 1', () => {
+  it('Teste se a página principal da Pokédex é renderizada', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+  it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const home = getByRole('link', { name: /Home/i });
+    expect(home).toBeInTheDocument();
+
+    const about = getByRole('link', { name: /About/i });
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoritePokemons).toBeInTheDocument();
+
+  });
 });
