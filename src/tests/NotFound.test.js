885a85e8bd6da062eab7aae730bb2fe1125ce55d
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-26 18:14:01 -0300

    refactor(Notfound.test.js): Adiciona teste de emoji do h2.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,10 +4,12 @@ import renderWithRouter from '../helpers/renderWithRouter';
 import NotFound from '../components/NotFound';
 
 describe('render elements', () => {
-  it('render header with text Page request not found', () => {
+  it('render header with text Page request not found and emoji', () => {
     renderWithRouter(<NotFound />);
     const heading = screen
       .getByRole('heading', { level: 2, name: /page requested not found/i });
     expect(heading).toBeInTheDocument();
+    const emoji = screen.getByRole('img', { name: /crying emoji/i });
+    expect(emoji).toBeInTheDocument();
   });
 });
