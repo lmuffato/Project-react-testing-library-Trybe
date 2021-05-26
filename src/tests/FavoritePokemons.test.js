d68b6c22de12ee15f4fe5687c15e0c042f9c867c
Stanrley Barbosa <3011stan@gmail.com>
2021-05-26 16:49:12 -0300

    Requisito 3: Testando componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('test <FavoritePokemons />', () => {
+  it('test message when there are no favorite pokemon', () => {
+    const { getByText } = renderWithRouter(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ [] } />
+      </MemoryRouter>,
+    );
+
+    const message = getByText(/No favorite pokemon found/i);
+    expect(message).toBeInTheDocument();
+  });
+});
