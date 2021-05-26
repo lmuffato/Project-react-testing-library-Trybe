a2f270402cb4879f67ae7815d3e683e4e84c8ed6
hheloisa <hhackenhaar@gmail.com>
2021-05-26 20:39:24 -0300

    testa se o pokemon favoritado aparace na tela de favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import { FavoritePokemons } from '../components';
+import App from '../App';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   test('testa se a página contém titulo "Favorite pokémons', () => {
@@ -14,5 +15,19 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
     renderWithRouter(<FavoritePokemons />);
     const noPokemons = screen.getByText('No favorite pokemon found');
     expect(noPokemons).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    const favoritePokemons = links[2];
+    const moreDetails = links[3];
+    userEvent.click(moreDetails);
+    const favorite = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favorite);
+    userEvent.click(favoritePokemons);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
   });
 });
