1d70b4f01661840efed66e64170cdb1dd8ca63ab
Sergio Martins <sergiomos2010@gmail.com>
2021-05-26 14:53:02 -0300

    Testing: There is filter button on pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Pokedex', () => {
+  const pokemonTestID = 'pokemon-name';
+
   test('Title is Encountered pokémons', () => {
     renderWithRouter(<App />);
     const heading = screen.getByRole('heading', {
@@ -21,7 +23,7 @@ describe('Pokedex', () => {
       name: /próximo pokémon/i,
     });
     userEvent.click(nextPokemonBtn);
-    const pokemom = screen.getByTestId('pokemon-name');
+    const pokemom = screen.getByTestId(pokemonTestID);
     expect(pokemom.innerHTML).toBe('Charmander');
 
     userEvent.click(nextPokemonBtn);
@@ -35,8 +37,8 @@ describe('Pokedex', () => {
       name: /próximo pokémon/i,
     });
 
-    const pokemom = screen.getByTestId('pokemon-name');
-    expect(pokemom.innerHTML).toBe('Pikachu');
+    const pokemon = screen.getByTestId(pokemonTestID);
+    expect(pokemon.innerHTML).toBe('Pikachu');
 
     userEvent.click(nextPokemonBtn);
     userEvent.click(nextPokemonBtn);
@@ -47,8 +49,30 @@ describe('Pokedex', () => {
     userEvent.click(nextPokemonBtn);
     userEvent.click(nextPokemonBtn);
 
-    expect(pokemom.innerHTML).toBe('Dragonair');
+    expect(pokemon.innerHTML).toBe('Dragonair');
     userEvent.click(nextPokemonBtn);
-    expect(pokemom.innerHTML).toBe('Pikachu');
+    expect(pokemon.innerHTML).toBe('Pikachu');
+  });
+
+  test('There is only one pokemon at time', () => {
+    renderWithRouter(<App />);
+    const pokemon = screen.getAllByTestId(pokemonTestID);
+    const nextPokemonBtn = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+
+    expect(pokemon.length).toBe(1);
+    userEvent.click(nextPokemonBtn);
+    expect(pokemon.length).toBe(1);
+  });
+
+  test('There are type filter buttons', () => {
+    renderWithRouter(<App />);
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+    const typesAmount = 7;
+    expect(filterButtons).toHaveLength(typesAmount);
+    expect(filterButtons[0].innerHTML).toBe('Electric');
+    expect(filterButtons[5].innerHTML).toBe('Normal');
+    expect(filterButtons[0]).toBe('Normal');
   });
 });
