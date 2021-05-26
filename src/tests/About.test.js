1cdc4a81099f66c9313f222b437b3cd9fd4e904c
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-05-26 18:27:57 -0300

    Requisito 2 - tentativa 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { About } from '../components';
-// import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 
 test('Testa se página contém um heading com o texto About Pokédex', () => {
   const { getByRole } = renderWithRouter(<About />);
-
   const heading = getByRole('heading', { name: /about pokédex/i });
   expect(heading).toBeInTheDocument();
 });
 
 test('Testa se página contém dois parágrafos', () => {
-  const { getAllByRole } = renderWithRouter(<About />);
-
-  const paragraphs = getAllByRole('paragraph');
-  expect(paragraphs.length).toBe(2);
+  const { getByText } = renderWithRouter(<About />);
+  const p1 = getByText('This application simulates a Pokédex, '
+  + 'a digital encyclopedia containing all Pokémons');
+  expect(p1).toBeInTheDocument();
+  const p2 = getByText('One can filter Pokémons by type, '
+  + 'and see more details for each one of them');
+  expect(p2).toBeInTheDocument();
 });
 
 test('Testa se a página contém a imagem de uma Pokédex', () => {
   const { getByRole } = renderWithRouter(<About />);
-
   const img = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
   const image = getByRole('img');
   expect(image.src).toBe(img);
