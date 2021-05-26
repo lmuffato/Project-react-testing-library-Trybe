a9562f1c60723f094a098db6c79bf61bc1ad8d6c
Vinicios <soicinivt@gmail.com>
2021-05-26 18:24:24 -0300

    Test if page contain information about pokedex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Tests in About.js ', () => {
+  it('Page contains informations of pokedex', () => {
+    const lintFault1 = 'This application simulates a Pokédex, a digital encyclopedia';
+    const lintFault2 = 'containing all Pokémons';
+    const informationText1 = `${lintFault1} ${lintFault2}`;
+    const lintFault3 = 'One can filter Pokémons by type, and see more details';
+    const lintFault4 = 'for each one of them';
+    const informationText2 = `${lintFault3} ${lintFault4}`;
+    render(<About />);
+    const pokedexInformation1 = screen.getByText(informationText1);
+    const pokedexInformation2 = screen.getByText(informationText2);
+    expect(pokedexInformation1 && pokedexInformation2).toBeInTheDocument();
+  });
+});
