4a6c5b434f289e84bd93891224edbbb02d07691f
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 15:31:53 -0300

    imports para teste do NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../helpers/renderWithRouter';
+
 describe('4. Testando componente <NotFound />', () => {
   test('Testar se a página contém um h2 com um texto', () => {});
   test('Testar se a página contém uma imagem', () => {});
