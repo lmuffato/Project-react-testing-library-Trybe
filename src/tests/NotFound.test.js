75f18a02bb8e7ba94dfd4dc5283ed0bc6984eae5
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-26 19:59:49 -0300

    feat: Requirement04,NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Test component NotFound', () => {
+  it('Test the component h2 have text Not Found', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const notFound = getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('test image render', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imageAlt = getByAltText('Pikachu crying'
+    + ' because the page requested was not found');
+    const image = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(imageAlt).toHaveAttribute('src', image);
+
+  });
+});
