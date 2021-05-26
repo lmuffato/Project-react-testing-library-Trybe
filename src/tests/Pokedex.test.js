77372153a700f1f31d3eea42c087f31b59d4e5a1
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-05-26 19:27:31 -0300

    Desafio 5 passou mas falta um teste

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,7 +4,6 @@ import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-// import Pokedex from '../components/Pokedex';
 
 const lastPokemon = 7;
 const nextPokemonTitle = 'Próximo pokémon';
@@ -20,61 +19,6 @@ const renderWithRouter = (component) => {
     history,
   });
 };
-
-// const pokemonMock = [
-//   {
-//     id: 25,
-//     name: 'PikaMock',
-//     type: 'Electric',
-//     averageWeight: {
-//       value: '6.0',
-//       measurementUnit: 'kg',
-//     },
-//     image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
-//     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
-//     foundAt: [
-//       {
-//         location: 'Kanto Viridian Forest',
-//         map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
-//       },
-//       {
-//         location: 'Kanto Power Plant',
-//         map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
-//       },
-//     ],
-//     summary: 'This intellig electricity to make them tender enough to eat.',
-//   },
-//   {
-//     id: 4,
-//     name: 'CharMock',
-//     type: 'Fire',
-//     averageWeight: {
-//       value: '8.5',
-//       measurementUnit: 'kg',
-//     },
-//     image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
-//     moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
-//     foundAt: [
-//       {
-//         location: 'Alola Route 3',
-//         map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
-//       },
-//       {
-//         location: 'Kanto Route 3',
-//         map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
-//       },
-//       {
-//         location: 'Kanto Route 4',
-//         map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
-//       },
-//       {
-//         location: 'Kanto Rock Tunnel',
-//         map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
-//       },
-//     ],
-//     summary: 'The flame on its force. If it is weak, the flame also burns weakly.',
-//   },
-// ];
 
 describe('5. Teste o componente <Pokedex.js />', () => {
   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
@@ -142,12 +86,13 @@ describe('5. Teste o componente <Pokedex.js />', () => {
   it('Filtros dinamicos:', () => {
     // const { getByRole, getByAltText } = renderWithRouter(<App />);
     // Os botões de filtragem devem ser dinâmicos;
-    // const { getAllByRole } = renderWithRouter(<App />);
-    // const button = getAllByRole('button');
 
     // Deve existir um botão de filtragem para cada tipo de Pokémon disponível nos dados, sem repetição. Ou seja, a sua Pokédex deve possuir pokémons do tipo Fire, Psychic, Electric, Bug, Poison, Dragon e Normal;
 
     // Deve ser mostrado como opção de filtro, um botão para cada um dos tipos. Além disso, o botão All precisa estar sempre visível.
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const buttons = getAllByTestId('pokemon-type-button');
+    expect(buttons).toHaveLength(lastPokemon);
   });
 
   it('Desabilita botao Proximo Pokemon.', () => {
