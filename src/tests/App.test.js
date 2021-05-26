1f4166646fad68a90918f2e9d2a208a12673d1f5
Stanrley Barbosa <3011stan@gmail.com>
2021-05-26 11:21:38 -0300

    Verificando se os links da pagina inicial existem

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,23 +2,42 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
+// import userEvent from '@testing-library/user-event';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Test App', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('checks: home, about and fav pokemons (and if the links work)', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+
+    const linkAbout = getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+
+    const linkFav = getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFav).toBeInTheDocument();
+  });
 });
