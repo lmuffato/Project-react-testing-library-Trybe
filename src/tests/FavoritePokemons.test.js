a3f4c8143a5fa314d147d26bfa30b145712700ea
Mateus Alencar <mtauws@gmail.com>
2021-05-26 15:36:09 -0300

    Test the <FavoritePokemons.js / component.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemon from '../components/FavoritePokemons';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testa FavoritePokemons', () => {
+  it('Testa se renderiza "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemon />);
+    const notFavorite = getByText('No favorite pokemon found');
+    expect(notFavorite).toBeInTheDocument();
+  });
+});
