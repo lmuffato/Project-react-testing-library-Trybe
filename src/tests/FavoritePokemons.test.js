74b3c1ed8f5cd5129b205b71ab0ad96656e55120
madruga665 <madruga665@hotmail.com>
2021-05-25 22:09:26 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testes do componente FavoritePokemons', () => {
+  test('Teste se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const noPokemonFound = getByText(/No favorite pokemon found/i);
+
+    expect(noPokemonFound).toBeInTheDocument();
+  });
+});
