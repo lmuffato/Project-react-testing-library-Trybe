9397abbd1cf6b4eba7aaed6d653679b818bc6fac
LucasMLara <lucasml@live.com>
2021-05-26 16:36:50 -0300

    test: :white_check_mark: FavoritePokemons Tests Implemented

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('"Favorite Pokémons" page Tests', () => {
+  test('Heading', () => {
+    const { getByText, getByRole } = renderWithRouter(<FavoritePokemons />);
+    const header = getByRole('heading', {
+      level: 2,
+      name: /favorite pokémons/i,
+    });
+    expect(header).toBeInTheDocument();
+    const paragraph = getByText('No favorite pokemon found');
+    expect(paragraph).toBeInTheDocument();
+  });
+});
+
+// https://testing-library.com/docs/queries/about/ getByRole and getByText
