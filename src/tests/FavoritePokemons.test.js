f706f902abafaece279fbd6bf3c5548544d7b8d5
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-26 15:42:00 -0300

    feat(FavoritePokemons.test.js): Testa quando nenhum pokemon e favoritado se aparece o texto (No favorite pokemon found)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Tests whether the information about the favorites pokemons is rendered', () => {
+  it('renders the text (No favorite pokemon found) if no pokemon is favored.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const heading = screen.getByRole('heading', { name: /favorite pokémons/i });
+    expect(heading).toBeInTheDocument();
+
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+});
