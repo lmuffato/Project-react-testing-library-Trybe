3789ec0497ba701a07acd1e3da169e351206a677
Gabriel Ferreira <glucas709@gmail.com>
2021-05-26 16:10:22 -0300

    Requiriment 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requirement 02 - Testing About', () => {
+  it('should show information about pokedex', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const aboutInfo = getByText(/This application simulates a Pokédex/);
+    expect(aboutInfo).toBeInTheDocument();
+  });
+  it('the About page should be rendered', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading');
+    expect(heading).toBeInTheDocument();
+    expect(heading.textContent).toBe('About Pokédex');
+  });
+  it('should contain an image of pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
