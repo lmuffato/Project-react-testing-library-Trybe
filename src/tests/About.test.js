9af57070d3b068f5e9588675ce8f8059ef8dcc1b
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-26 17:55:49 -0300

    feat: Requirement01

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,10 +1,1 @@
-const { render } = require("@testing-library/react");
-import App from '../App'
-import about from  '../components/About';
-
-describe('about tests', () => {
-  test('Teste se a página principal da Pokédex é renderizada ao carregar a aplicação no caminho de URL /.', () => {
-    const { getByRole } = render(<About />);
-  });
-})
-
+test('', () => {});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Teste about', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(
+      <App />,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se a página principal da Pokédex é'
+      + ' renderizada ao carregar a aplicação no caminho de URL /', () => {
+    const { history, getByRole } = renderWithRouter(
+      <App />,
+    );
+    expect(history.location.pathname).toBe('/');
+    expect(getByRole('heading', { name: /encountered pokémons/i })).toBeInTheDocument();
+    expect(getByRole('link', {
+      name: /home/i,
+    })).toBeInTheDocument();
+    expect(getByRole('link', {
+      name: /about/i,
+    })).toBeInTheDocument();
+    expect(getByRole('link', {
+      name: /favorite pokémons/i,
+    })).toBeInTheDocument();
+  });
 });
