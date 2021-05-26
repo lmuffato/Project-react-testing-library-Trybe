1b0777c7fd0117f18748663761f5466a06a3c60a
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-26 19:09:13 -0300

    feat(Pokedex.test.js): Testa se ao clicar no botao, o proximo pokemon e mostrado e ao chegar no ultimo, mostra o primeiro pokemon ao clicar no botao.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 import Pokedex from '../components/Pokedex';
@@ -10,5 +11,36 @@ describe('', () => {
     const heading = screen
       .getByRole('heading', { level: 2, name: /Encountered Pokémons/i });
     expect(heading).toBeInTheDocument();
+  });
+  it('rendering next pokemon, when clicking on the button', () => {
+    renderWithRouter(<App />);
+    const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(nextPokemonButton);
+    let pokemon = screen.getByText(/Charmander/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Caterpie/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Ekans/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Alakazam/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Mew/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Rapidash/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Snorlax/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Dragonair/i);
+    expect(pokemon).toBeInTheDocument();
+    userEvent.click(nextPokemonButton);
+    pokemon = screen.getByText(/Pikachu/i);
+    expect(pokemon).toBeInTheDocument();
   });
 });
