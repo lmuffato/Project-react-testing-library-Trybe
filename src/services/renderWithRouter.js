c182f2a6a11a0766c30038cad07a4ce31de62ccd
LucasMLara <lucasml@live.com>
2021-05-26 11:44:30 -0300

    fix: :bug: renaming router to Router.
    my bad =/

--- src/services/renderWithRouter.js
+++ src/services/renderWithRouter.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import React from 'react';
-import { router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 function renderWithRouter(component) {
   const historyMock = createMemoryHistory();
