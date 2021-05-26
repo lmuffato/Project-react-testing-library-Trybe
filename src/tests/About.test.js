b32bf2dc1f42b45a62bfb5e01e757d67b7d38a34
hheloisa <hhackenhaar@gmail.com>
2021-05-26 19:59:15 -0300

    Inicia resoluçoes de testes do componente <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,9 +5,20 @@ import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
 
 describe('Testa o componente About', () => {
-  test('Testa se a página contém as infos sobre a Pokédex', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const textPokedex = screen.getByText(/This application simulates a Pokédex/);
+    expect(textPokedex).toBeInTheDocument();
+  });
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
     renderWithRouter(<About />);
     const aboutPokedex = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
     expect(aboutPokedex).toBeInTheDocument();
+  });
+  test('', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toHaveAttribute('alt', 'Pokédex');
   });
 });
