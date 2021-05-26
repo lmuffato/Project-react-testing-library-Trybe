f0616f2de9db55ea748284aa49180618da7e8b9b
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-05-26 12:37:53 -0300

    feat: tests of requirement 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,59 @@
+import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Test <App>', () => {
+  test('Test index page', () => {
+    const { history: { location: { pathname } } } = renderWithRouter(<App />);
+    expect(pathname).toBe('/');
+  });
+  test('Test not found page', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    history.push('/favpokemonster');
+    const notFound = getByText(/not found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});
+
+describe('Test nav links', () => {
+  test('Test if links is in the document', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const homeLink = getByRole('link', {
+      name: /home/i,
+    });
+    const aboutLink = getByRole('link', {
+      name: /about/i,
+    });
+    const favPkmnLink = getByRole('link', {
+      name: /favorite/i,
+    });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favPkmnLink).toBeInTheDocument();
+  });
+});
+
+describe('Test links redirection', () => {
+  test('Clicking on home link', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const homeLink = getByText(/home/i);
+    userEvent.click(homeLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  test('Clicking on about link', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const aboutLink = getByText(/about/i);
+    userEvent.click(aboutLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  test('Clicking on favorite pokemons link', () => {
+    const { history, getByText } = renderWithRouter(<App />);
+    const favLink = getByText(/favorite/i);
+    userEvent.click(favLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });
