51fd9a17427d8b58aa1359f382e3b3a2da562345
Lucas André <lucas.cancado89@gmail.com>
2021-05-25 22:47:56 -0300

    requisit 6 inicio

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+describe('Testing Pokemon Component', () => {
+  it('', () => {
+    const { getByText, getByRole } = render(
+      <BrowserRouter>
+        <Pokemon pokemon={ pokemons[0] } />
+      </BrowserRouter>,
+    );
+    const pikachu = getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+    const type = getByText('Electric');
+    expect(type).toBeInTheDocument();
+    const weight = getByText('Average weight: 6.0 kg');
+    expect(weight).toBeInTheDocument();
+    const pokemonImg = getByRole('img', { name: 'Pikachu sprite'});
+    expect(pokemonImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
