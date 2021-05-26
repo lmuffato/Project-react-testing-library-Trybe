d9ed088ad886b70a777e3b76c0b999de1a261216
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-26 14:51:07 -0300

    feat(About.test.js): Testa se e renderizado os dois paragrafos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -9,4 +9,18 @@ describe('Tests whether the information about the pokedex is rendered', () => {
     const heading = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
     expect(heading).toBeInTheDocument();
   });
+
+  it('Renders paragraphs', () => {
+    renderWithRouter(<About />);
+    const text1Part1 = 'This application simulates a Pokédex, ';
+    const text1Part2 = 'a digital encyclopedia containing all Pokémons';
+    const text2Part1 = 'One can filter Pokémons by type, ';
+    const text2Part2 = 'and see more details for each one of them';
+    const text1Regex = new RegExp(`${text1Part1}${text1Part2}`, 'i');
+    const text2Regex = new RegExp(`${text2Part1}${text2Part2}`, 'i');
+    const paragraphy1 = screen.getByText(text1Regex);
+    const paragraphy2 = screen.getByText(text2Regex);
+    expect(paragraphy1).toBeInTheDocument();
+    expect(paragraphy2).toBeInTheDocument();
+  });
 });
