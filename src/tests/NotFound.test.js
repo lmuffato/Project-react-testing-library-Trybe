c3eee6421c095872d98dac13a1bba6c1fdbf0b0e
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-26 17:58:52 -0300

    feat(Notfound.js): Testa se h2 e renderizado com o texto Page requested not found.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('render elements', () => {
+  it('render header with text Page request not found', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen
+      .getByRole('heading', { level: 2, name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
