176a62dd306fb8f7810c4f7747bcdb109b7b6db4
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-05-26 13:58:45 -0300

    feat: Requirement 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../../renderWithRouter';
+import App from '../App';
+
+describe('testing NotFound component', () => {
+  test('contain a heading h2 with the text "Page requested not found 😭"', () => {
+    const { getByText, getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/anything');
+
+    expect(history.location.pathname).toBe('/anything');
+
+    const heading2 = getByRole('heading', { level: 2 });
+    expect(heading2).toBeInTheDocument();
+    expect(heading2.textContent).toBe('Page requested not found 😭');
+  });
+
+  test('shows correct image"', () => {
+    const { getByAltText, getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/anything');
+
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
