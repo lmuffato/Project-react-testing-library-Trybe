181f8dab21efac54f0eb7c74425b2923231d966b
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-05-26 16:54:54 -0300

    1. Teste o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -21,4 +23,60 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('testa se há um menu fixo, com links pra Home, About e Favorite Pokemons', () => {
+  const { getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const navigation = getByRole('navigation');
+  expect(navigation).toBeInTheDocument();
+
+  const navHome = getByRole('link', { name: /home/i });
+  expect(navHome).toBeInTheDocument();
+
+  const navAbout = getByRole('link', { name: /about/i });
+  expect(navAbout).toBeInTheDocument();
+
+  const navFavorite = getByRole('link', { name: /favorite pokémons/i });
+  expect(navFavorite).toBeInTheDocument();
+});
+
+test('Ao clicar em Home é redirecionado para "/"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /home/i }));
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/');
+});
+
+test('Ao clicar em About é redirecionado para "/about"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /about/i }));
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/about');
+});
+
+test('Ao clicar em Pokémons favoritados é redirecionado para "/favorites"', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /favorite pokémons/i }));
+
+  const { pathname } = history.location;
+  expect(pathname).toBe('/favorites');
+});
+
+test('Ao clicar em uma URL desconhecida é redirecionado para a página Not Found', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  history.push('/pagina-que-nao-existe');
+
+  const notFound = getByText('Page requested not found');
+  expect(notFound).toBeInTheDocument();
 });
---
+++ src/tests/renderWithRouter.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+function renderWithRouter(component) {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>), history,
+  });
+}
+
+export default renderWithRouter;
