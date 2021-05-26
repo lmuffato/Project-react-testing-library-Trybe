f1a91480bb7e5ac6b47c1b2c00e521a6915c0460
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-05-26 14:54:37 -0300

    feat: teste de rederizaçao da pagina

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+test('', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const heading = getByText('Page requested not found');
+
+  expect(heading).toBeInTheDocument();
+});
+
+test('', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const image = getByAltText('Pikachu crying because the page requested was not found');
+
+  expect(image).toBeInTheDocument();
+//   expect(image).toHaveAttribute('img', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
