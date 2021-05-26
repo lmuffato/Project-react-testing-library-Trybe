ccadc38158dd17f88e61b8354352257b34cd00af
Gabriel Ferreira <glucas709@gmail.com>
2021-05-26 18:44:58 -0300

    Requiriment 4

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -9,6 +9,7 @@ const NotFound = () => (
     </h2>
     <img
       className="not-found-image"
+      data-testid="not-found-image"
       src={`https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`}
       alt="Pikachu crying because the page requested was not found"
     />
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Requiriment 04 - Testing a NotFound', () => {
+  it('should should contain a heading h2', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', { level: 2 });
+    expect(heading).toBeInTheDocument();
+    expect(heading.textContent).toBe('Page requested not found 😭');
+  });
+  it('should show an image', () => {
+    const { getByTestId } = renderWithRouter(<NotFound />);
+    const image = getByTestId('not-found-image');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
