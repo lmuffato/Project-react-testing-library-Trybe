a87ab975fd2bd5935d3db3ef225ec28f6d8ba847
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-05-26 18:52:13 -0300

    Req4: Tests notfound component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByText, getByRole } = render(<NotFound />);
+  expect(getByRole('heading')).toBeInTheDocument();
+  expect(getByText('Page requested not found')).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem', () => {
+  const { getByAltText } = render(<NotFound />);
+  const imagem = getByAltText('Pikachu crying because the page requested was not found');
+  expect(imagem).toBeInTheDocument();
+  expect(imagem.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
