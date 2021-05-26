0670e9790169fca27536a51c69c3f63cbdde37c7
LucasMLara <lucasml@live.com>
2021-05-26 11:45:04 -0300

    docs: :art: adding Describe on App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,27 +3,29 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText, getByRole } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('LandingPage render tests ', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText, getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
 
-  const homeLink = getByRole('link', {
-    name: 'Home',
-  });
-  expect(homeLink).toBeInTheDocument();
+    const homeLink = getByRole('link', {
+      name: 'Home',
+    });
+    expect(homeLink).toBeInTheDocument();
 
-  const aboutLink = getByRole('link', {
-    name: 'About',
-  });
-  expect(aboutLink).toBeInTheDocument();
+    const aboutLink = getByRole('link', {
+      name: 'About',
+    });
+    expect(aboutLink).toBeInTheDocument();
 
-  const favPkmonLink = getByRole('link', {
-    name: 'Favorite Pokémons',
+    const favPkmonLink = getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(favPkmonLink).toBeInTheDocument();
   });
-  expect(favPkmonLink).toBeInTheDocument();
 });
