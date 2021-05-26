35793aed5fb11627696f15ece06278a92be7e199
LucasMLara <lucasml@live.com>
2021-05-26 15:25:16 -0300

    test: :white_check_mark: About Test Implement
    Checking all paragraphs and image on screen

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,8 +3,34 @@ import About from '../components/About';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('"About" page Tests', () => {
-  test('Header', () => {});
-  test('First Paragraph', () => {});
-  test('Second Paragraph', () => {});
-  test('Pokedex Image', () => {});
+  test('Header', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const pageHeader = getByRole('heading', {
+      level: 2,
+      name: /about pokédex/i,
+    });
+    expect(pageHeader).toBeInTheDocument();
+  });
+  test('First Paragraph', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const breakedSentence = getByText('This application simulates a Pokédex,'
+    + ' a digital encyclopedia containing all Pokémons');
+    expect(breakedSentence).toBeInTheDocument();
+  });
+
+  test('Second Paragraph', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const breakedSentence2 = getByText('One can filter Pokémons by type,'
+    + ' and see more details for each one of them');
+    expect(breakedSentence2).toBeInTheDocument();
+  });
+  test('Pokedex Image', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    // https://testing-library.com/docs/queries/about Priority #2 - Semantic Queries
+    const pokedexImg = getByAltText('Pokédex');
+
+    expect(pokedexImg.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(pokedexImg).toBeInTheDocument();
+  });
 });
