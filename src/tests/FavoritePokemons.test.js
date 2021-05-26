c686d8c2b7450594bffc4361c56488707b03d5e7
mallmann02 <leomallmann020302@gmail.com>
2021-05-26 20:03:38 -0300

    feat: Complete test on Component FavoritePokemons.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
+
+const pokemonsIds = pokemons.map(({id}) => id);
+
+test('The page starts with the'
++ 'text "No favorite pokemon found"', () => {
+  render(<FavoritePokemons />);
+
+  const notFoundText = 'No favorite pokemon found'; 
+  const paragraphNotFound = screen.getByText(notFoundText);
+  expect(paragraphNotFound).toBeInTheDocument();
+});
+
+test('Show all the favorited Pokemons', () => {
+  const historyMock = createMemoryHistory();
+  render(
+    <Router history={ historyMock }>
+      <App />
+    </Router>
+  );
+
+  const favoritePokemonById = (id) => {
+    historyMock.push(`/pokemons/${id}`);
+    const bookmarkButton = screen.getByRole('checkbox');
+    userEvent.click(bookmarkButton);
+  };
+
+  favoritePokemonById('25');
+  favoritePokemonById('4');
+
+  historyMock.push('/favorites');
+  const pokemonCards = document.querySelectorAll('.favorite-pokemon')
+  expect(pokemonCards.length).toEqual(2);
+});
