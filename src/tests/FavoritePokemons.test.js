155826033dbc71ef940d85723bfbbc4ce3fb9ff3
Iago de Paula <iago.pferreiravr@gmail.com>
2021-05-26 10:57:37 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('renders the favorites page', () => {
+  it(`verify if is displayed the message "No favorite pokemon found",
+    if the person doesn't have favorite pokemons`, () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noFavoritePokemon = getByText('No favorite pokemon found');
+
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
+
+  it('verify if all pokemon cards are displayed', () => {
+    const { getAllByTestId } = renderWithRouter(<FavoritePokemons
+      pokemons={ pokemons }
+    />);
+    const favoritesPokemons = getAllByTestId('pokemon-name');
+
+    expect(favoritesPokemons.length).toBe(pokemons.length);
+  });
+
+  it('verify if no card is rendered', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const noFavoritesPokemons = getByText('No favorite pokemon found');
+    // const storage = localStorage.getItem('favoritePokemonIds');
+
+    expect(noFavoritesPokemons).toBeInTheDocument();
+  });
+});
