d7c28b9840d962065bb50e345c8422e28fdbb715
Stanrley Barbosa <3011stan@gmail.com>
2021-05-26 20:41:40 -0300

    Requisito 5 completo: Teste componente Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+// import { MemoryRouter } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/renderWithRouter';
 import { Pokedex } from '../components';
 import pokemons from '../data';
 
 describe('test <Pokedex />', () => {
+  const qtyButtons = 7;
   it('test text h2', () => {
     // console.log(pokemons[0].id);
     const { getByRole } = renderWithRouter(
       <Pokedex
-        pokemons={pokemons}
+        pokemons={ pokemons }
         isPokemonFavoriteById={ pokemons[0] }
-      />
+      />,
     );
 
     const phraseInPokedex = getByRole('heading', {
@@ -25,13 +26,66 @@ describe('test <Pokedex />', () => {
   it('test next pokemon button', () => {
     const { getByText } = renderWithRouter(
       <Pokedex
-        pokemons={pokemons}
+        pokemons={ pokemons }
         isPokemonFavoriteById={ pokemons[0] }
-      />
+      />,
     );
     const btnNext = getByText('Próximo pokémon');
     userEvent.click(btnNext); // clicando em "Proximo pokemon"
     const charmander = getByText('Charmander');
     expect(charmander).toBeInTheDocument(); // verificando se o prox é exibido.
+  });
+
+  it('test if only pokemon is shown at a time', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ pokemons[0] }
+      />,
+    );
+
+    const qtyPokemonName = getAllByTestId('pokemon-name').length;
+    expect(qtyPokemonName).toBe(1); // verifica se há somente 1 pokemon renderizado
+  });
+
+  it('test whether the filter buttons exist', () => {
+    const { getAllByTestId } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ pokemons[0] }
+      />,
+    );
+
+    const filtersButton = getAllByTestId('pokemon-type-button');
+    expect(filtersButton.length).toBe(qtyButtons);
+  });
+
+  it('test if the Pokedéx contains the all button', () => {
+    const { getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ pokemons[0] }
+      />,
+    );
+    const btnAll = getByText('All');
+    userEvent.click(btnAll);
+    const verifyPokemon = getByText('Pikachu');
+    expect(verifyPokemon).toBeInTheDocument();
+  });
+
+  it('test if the next button is disable when necessary', () => {
+    const { getByRole, getByText } = renderWithRouter(
+      <Pokedex
+        pokemons={ pokemons }
+        isPokemonFavoriteById={ pokemons[0] }
+      />,
+    );
+
+    const btnElectric = getByRole('button', {
+      name: 'Electric',
+    });
+    userEvent.click(btnElectric);
+    const btnNext = getByText('Próximo pokémon');
+    expect(btnNext).toBeDisabled();
   });
 });
