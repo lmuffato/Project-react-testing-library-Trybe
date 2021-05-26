9909095ce58ded8016ad33687f44133e9b39b8ab
phlopes <phlopes93@gmail.com>
2021-05-26 14:47:39 -0300

    teste component FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoritePokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavoritePokemon).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    // implementar  test
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    // implementar test
+  });
+});
