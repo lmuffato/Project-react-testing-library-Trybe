3fc816d63cb65a13696c596c60d0b834104b50ad
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 14:51:07 -0300

    mudança de estrutura no App para usar renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createMemoryHistory } from 'history';
 import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('1. Testando componente <App />', () => {
   test('página principal da Pokédex é renderizada ao carregar a aplicação em /`', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App />);
     const heading = getByText(/Encountered pokémons/i);
     expect(heading).toBeInTheDocument();
   });
 
   test('topo da aplicação contém um conjunto fixo de links de navegação', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App />);
 
     const homeLink = getByText('Home');
     expect(homeLink).toBeInTheDocument();
@@ -34,11 +25,7 @@ describe('1. Testando componente <App />', () => {
   });
 
   test('o app é redirecionado para / ao clicar em Home', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App />);
 
     const homeLink = getByText('Home');
     userEvent.click(homeLink);
@@ -47,11 +34,7 @@ describe('1. Testando componente <App />', () => {
   });
 
   test('o app é redirecionado para /about ao clicar em About', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App />);
 
     const aboutLink = getByText('About');
     userEvent.click(aboutLink);
@@ -60,11 +43,7 @@ describe('1. Testando componente <App />', () => {
   });
 
   test('o app é redirecionado para /favorites ao clicar em Favorite Pokémons', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<App />);
 
     const favLink = getByText(/Favorite Pokémons/i);
     userEvent.click(favLink);
@@ -73,11 +52,7 @@ describe('1. Testando componente <App />', () => {
   });
 
   test('o app é redirecionado para Not Found ao entrar em uma URL desconhecida', () => {
-    const { getByRole } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByRole } = renderWithRouter(<App />);
 
     const history = createMemoryHistory();
     history.push('/xablau');
