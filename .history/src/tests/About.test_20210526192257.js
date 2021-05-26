40e758711e90dbf1a32f3106c06d330503e9b4d3
Juan Pablo <juan.tentiomaru9309@gmail.com>
2021-05-26 19:28:45 -0300

    req2 test

--- .history/src/tests/About.test_20210526192257.js
+++ .history/src/tests/About.test_20210526192257.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { fireEvent } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from '../components/MemoryRouter';
 
@@ -6,23 +8,16 @@ describe('Renders about', () => {
   test('Tests if exits an H2 with the text: About Pokédex ', () => {
     const { getByRole } = renderWithRouter(<About />);
     const h2Finder = getByRole('heading', { level: 2 });
-
     expect(h2Finder).toHaveTextContent('About Pokédex');
   });
 
   test('Tests if exists two p`s in the component ', () => {
     const { getAllByText } = renderWithRouter(<About />);
     const pFinder = getAllByText(/Pokémons/i);
-
     expect(pFinder[0]).toBeInTheDocument();
     expect(pFinder[1]).toBeInTheDocument();
   });
 
-  test('Tests if exists an image', () => {
-    const { getByRole } = renderWithRouter(<About />);
-    const imgFinder = getByRole('img');
-    const imgHttp = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-
-    expect(imgFinder.src).toContain(imgHttp);
-  });
-});
+  test('', () => {});
+  test('', () => {});
+})
