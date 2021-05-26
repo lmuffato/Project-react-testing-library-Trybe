8d434bf50b8b6a2c44c8b377603670cd1bcff3a2
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-26 17:58:30 -0300

    Req 3 - Test component FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,38 @@
-test('', () => {});
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Component FavoritePokemons.js tests', () => {
+  test('There is a h2 heading with text About Pokedex', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <FavoritePokemons />
+      </BrowserRouter>,
+    );
+
+    const emptyMsg = getByText('No favorite pokemon found');
+    expect(emptyMsg).toBeInTheDocument();
+  });
+
+  test('Pikachu is a favorite pokemons', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <FavoritePokemons
+          pokemons={ [{
+            name: 'Pikachu',
+            averageWeight: {
+              measurementUnit: 'kg',
+              value: '6.0',
+            },
+            id: 25,
+          }] }
+        />
+      </BrowserRouter>,
+    );
+
+    const pokemonName = getByText('Pikachu');
+
+    expect(pokemonName).toBeInTheDocument();
+  });
+});
