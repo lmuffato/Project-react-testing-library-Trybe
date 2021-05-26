fc97669952b177919ed0374416e175544d9f8df4
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-26 19:32:09 -0400

    fix: app tets ,including all tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MemoryRouter as BrowserRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 const renderApp = () => render(
@@ -8,7 +10,7 @@ const renderApp = () => render(
     <App />
   </BrowserRouter>,
 );
-
+const renderAppWithRouter = () => renderWithRouter(<App />);
 describe('App.test.js', () => {
   test('Exibe um texto que contém "Pokedex".', () => {
     const { getByText } = renderApp();
@@ -23,5 +25,34 @@ describe('App.test.js', () => {
     expect(linkAbout).toBeInTheDocument();
     const linkFav = getByRole('link', { name: /favorite pokémons/i });
     expect(linkFav).toBeInTheDocument();
+  });
+  test('Ao clicar em Home direciona para a pagina de início', () => {
+    const { getByRole, history } = renderAppWithRouter();
+    const linkHome = getByRole('link', { name: /home/i });
+    userEvent.click(linkHome);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+  });
+  test('Ao clicar em About direciona para a pagina de Sobre', () => {
+    const { getByRole, history } = renderAppWithRouter();
+    const linkAbout = getByRole('link', { name: /about/i });
+    userEvent.click(linkAbout);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
+  });
+  test('Ao clicar em FavoritePokemons direciona para a pagina de favoritos', () => {
+    const { getByRole, history } = renderAppWithRouter();
+    const linkFavorites = getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(linkFavorites);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+  test('Ao ir há uma pagina que não existe direciona para a pagina NotFound', () => {
+    const { getByRole, history } = renderAppWithRouter();
+    history.push('where-i-am?');
+    const notFoundText = getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+      level: 2 });
+    expect(notFoundText).toBeInTheDocument();
   });
 });
