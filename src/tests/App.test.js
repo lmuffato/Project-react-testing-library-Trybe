43732afaafd9cc4d416afd7a668690b0b3878a01
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-26 15:36:29 -0300

    test: testing <App /> completed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,33 +1,97 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
-describe('Testing the <App.js /> component', () => {
+describe('1 - Testing the <App.js /> component', () => {
   test('renders a reading with the text \'Pokédex\'', () => {
-    const { getByText } = renderWithRouter(<App />);
+    renderWithRouter(<App />);
 
-    const heading = getByText(/Pokédex/i);
+    const heading = screen.getByText(/Pokédex/i);
     expect(heading).toBeInTheDocument();
   });
 
   test('shows the home page when the route is \'/\'', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
     const { location: { pathname } } = history;
-    const homePageText = getByText(/Encountered pokémons/i);
+    const homePageText = screen.getByText(/Encountered pokémons/i);
 
     expect(pathname).toBe('/');
     expect(homePageText).toBeInTheDocument();
   });
 
   test('the application contains a fixed set of nagivation links', () => {
-    const { getAllByRole } = renderWithRouter(<App />);
-    const navigationLinks = getAllByRole('link');
+    renderWithRouter(<App />);
+
+    const headerNavigation = screen.getByRole('navigation');
+    const navigationLinks = headerNavigation.getElementsByClassName('link');
+    const linksLength = 3;
     const firstLink = navigationLinks[0];
     const secondLink = navigationLinks[1];
     const thirdLink = navigationLinks[2];
 
+    expect(navigationLinks.length).toBe(linksLength);
     expect(firstLink.textContent).toBe('Home');
     expect(secondLink.textContent).toBe('About');
     expect(thirdLink.textContent).toBe('Favorite Pokémons');
+  });
+
+  test('go to the home page (/) when clicked in the \'Home\' link', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: /home/i });
+
+    userEvent.click(homeLink);
+    const { location: { pathname } } = history;
+    const homeText = screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    });
+
+    expect(pathname).toBe('/');
+    expect(homeText).toBeInTheDocument();
+  });
+
+  test('go to the about page (/about) when clicked in the \'About\' link', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+
+    userEvent.click(aboutLink);
+    const { location: { pathname } } = history;
+    const aboutText = screen.getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+
+    expect(pathname).toBe('/about');
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('go to the favorite page (/favorites) when'
+  + 'clicked in the \'Favorite Pokémons\' link', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+
+    userEvent.click(favoriteLink);
+    const { location: { pathname } } = history;
+    const favoriteText = screen.getByRole('heading', {
+      name: /favorite pokémons/i,
+      level: 2,
+    });
+
+    expect(pathname).toBe('/favorites');
+    expect(favoriteText).toBeInTheDocument();
+  });
+
+  test('go to the home page \'/\' after click in the \'Home\' link', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/no-match');
+    const notFoundText = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFoundText).toBeInTheDocument();
   });
 });
