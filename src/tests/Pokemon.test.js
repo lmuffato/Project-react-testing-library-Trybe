a899dbdf5174b22182e98e0f3843d48dc782fb1f
Viegas <mvrdgs@gmail.com>
2021-05-26 05:27:15 -0300

    implement: test 'More Details' page and 'Favorite' icon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -21,4 +21,27 @@ describe('Test Pokemon card', () => {
     expect(pokemonType.textContent).toMatch(/Psychic/i);
     expect(pokemonWeight.textContent).toMatch(/\d+ kg/i);
   });
+
+  it('Test if card has a link to \'More Details\'', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', { name: /More Details/i });
+    expect(detailsLink).toBeInTheDocument();
+    expect(detailsLink.href).toMatch(/\/pokemons\/\d+/i);
+    expect(detailsLink.textContent).toMatch(/More Details/i);
+  });
+
+  it('Test \'More Details\' page and \'Favorite\' icon', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const detailsLink = getByRole('link', { name: /More Details/i });
+    fireEvent.click(detailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+    const favoriteCheck = getByRole('checkbox', { name: /pokémon favoritado/i });
+    fireEvent.click(favoriteCheck);
+    const homeLink = getByRole('link', { name: /home/i });
+    fireEvent.click(homeLink);
+    const favoriteIcon = getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(favoriteIcon).toBeInTheDocument();
+    expect(favoriteIcon.src).toBe('http://localhost/star-icon.svg');
+  });
 });
