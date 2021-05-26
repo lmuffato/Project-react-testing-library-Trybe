b079875938fd0ab3d38361e2a14d0484a9850c3c
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-25 23:19:29 -0300

    feat(About.test.js): Testa se e renderizado um h2 com o texto About Pokedex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import About from '../components/About';
+
+describe('Tests whether the information about the pokedex is rendered', () => {
+  it('Renders a header with the text About Pokedex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
