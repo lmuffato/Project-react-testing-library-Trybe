49e79fbfeaf16b159994115087e2ea36cffca879
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-26 18:49:42 -0300

    Req 3 - Test component NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Component NotFound.js tests', () => {
+  test('There is a h2 heading with text: Page requested not found 😭', () => {
+    const { getByRole, getByLabelText } = render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+
+    const heading = getByRole('heading', {
+      name: /Page requested not found/,
+      level: 2,
+    });
+
+    const cryingEmoji = getByLabelText('Crying emoji');
+    expect(heading).toBeInTheDocument();
+    expect(cryingEmoji).toBeInTheDocument();
+  });
+
+  test('There is an certain img', () => {
+    const { getByAltText } = render(
+      <BrowserRouter>
+        <NotFound />
+      </BrowserRouter>,
+    );
+
+    const img = getByAltText('Pikachu crying because the page requested was not found');
+    expect(img).toBeInTheDocument();
+    expect(img.src).toMatch(/kNSeTs31XBZ3G/);
+  });
+});
