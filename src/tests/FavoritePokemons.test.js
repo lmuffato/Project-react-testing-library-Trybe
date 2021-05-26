30550ff3bc0af0306c11d3e9fdad93ceb865f678
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-05-25 21:22:43 -0400

    feat req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
 describe('test all screen application of the FavoritePokemons', () => {
   it('"No favorite pokemon" found msg screen,if a person has no favorite pokemon',
@@ -9,4 +11,18 @@ describe('test all screen application of the FavoritePokemons', () => {
       const notFavorite = getByText(/No favorite pokemon found/i);
       expect(notFavorite).toBeInTheDocument();
     });
+});
+
+describe('Check card favorite pokémon', () => {
+  it('screen card favorites', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const details = getByText('More details');
+    userEvent.click(details);
+    const checkbox = getByText('Pokémon favoritado?');
+    userEvent.click(checkbox);
+    const favorite = getByText('Favorite Pokémons');
+    userEvent.click(favorite);
+  });
+  const cardPokemon = getByText('Average weight:');
+  expect(cardPokemon).toBeInTheDocument();
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+// import React from 'react';
+// import renderWithRouter from '../services/renderWithRouter';
+// import Pokemon from '../components/Pokemon';
+// import pokemons from '../data';
+
+// describe('testing all screen application of the Pokémon', () => {
+//   it('cheking render a pokémon with info', () => {
+//     const { getByTestId, getByAltText } = renderWithRouter(<Pokemon />);
+//     const name = getByTestId('pokemon-overview');
+//     const type = getByTestId('pokemon-type');
+//     const weight = getByTestId('pokemon-weight');
+//     const img = getByAltText('is marked as favorite');
+//     expect(name).toHaveTextContent(`${pokemons.name}`);
+//     expect(type).toHaveTextContent(`${pokemons.type}`);
+//     expect(weight).toHaveTextContent(
+//       `Average weight: ${pokemons[0]
+//         .averageWeight.value} ${pokemons[0]
+//         .averageWeight.measurementUnit}`,
+//     );
+//     expect(img.src).toBe(pokemons[0]);
+//   });
+// });
