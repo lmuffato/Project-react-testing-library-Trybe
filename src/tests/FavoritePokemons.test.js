f5a24ee438aa3d210e16f0ae6f2e6210081084a2
Debora Passos <deborinhapassos@gmail.com>
2021-05-26 16:20:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('renderizar o componente FavoritePokemons', () => {
+  it('deve verificar se tem pokemon favorito', () => {
+    const { getByRole, getByText } = renderWithRouter(<FavoritePokemons />);
+
+    const pokemonNotFound = getByText(/No favorite pokemon found/);
+    const favoriteFound = getByRole('heading', {
+      level: 2,
+      name: /Favorite pokémons/i,
+    });
+    expect(favoriteFound).toBeInTheDocument();
+    expect(pokemonNotFound).toBeInTheDocument();
+  });
+});
