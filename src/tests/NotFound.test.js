a6d525bfeccb134a087fcdcdfba78017d06275cd
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-25 21:56:54 -0300

    feat: requirement04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { NotFound } from '../components';
+
+describe('Test the component <NotFound.js>', () => {
+  it('Test if page contains an heading with text', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    expect(getByRole('heading', {
+      name: /page requested not found Crying emoji/i,
+    })).toBeInTheDocument();
+  });
+
+  it('Test if the page shows the image', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+    const image = getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
