35487f28b6c53f2b7d682736cabc88e13bd6dee6
LucasMLara <lucasml@live.com>
2021-05-26 17:39:08 -0300

    test: :white_check_mark: NotFound Tests Implemented

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('"NotFound" page tests', () => {
+  test('Header', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const header = getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+
+    expect(header).toBeInTheDocument();
+  });
+
+  test('Show a Crying Pikachu when no Pokemon was Found', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    // https://testing-library.com/docs/queries/about Priority #2 - Semantic Queries
+    const notFoundGif = getByAltText('Pikachu crying because'
+    + ' the page requested was not found');
+
+    expect(notFoundGif.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(notFoundGif).toBeInTheDocument();
+
+  });
+});
