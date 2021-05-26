905584cd219b234416f244878fc77771a11ae9a7
LucasMLara <lucasml@live.com>
2021-05-26 11:54:59 -0300

    docs: :memo: Creating 'default template tests from about.tests.js'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,6 +2,9 @@ import React from 'react';
 import About from '../components/About';
 import renderWithRouter from '../services/renderWithRouter';
 
-describe('', () => {
-  test('', () => {});
+describe('"About" page Tests', () => {
+  test('Header', () => {});
+  test('First Paragraph', () => {});
+  test('Second Paragraph', () => {});
+  test('Pokedex Image', () => {});
 });
