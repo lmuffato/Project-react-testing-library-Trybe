33fb4c5a2f864345b322d8eb02b4ad4bf8013ca0
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-05-26 18:46:57 -0300

    Requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Testa se a mensagem (No favorite pokemon found) aparece na tela ', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  history.push('/favorites');
+
+  const myText = getByText('No favorite pokemon found');
+
+  expect(myText).toBeInTheDocument();
+});
