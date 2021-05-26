b961f05d0f6344a5d6b4f123a35a227d45471fba
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 15:22:32 -0300

    estruturando testes do req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,4 @@
-test('', () => {});
+describe('4. Testando componente <NotFound />', () => {
+  test('Testar se a página contém um h2 com um texto', () => {});
+  test('Testar se a página contém uma imagem', () => {});
+});
