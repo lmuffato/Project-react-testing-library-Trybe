23dcb3c0cd431d1cb086572275fa3dbdd057c8e7
jepaur5 <jepaur5@gmail.com>
2021-05-26 17:51:49 -0300

    requisito4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  it('Test if a contains an h2 heading with the text Page requested not found 😭', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const title = getByRole('heading', {
+      level: 2,
+    });
+
+    expect(title.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('Test if the page shows the image', () => {
+    const { getAllByRole } = renderWithRouter(<NotFound />);
+
+    const image = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(getAllByRole('img')[1].src).toBe(image);
+  });
+});
