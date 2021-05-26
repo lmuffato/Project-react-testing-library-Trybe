ae4c92beb66f29d35d1371f2da793d1fefa2a943
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-26 11:45:59 -0300

    requisito 5 iniciado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,19 +1,20 @@
-// import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { render, screen } from '@testing-library/react';
-// import Pokedex from '../components/Pokedex';
-// import renderWithRouter from './renderWithRouter';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-// test('Verifica se aparece h2 com texto Encountered pokémons', () => {
-//   render(<Pokedex />);
-//   const h2Text = screen.getByRole('heading', {
-//     name: /Encountered pokémons/i,
-//   });
-//   expect(h2Text).toBeInTheDocument();
-// });
+test('Verifica se aparece h2 com texto Encountered pokémons', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const h2Text = getByRole('heading', {
+    name: /Encountered pokémons/i,
+  });
+  expect(h2Text).toBeInTheDocument();
+});
 
-// test('Verifica se botão próximo pokemon funciona', async () => {
-//   render(<Pokedex />);
-//   const buttonNext = screen.getByText('Próximo pokémon');
-//   expect(buttonNext).toBeDefined();
-// });
+test('Verifica se botão próximo pokemon funciona', async () => {
+  const { getByText } = renderWithRouter(<App />);
+  const buttonNext = getByText('Próximo pokémon');
+  expect(buttonNext).toBeDefined();
+});
