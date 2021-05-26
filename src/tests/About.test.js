daa07062bf0ec11a9a83a777c4c4962dddada81c
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-05-26 00:45:14 -0300

    Req 2: No mutants surived

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  const { getByText, getByRole } = render(<About />);
+  expect(getByRole('heading')).toBeInTheDocument();
+  expect(getByText('About Pokédex')).toBeInTheDocument();
+});
+
+test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  const { getByText } = render(<About />);
+  const firstParagraph = getByText('This application simulates a Pokédex, a '
+  + 'digital encyclopedia containing all Pokémons');
+  expect(firstParagraph).toBeInTheDocument();
+
+  const secondParagraph = getByText('One can filter Pokémons by type, '
+  + 'and see more details for each one of them');
+  expect(secondParagraph).toBeInTheDocument();
+});
+
+test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+  const { getByRole, getByAltText } = render(<About />);
+  const imagem = getByAltText('Pokédex');
+  expect(imagem).toBeInTheDocument();
+
+  const imgSrc = getByRole('img');
+  expect(imgSrc.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
