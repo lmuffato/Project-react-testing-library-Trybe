c42962e19122e7770aef84dba5d5875e29df9a50
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-26 17:16:19 -0300

    Requisito 1 Parte 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -21,3 +21,23 @@ test('shows the Pokédex when the route is `/`', () => {
   );
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
+
+test('Testa se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  const { getByText, getByRole } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const navigation = getByText('navigation');
+  expect(navigation).toBeInTheDocument();
+
+  const navigationHome = getByText('link', { nome: /home/i });
+  expect(navigationHome).toBeInTheDocument();
+
+  const navigationAbout = getByRole('link', { nome: /about/i });
+  expect(navigationAbout).toBeInTheDocument();
+
+  const navigationFav = getByText('link', { nome: /Favorite Pokémons/i });
+  expect(navigationFav).toBeInTheDocument();
+});
