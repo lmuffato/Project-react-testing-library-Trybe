b6d209bac07757c40abefed64b4e6c7f6f0a1239
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-26 13:06:01 -0300

    feat: Testa se página inicial contém  h2 com o texto Encountered pokémons

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Componente Pokedex', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const h2 = getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+    expect(h2).toBeInTheDocument();
+  });
+});
