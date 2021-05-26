22b62686f71d231f2f97b99a448779680eb6b096
hheloisa <hhackenhaar@gmail.com>
2021-05-26 20:07:17 -0300

    Adiciona teste de numero de paragrafos no componente <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,6 +4,8 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
 
+// Referência para resolução do teste da imagem: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f;
+
 describe('Testa o componente About', () => {
   test('Teste se a página contém as informações sobre a Pokédex.', () => {
     renderWithRouter(<About />);
@@ -20,5 +22,10 @@ describe('Testa o componente About', () => {
     const image = screen.getByRole('img');
     expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
     expect(image).toHaveAttribute('alt', 'Pokédex');
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    const paragraphs = screen.getAllByText(/pokémon/i);
+    expect(paragraphs.length).toBe(2);
   });
 });
