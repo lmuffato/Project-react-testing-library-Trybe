030b9ed1ec4f134597d3610b86eb8eb019384818
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 14:50:32 -0300

    mudando estrutura para usar renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,48 +1,31 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import About from '../components/About';
+import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('2. Testando componente <About />', () => {
   test('Teste se a página contém as informações sobre a Pokédex.', () => {
-    const { getByText } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
+    const { getByText } = renderWithRouter(<About />);
 
     const aboutHeading = getByText('About Pokédex');
     expect(aboutHeading).toBeInTheDocument();
   });
 
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
-    const { getByRole } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
+    const { getByRole } = renderWithRouter(<About />);
 
     const pageTitle = getByRole('heading', { level: 2 });
     expect(pageTitle).toHaveTextContent('About Pokédex');
   });
 
   test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
+    const { container } = renderWithRouter(<About />);
 
     const paragraphs = container.querySelectorAll('p');
     expect(paragraphs).toHaveLength(2);
   });
 
   test('Teste se a página contém a imagem de uma Pokédex:', () => {
-    const { getByAltText } = render(
-      <MemoryRouter>
-        <About />
-      </MemoryRouter>,
-    );
+    const { getByAltText } = renderWithRouter(<About />);
 
     const imagePath = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const image = getByAltText('Pokédex');
