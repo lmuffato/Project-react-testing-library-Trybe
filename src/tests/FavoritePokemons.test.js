df7026f995c6302631fc8efbf0b7b2c7aaf181aa
hheloisa <hhackenhaar@gmail.com>
2021-05-26 20:20:08 -0300

    Inicia testes de <FavoritePokemons />, testa titulo(h2) e (no fav pokemons found)

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('testa se a página contém titulo "Favorite pokémons', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const titleFav = screen.getByRole('heading', { name: 'Favorite pokémons', level: 2 });
+    expect(titleFav).toBeInTheDocument();
+  });
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noPokemons = screen.getByText('No favorite pokemon found');
+    expect(noPokemons).toBeInTheDocument();
+  });
+});
