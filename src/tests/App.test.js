6442ba8c8072bb4ed44b731bd976fe3b4f707f56
Nathalia Zebral <natzebral@gmail.com>
2021-05-26 17:59:17 -0300

    test if the main page is in the / url

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +12,14 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('test if the main page is in the / url', () => {
+  const { history, getByRole } = renderWithRouter(
+    <App />,
+  );
+  expect(history.location.pathname).toBe('/');
+  expect(getByRole('heading', {
+    name: /encountered pokémons/i,
+  })).toBeInTheDocument();
 });
