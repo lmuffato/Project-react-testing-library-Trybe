1a5eea7f5c5e6100fe1f92a0505a60ef081a43db
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-26 18:37:17 -0300

    feat(Pokedex.test.js): 'Testa se o h2 e renderizado.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+import Pokedex from '../components/Pokedex';
+
+describe('', () => {
+  it('renders a heading with the text `Encountered pokémons`', () => {
+    renderWithRouter(<App />);
+    const heading = screen
+      .getByRole('heading', { level: 2, name: /Encountered Pokémons/i });
+    expect(heading).toBeInTheDocument();
+  });
+});
