c030b3b2b875d3924cb3a7c955afabd69c6aeb71
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-26 16:40:57 -0300

    feat: testes do componente Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,8 @@
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 
 describe('Componente Pokedex', () => {
   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
@@ -16,5 +18,31 @@ describe('Componente Pokedex', () => {
     const { getAllByText } = renderWithRouter(<App />);
     const text = getAllByText(/More details/);
     expect(text.length).toBe(1);
+  });
+
+  test('Teste se é exibido o próximo Pokémon ao clicar no botão Próximo pokémon ', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const btn = getByTestId('next-pokemon');
+    const currPokemon = getByTestId('pokemon-name');
+    expect(btn).toBeInTheDocument();
+    expect(btn.type).toBe('button');
+    expect(currPokemon).toHaveTextContent('Pikachu');
+    expect(currPokemon).not.toHaveTextContent('Charmander');
+    fireEvent.click(btn);
+    expect(currPokemon).not.toHaveTextContent('Pikachu');
+    expect(currPokemon).toHaveTextContent('Charmander');
+  });
+
+  test('Estando no último, volta ao primeiro Pokémon, clicando em Próximo ', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const nameFistPokemon = pokemons[0].name;
+    const regex = new RegExp(nameFistPokemon);
+    const btn = getByTestId('next-pokemon');
+    const currPokemon = getByTestId('pokemon-name');
+
+    pokemons.forEach(() => {
+      fireEvent.click(btn);
+    });
+    expect(currPokemon).toHaveTextContent(regex);
   });
 });
