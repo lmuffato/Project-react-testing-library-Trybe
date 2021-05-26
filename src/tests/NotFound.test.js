599c14ef988d7b1526db477ebaa01a0843786d9d
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-05-26 18:53:16 -0300

    Requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Testa se a pagina contem um h2 com o tetst Page requested not found 😭 ', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  history.push('/desconhecido');
+
+  const notFoundText = getByRole('heading', {
+    name: /Page requested not found Crying emoji/i,
+    level: 2,
+  });
+
+  expect(notFoundText).toBeInTheDocument();
+});
+
+it('Testa se o src da imagem e o esperado', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  history.push('/desconhecido');
+
+  const myImage = getByRole('img', {
+    name: 'Pikachu crying because the page requested was not found',
+  });
+  expect(myImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
