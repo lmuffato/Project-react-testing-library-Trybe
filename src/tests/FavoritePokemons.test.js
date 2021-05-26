6656ecc144703c30b59d4d214d8b3cd4d6635db6
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-05-26 19:53:05 -0300

    requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RenderWithRouter from './RenderWithRouter';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+
+describe('Favorite Pokemons', () => {
+  test('é exibido na tela a mensagem No favorite pokemon found'
+  + 'se a pessoa não tiver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+    const titulo = screen.getByText('No favorite pokemon found');
+    expect(titulo).toBeInTheDocument();
+  });
+
+  test('é exibido todos os cards de pokémons favoritados', () => {
+    RenderWithRouter(<App />);
+    const aboutLink = screen.getAllByRole('link')[2];
+    userEvent.click(aboutLink);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('not found', () => {
+  test('este se página contém um heading h2', () => {
+    render(<NotFound />);
+    const titulo = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(titulo).toBeInTheDocument();
+  });
+
+  test('imagem de um pokemon', () => {
+    render(<NotFound />);
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    // usei o exemplo dessa resposta no stack overflow https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+  });
+});
