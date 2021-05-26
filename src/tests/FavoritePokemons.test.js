c38d2b0c186f1bbeedcb5d87ed768a7762a43cf2
igson <igson660@gmail.com>
2021-05-25 22:43:58 -0500

    finaliza o teste do component NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithHistory';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testes do component About.js', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/No favorite pokemon/i)).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithHistory';
+import NotFound from '../components/NotFound';
+
+describe('testes do component About.js', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    expect(getByText(/Page requested /i)).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém a imagem de uma Pokédex', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    expect(getByAltText(/Pikachu crying because/i).src)
+      .toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
