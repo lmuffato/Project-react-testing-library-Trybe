46ce7aec798ca493c224d5c7b9cbff264552b6d7
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-25 21:23:27 -0300

    requisito 2 feito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -12,10 +12,10 @@ test('Verifica se as informações sobra a Pokedex aparecem', () => {
   expect(h2Text).toBeInTheDocument();
 });
 
-// test('Verifica se as informações sobra a Pokedex aparecem', () => {
-//     render(<About />);
-//     const h2Text = screen.getByRole('heading', {
-//       name: /'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'/i,
-//     });
-//     expect(h2Text).toBeInTheDocument();
-//   });
+// forma de pegar link vista no stackoverflow
+// https://stackoverflow.com/questions/60509527/jestreact-native-testing-library-how-to-test-an-image-src
+test('Verifica se a Pokedex é renderizada', () => {
+  render(<About />);
+  const Pokedex = screen.getByRole('img');
+  expect(Pokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+// import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from './renderWithRouter';
+
+test('Verifica se aparece h2 com texto Encountered pokémons', () => {
+  render(<Pokedex />);
+  const h2Text = screen.getByRole('heading', {
+    name: /Encountered pokémons/i,
+  });
+  expect(h2Text).toBeInTheDocument();
+});
