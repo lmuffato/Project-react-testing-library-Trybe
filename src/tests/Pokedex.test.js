d1a0e501d055ece36bb5e6a0108d9bd77e03d21d
guidugaich <guidugaich.dev@gmail.com>
2021-05-26 15:37:41 -0300

    montando estrutura para os testes da pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+describe('5. Testando componente <Pokedex />', () => {
+  test('Página contém um heading h2 com o texto Encountered pokémon', () => {});
+  test('é exibido o próximo Pokémon da lista quando o botão Próximo pokémon é clicado', () => {});
+  test('é mostrado apenas um Pokémon por vez', () => {});
+  test('a Pokédex tem os botões de filtro', () => {});
+  test('a Pokédex contém um botão para resetar o filtro', () => {});
+  test('é criado, dinamicamente, um botão de filtro para cada tipo de Pokémon', () => {});
+  test('Botão Próximo deve ser desabilitado quando a lista tiver só um pokémon', () => {});
+});
