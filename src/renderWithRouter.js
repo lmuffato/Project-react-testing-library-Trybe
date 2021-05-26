66241921678f07c0717cb96012716354850a17f6
Vinicios <soicinivt@gmail.com>
2021-05-26 17:21:38 -0300

    test with links and routes in App.test.js

---
+++ src/renderWithRouter.js
@@ -0,0 +1,15 @@
+// Diretamente a fonte de conhecimento mor : https://app.betrybe.com/course/front-end/testes-automatizados-com-react-testing-library/rtl-testando-react-router/58c480e0-79ed-47bd-a819-f88d82997927/conteudos/3ac4a246-7ea6-4722-bd9d-7c141435d8ee/testando-react-router/367f539c-699b-4e1c-abcc-03b14d1a608e?use_case=side_bar
+
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={history}>{component}</Router>), history,
+  });
+};
+
+export default renderWithRouter;
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,60 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+/* afterEach(cleanup); */
+describe('Trybe default tests', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = screen.getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('shows the Pokédex when the route is `/`', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+describe('Tests makes for me', () => {
+  describe('Tests with the component \'App\'', () => {
+    it('Test the order of links', () => {
+      render(
+        <MemoryRouter initialEntries={ ['/'] }>
+          <App />
+        </MemoryRouter>,
+      );
+      const firstLink = screen.getByRole('link', { name: 'Home' });
+      const secondLink = screen.getByRole('link', { name: 'About' });
+      const thirdLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      const allLinks = screen.getAllByRole('link');
+      expect(allLinks[0]).toBe(firstLink);
+      expect(allLinks[1]).toBe(secondLink);
+      expect(allLinks[2]).toBe(thirdLink);
+    });
+    it('Test with Favorite Pokemons link', () => {
+      const { history } = renderWithRouter(<App />);
+      const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      fireEvent.click(favoritePokemons);
+      const localPath = history.location.pathname;
+      expect(localPath).toBe('/favorites');
+    });
+    it('Test if page not foud appear when invalid url is called', () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/notFoudIsHere');
+      const notFoundPage = screen.getByText(/not found/i);
+      expect(notFoundPage).toBeInTheDocument();
+    });
+  });
 });
