d5a46bcdce29a908c54a2be3b2edbf169a140c4f
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-27 09:48:56 -0300

    refactor: testing component <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import renderWithRouter from './helpers/renderWithRouter';
+import { screen, render } from '@testing-library/react';
 import { About } from '../components';
 
-describe('2 - Testing the <About /> component', () => {
+describe('2 - Testing the component <About />', () => {
   test('the page /about must have a heading h2 with the text \'About Pokédex\'', () => {
-    const { history } = renderWithRouter(<About />);
+    render(<About />);
 
-    history.push('/about');
-    const { location: { pathname } } = history;
     const aboutHeading = screen.getByRole('heading', {
       name: /about pokédex/i,
       level: 2,
     });
 
-    expect(pathname).toBe('/about');
     expect(aboutHeading).toBeInTheDocument();
   });
 
   test('the page /about must have two paragraphs', () => {
-    const { history } = renderWithRouter(<About />);
+    render(<About />);
 
-    history.push('/about');
     const paragraphs = document.querySelectorAll('p');
     const paragraphsLength = 2;
 
@@ -29,9 +24,8 @@ describe('2 - Testing the <About /> component', () => {
   });
 
   test('the page /about must have a image with a specific source', () => {
-    const { history } = renderWithRouter(<About />);
+    render(<About />);
 
-    history.push('/about');
     const image = screen.getByRole('img', { name: /pokédex/i });
     const imageSource = (
       'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
