7f31d6d78a8b693d6963a2585828c198ece59487
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-27 18:46:13 -0300

    testa retorno da rota /about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('tests return of application details', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/about'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const heading = getByText(/About Pokédex/i);
+  expect(heading).toBeInTheDocument();
+});
