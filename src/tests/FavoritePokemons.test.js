fabad96bc8a15c4fe20fcc4d5aa4aa8c532f981e
lucas-chave <lcchavesr150@gmail.com>
2021-05-27 09:56:40 -0300

    test requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requirement 3 - testing the <FavoritePokemosn/> component', () => {
+  it('Check if it contains the information "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText('No favorite pokemon found')).toBeDefined();
+  });
+});
