5d30022a7f576a5f966ca59758481aeda18c5649
Guilherme de Prais <guilherme.prais@gmail.com>
2021-05-27 15:53:57 -0300

    req 03: complete and create renderWithRouter

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+
+  const text = getByText(/No favorite pokemon found/i);
+  expect(text).toBeInTheDocument();
+});
