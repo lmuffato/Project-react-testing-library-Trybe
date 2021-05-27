27792aafa5049f026442401f059b49e3cf7a9ada
Guilherme de Prais <guilherme.prais@gmail.com>
2021-05-27 17:24:21 -0300

    req 04: complete

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from "../components";
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+
+  const h2 = getByRole('heading', { name: /Page requested not found/i, level: 2 });
+  expect(h2).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem', () => {
+  const { getByRole } = renderWithRouter(<NotFound />);
+
+  const img = getByRole('img', { name: /Pikachu crying because the page/i });
+  expect(img).toBeInTheDocument();
+  expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+
+})
