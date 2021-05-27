cfcef9bd607bcec359646bbe845996ef604b2101
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-27 18:00:56 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Test NotFound', () => {
+  test('testing h2 heading ', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const notFound = getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFound).toBeInTheDocument();
+  });
+  test('testing image ', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const imageNotFound = getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
