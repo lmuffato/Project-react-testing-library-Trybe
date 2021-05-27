687f8870102e435b5ac7fd50864765ea10170e3e
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-05-27 14:42:12 -0300

    feat: requisito 5 completo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -26,13 +26,49 @@ describe('testing the component "Pokedex"', () => {
     expect(nextPokemon).toBeInTheDocument();
   });
 
-  it('', () => {
+  // it('show one pokemon at a time', () => {
+  //   renderWithRouter(<App />);
+  //   const validation = document.querySelector('a');
+  //   expect(validation).toHaveAttribute('a', 'More details');
+  // });
+
+  it('Test for filter buttons', () => {
     const { getByRole } = renderWithRouter(<App />);
+    const filterBtn = (btn) => getByRole('button', { name: `${btn}` });
+    const btnAll = filterBtn('All');
+    const btnElectric = filterBtn('Electric');
+    const btnFire = filterBtn('Fire');
+    const btnBug = filterBtn('Bug');
+    const btnPoison = filterBtn('Poison');
+    const btnPsychic = filterBtn('Psychic');
+    const btnNormal = filterBtn('Normal');
+    const btnDragon = filterBtn('Dragon');
+
+    expect(btnAll).toBeInTheDocument();
+    expect(btnElectric).toBeInTheDocument();
+    expect(btnFire).toBeInTheDocument();
+    expect(btnBug).toBeInTheDocument();
+    expect(btnPoison).toBeInTheDocument();
+    expect(btnPsychic).toBeInTheDocument();
+    expect(btnNormal).toBeInTheDocument();
+    expect(btnDragon).toBeInTheDocument();
+  });
+
+  it('Test if the Pokédex contains a button to reset the filter', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
     const buttonAll = getByRole('button', {
       name: /all/i,
     });
 
     userEvent.click(buttonAll);
-  //   expect(buttonAll).toHaveLength/(8);
+    const firstPokemon = getByText('Pikachu');
+    expect(firstPokemon).toBeInTheDocument();
+  });
+
+  it('shows the filter buttons dynamically', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const filterButtons = getAllByTestId('pokemon-type-button');
+    const seven = 7;
+    expect(filterButtons).toHaveLength(seven);
   });
 });
