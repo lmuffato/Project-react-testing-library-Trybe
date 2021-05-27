dffc54db6b867e3b445aebcf0819518477cf311c
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-27 16:28:32 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -22,4 +22,12 @@ describe('Test About', () => {
     expect(paragraph1).toBeInTheDocument();
     expect(paragraph2).toBeInTheDocument();
   });
+
+  test('testing pokedex image ', () => {
+    const { getByRole } = renderWithRouter(<About />);
+
+    const image = getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
