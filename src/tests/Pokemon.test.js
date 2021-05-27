21d7ae63df561795ec8b9c62fa5197052698b86b
hheloisa <hhackenhaar@gmail.com>
2021-05-27 15:58:01 -0300

    Inicia testes em <Pokemon />, testa nome

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+const pokeType = 'pokemon-type';
+const btnPokeType = 'pokemon-type-button';
+const pokeName = 'pokemon-name';
+const nextPokemon = 'next-pokemon';
+
+describe('Testa os componentes do cardPokemon', () => {
+  test('Testa se o nome correto do pokemon aparece na tela', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId(pokeName);
+    expect(pokemonName.innerHTML).toBe('Pikachu');
+  });
+});
