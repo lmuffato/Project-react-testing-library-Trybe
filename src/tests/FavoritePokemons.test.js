f71f79dbcd62cd3f389221382efeca96fde0682a
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-27 17:55:24 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+  const { getByText, getByRole } = renderWithRouter(
+    <MemoryRouter>
+      <FavoritePokemons />
+    </MemoryRouter>,
+  );
+  const textFavPoke = getByText('No favorite pokemon found');
+  expect(textFavPoke).toBeInTheDocument();
+
+  const heading = getByRole('heading', {
+    level: 2,
+    name: /Favorite pokémons/i,
+  });
+  expect(heading).toBeInTheDocument();
+});
