ecdd0d51770aecd97bcf9fee4ae817864ff1bf04
mallmann02 <leomallmann020302@gmail.com>
2021-05-26 22:46:50 -0300

    Complete tests on Component "NotFound.js"

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons, NotFound } from '../components';
+import pokemons from '../data';
+
+test('', () => {
+  render(<NotFound />);
+  const notFoundTextEl = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i
+  });
+  
+  const notFoundImageEl = screen.getAllByRole('img');
+  const correctImagePath = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  expect(notFoundTextEl).toBeInTheDocument();
+  expect(notFoundImageEl[1].src).toEqual(correctImagePath);
+
+});
