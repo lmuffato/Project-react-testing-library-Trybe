67b6f9d239f0b483e724a6253002e869ff9c17db
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-27 14:42:54 -0400

    feat: no favorite pokemon found test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+const renderAppWithRouter = () => renderWithRouter(<App />);
+const renderFavoriteWithRouter = () => renderWithRouter(<FavoritePokemons />);
+
+describe('FavoritePokemons.test.js', () => {
+  test('Exibe "No favorite pokemon found" quando não há Pokemon favorito', () => {
+    const { getByText } = renderFavoriteWithRouter();
+    const noPokeFav = getByText('No favorite pokemon found');
+    expect(noPokeFav).toBeInTheDocument();
+  });
+});
