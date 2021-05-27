091d57221cd1c282dbc3bab335a2e08c8e3d8cb5
Villefor <franklincrius@gmail.com>
2021-05-27 03:28:17 -0300

    3 requisito passou, mesmo sem estar completo. 4 requisito feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('testing App components', () => {
+  it('renders a reading with the text `about pokédex`', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const paragraph = getByText(/No favorite pokemon found/i, {
+    });
+    expect(paragraph).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('testing App components', () => {
+  it('renders a reading with the text `page requested and an emoji`', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      name: /Page requested not found Crying emoji/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('test if there is a pikachu crying img', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const pokedexImg = getByAltText(
+      'Pikachu crying because the page requested was not found', {
+      },
+    );
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg.src)
+      .toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
