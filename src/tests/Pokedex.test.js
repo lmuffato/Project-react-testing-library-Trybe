edd572a257aff082aaaa87fe6c9bfc07dbcdd4c8
LucasMLara <lucasml@live.com>
2021-05-27 20:13:17 -0300

    test: :white_check_mark: Pokedex Test Implement

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import Pokedex from '../components/Pokedex';
+import renderWithRouter from '../services/renderWithRouter';
+import pokemons from '../data';
+
+describe('"Pokedex" Buttons Tests', () => {
+  test('"Header"', () => {
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ pokemons }
+    />);
+    const pageHeader = getByRole('heading', {
+      level: 2,
+      name: /Encountered pokémons/i,
+    });
+    expect(pageHeader).toBeInTheDocument();
+  });
+
+  const PKN_TYPE_BUTTON = 'pokemon-type-button';
+  test('"All" Button', () => {
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const allPokemons = getByRole('button', { name: /all/i });
+    userEvent.type(allPokemons);
+    expect(allPokemons).toBeInTheDocument();
+  });
+
+  test('"Elements" Buttons', () => {
+    const { getAllByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const elementsButtons = getAllByTestId(PKN_TYPE_BUTTON);
+    userEvent.type(elementsButtons);
+    expect(elementsButtons[0]).toHaveTextContent(/electric/i);
+  });
+
+  test('"Next" Button', () => {
+    const { getByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ {} }
+    />);
+    const nextPokemon = getByTestId('next-pokemon');
+    userEvent.type(nextPokemon);
+    expect(nextPokemon).toHaveTextContent(/próximo pokémon/i);
+  });
+});
