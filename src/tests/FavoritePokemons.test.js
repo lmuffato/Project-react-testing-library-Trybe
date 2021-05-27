e5272615ef855bac0093daaaed830caf41659e78
rcalvs <rafaelcalvette@gmail.com>
2021-05-27 15:24:41 -0300

    FavPkm test

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../services/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('FavoritePokemons tests', () => {
+  it('mostra uma pagina com "No favorite pokemon found" caso ocorra', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  it('mostra os pokemons favoritados', () => {
+    // Dragonair Favoritado
+    const favoritedPokemons = [pokemons[0]];
+    const { getAllByText } = renderWithRouter(<FavoritePokemons
+      pokemons={ favoritedPokemons }
+    />);
+    expect(getAllByText(/Average weight/i).length).toBe(1);
+  });
+
+  it('mostra os link `More details`', () => {
+    // Dragonair Favoritado
+    const favoritedPokemons = [pokemons[0]];
+    const { getAllByRole } = renderWithRouter(<FavoritePokemons
+      pokemons={ favoritedPokemons }
+    />);
+    expect(getAllByRole('link')).toHaveLength(1);
+
+  });
+});
