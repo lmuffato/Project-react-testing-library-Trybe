079c19ea04fe618ff6a88919d60aef150666561e
lucas-chave <lcchavesr150@gmail.com>
2021-05-27 10:13:07 -0300

    test requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requirement 4 - testing <NotFound/> component', () => {
+  it('Check if it contains tje text "PAge requested not found"', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    expect(getByText('Page requested not found')).toBeDefined();
+  });
+
+  it('Check if it contains the image with the source "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    expect(getByAltText('Pikachu crying because the page requested was not found').src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
