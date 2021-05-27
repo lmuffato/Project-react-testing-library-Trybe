27696e2e15138148224081e72dc191706440c107
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-05-26 22:58:31 -0300

    requisito 7 iniciado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,7 +1,29 @@
-// import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
+import React from 'react';
 // import { screen, render } from '@testing-library/react';
-// import NotFound from '../components/NotFound';
-// import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+// import { createMemoryHistory } from 'history';
+// import { Router } from 'react-router';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-// test('', () => {});
+test('', () => {});
+
+test('', () => {});
+
+test('testa se pode favoritar um pokémon', () => {
+const { getByRole, getByAltText, getByLabelText } = renderWithRouter(<App />);
+  const selectPikachu = getByRole('button', {
+    name: 'Electric',
+  });
+  userEvent.click(selectPikachu);
+  const selectDetails = getByRole('link', {
+    name: 'More details',
+  });
+  userEvent.click(selectDetails);
+  const checkbox = getByRole('checkbox');
+  const labelFavorite = getByLabelText('Pokémon favoritado?');
+  userEvent.click(labelFavorite);
+  const sprite = getByAltText('Pikachu is marked as favorite');
+  expect(labelFavorite).toBeInTheDocument();
+  expect(checkbox).toBeInTheDocument();
+});
