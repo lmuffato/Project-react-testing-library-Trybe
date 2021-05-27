71318bac974b5fa1b45aae4c65b7429e356aabf8
guidugaich <guidugaich.dev@gmail.com>
2021-05-27 17:21:43 -0300

    adicionando imports ao pokemonDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,5 +1,29 @@
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
 describe('7. Testando componente <PokemonDetails />', () => {
-  test('as informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {});
+  test('as informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {
+    const { getByRole, queryByRole, history, container } = renderWithRouter(<App />);
+
+    const moreDetailsLink = getByRole('link', { name: 'More details' });
+    expect(moreDetailsLink).toBeInTheDocument();
+    userEvent.click(moreDetailsLink);
+
+    expect(history.location.pathname).toBe('/pokemons/25');
+
+    const detailHeading = getByRole('heading', { level: 2, name: 'Pikachu Details' });
+    expect(detailHeading).toBeInTheDocument();
+
+    expect(queryByRole('link', { name: 'More details' })).toBeNull();
+
+    const summaryDeatailHeading = getByRole('heading', { level: 2, name: 'Summary' });
+    expect(summaryDeatailHeading).toBeInTheDocument();
+
+    const paragraph = container.querySelector('p');
+    expect(paragraph).toBeInTheDocument();
+  });
 
   test('existe na página uma seção com os mapas contendo as localizações do pokémon', () => {});
 
