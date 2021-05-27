82f51fe410a76ae9eff7bc055b100c0c22fc2e34
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-27 15:04:10 -0400

    feat: pokemons added to favorite

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 import App from '../App';
@@ -11,5 +12,17 @@ describe('FavoritePokemons.test.js', () => {
     const { getByText } = renderFavoriteWithRouter();
     const noPokeFav = getByText('No favorite pokemon found');
     expect(noPokeFav).toBeInTheDocument();
+  });
+  test('Quando pokemon é adicionado aos favoritos, exibe na pagina de Favoritos', () => {
+    const { getByText, getByRole, getByLabelText } = renderAppWithRouter();
+    userEvent.click(getByRole('link', { name: /more details/i }));
+    userEvent.click(getByLabelText(/pokémon favoritado?/i));
+    userEvent.click(getByRole('link', { name: /home/i }));
+    userEvent.click(getByRole('button', { name: /fire/i }));
+    userEvent.click(getByRole('link', { name: /more details/i }));
+    userEvent.click(getByLabelText(/pokémon favoritado?/i));
+    userEvent.click(getByRole('link', { name: /favorite pokémons/i }));
+    const pokemonsFavorited = [getByText(/pikachu/i), getByText(/charmander/i)];
+    pokemonsFavorited.forEach((pokemon) => expect(pokemon).toBeInTheDocument());
   });
 });
