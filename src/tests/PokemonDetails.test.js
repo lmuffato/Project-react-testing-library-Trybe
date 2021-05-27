a98a34f27811ed33e08bbe4ea059823bcfbf2c9d
guidugaich <guidugaich.dev@gmail.com>
2021-05-27 16:37:20 -0300

    montando estrutura para o requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+describe('7. Testando componente <PokemonDetails />', () => {
+  test('as informações detalhadas do Pokémon selecionado são mostradas na tela.', () => {});
+
+  test('existe na página uma seção com os mapas contendo as localizações do pokémon', () => {});
+
+  test('o usuário pode favoritar um pokémon através da página de detalhes.', () => {});
+
+})
