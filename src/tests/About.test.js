5af46970b51763b4f6b981decdf214c5da74336e
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-26 22:00:25 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import About from '../components/About';
+// import renderWithRouter from './renderWithRouter';
+
+test('Teste se a página contém as informações sobre a Pokédex.', () => {
+  const { getByRole } = render(
+    <BrowserRouter>
+      <About />
+    </BrowserRouter>,
+  );
+  const infoPoke = getByRole('heading', { level: 2 });
+  expect(infoPoke).toBeInTheDocument();
+});
