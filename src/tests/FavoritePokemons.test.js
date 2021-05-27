71d4a986181ae477d6b91a7cc159ce27d1506f2f
Lucas André <lucas.cancado89@gmail.com>
2021-05-27 16:57:20 -0300

    Refatoring

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import pokemons from '../data';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('testa FavoritePokemons component', () => {
@@ -7,5 +9,26 @@ describe('testa FavoritePokemons component', () => {
     const { getByText } = render(<FavoritePokemons />);
     const noFavorites = getByText('No favorite pokemon found');
     expect(noFavorites).toBeInTheDocument();
+  });
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </BrowserRouter>,
+    );
+    pokemons.forEach(({ name }) => {
+      const pokemonName = getByText(name);
+      expect(pokemonName).toBeInTheDocument();
+    });
+  });
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <FavoritePokemons />
+      </BrowserRouter>,
+    );
+    pokemons.forEach(({ name }) => {
+      expect(() => getByText(name)).toThrow('Unable to find an element');
+    });
   });
 });
