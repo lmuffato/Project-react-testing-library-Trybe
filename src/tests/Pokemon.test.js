59c8d7616d0535f44d7d61ece33362a45af6ff45
guidugaich <guidugaich.dev@gmail.com>
2021-05-27 15:31:14 -0300

    subrequisito 1 - req 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,29 @@
-describe('5. Testar componente Pokemon', () => {
-  test('é renderizado um card com as informações de determinado pokémon.', () => {});
+import React from 'react';
+import renderWithRouter from "../helpers/renderWithRouter";
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
+
+describe('6. Testar componente <Pokemon />', () => {
+  const pikachu = pokemons[0];
+
+  test('é renderizado um card com as informações de determinado pokémon.', () => {
+    const { getByTestId, getByAltText } = renderWithRouter(
+      <Pokemon
+        pokemon={ pikachu }
+        isFavorite="true"
+      />,
+    );
+
+    expect(getByTestId('pokemon-name')).toHaveTextContent('Pikachu');
+    expect(getByTestId('pokemon-type')).toHaveTextContent('Electric');
+    expect(getByTestId('pokemon-weight')).toHaveTextContent('Average weight: 6.0 kg');
+
+    const imgPath = 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    const image = getByAltText('Pikachu sprite');
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain(imgPath);
+  });
+  
   test('o card do Pokémon contém um link para exibir mais detalhes', () => {});
   test('ao clicar no link, é feito o redirecionamento para a página de detalhes', () => {});
   test('a URL exibida no navegador muda para `/pokemon/<id>`', () => {});
