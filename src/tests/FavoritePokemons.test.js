8021c2e5638415f57f55ae2ac4821212f8ff3f0a
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-05-27 09:34:59 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -15,7 +15,23 @@ describe('Favorite Pokemons', () => {
 
   test('é exibido todos os cards de pokémons favoritados', () => {
     RenderWithRouter(<App />);
-    const aboutLink = screen.getAllByRole('link')[2];
-    userEvent.click(aboutLink);
+    // clico no more datails link
+    const detailsLink = screen.getByText('More details');
+    userEvent.click(detailsLink);
+
+    // clico na checkbok para adicionar as favoritos
+    const addFavorito = screen.getByRole('checkbox');
+    userEvent.click(addFavorito);
+    // expect(checkbox).toBeChecked(); // faço um expect para ter
+
+    // clico no link de pokemons favoritos
+    const favPokemonComponent = screen.getByText('Favorite Pokémons');
+    userEvent.click(favPokemonComponent);
+
+    // verifico se existe o texto More details pois assim é sinal que existe o card do pokemon na pagina de favoritos
+    const pokemonDetail = screen.getByText('More details');
+    expect(pokemonDetail).toBeInTheDocument();
+
+    // tirei essa ideia do pull request do colega de turma Eduardo Seije https://github.com/tryber/sd-010-a-project-react-testing-library/pull/20/files
   });
 });
