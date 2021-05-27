5c80094650fd4f51849b9be14ca88f9e5573737c
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-27 16:11:02 -0300

    Requisito 2 segundo item

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,11 +3,23 @@ import About from '../components/About';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('Test About', () => {
-  test('', () => {
+  test('testing h2 heading ', () => {
     const { getByRole } = renderWithRouter(<About />);
 
-    const pokedex = getByRole('heading', { name: /about pokédex/i });
+    const pokedex = getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
 
     expect(pokedex).toBeInTheDocument();
+  });
+  test('testing pokedex paragraphs ', () => {
+    const { getByText } = renderWithRouter(<About />);
+
+    const paragraph1 = getByText(/this application simulates a pokédex/i);
+    const paragraph2 = getByText(/One can filter Pokémons by type/i);
+
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
   });
 });
