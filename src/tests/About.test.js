c783fa3bc595556fd3398fca746b1799f78fa684
rcalvs <rafaelcalvette@gmail.com>
2021-05-27 14:56:49 -0300

    about test 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -5,7 +5,8 @@ import renderWithRouter from '../services/renderWithRouter';
 describe('About tests',() => {
   it('mostra Pokédex quando a rota é `/about`', () => {
     const { getByText, history } = renderWithRouter(<About />);
-    // const pathname = history.location.pathname; //history ainda no :/
+    // const pathname = history.location.pathname; 
+    // history ainda no :/, teste pode ser feito unitario
     // expect(pathname).toBe('/about');
     expect(getByText('About Pokédex')).toBeInTheDocument();
   });
@@ -13,6 +14,20 @@ describe('About tests',() => {
   it('mostra image da Pokédex', () => {
     const { getByAltText } = renderWithRouter(<About />);
     expect(getByAltText(/Pokédex/i)).toBeInTheDocument();
+  });
+
+  it('mostra um h2', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading2 = getByRole('heading', {
+      level: 2
+    });
+    expect(heading2).toBeInTheDocument();
+  });
+
+  it('mostra 2 p com texto', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const paragraphs = getAllByText(/Pokémons/i);
+    expect(paragraphs.length).toBe(2);
   });
 
 });
