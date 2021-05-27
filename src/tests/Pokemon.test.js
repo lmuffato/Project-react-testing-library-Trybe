350bfc29506238a3e1309b60fa2d6bd9fdd01ba2
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-27 12:28:19 -0300

    feat: requirement06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 describe('Test the <Pokemon /> component', () => {
   it('Test if a card is rendered with the information of a certain Pokémon.', () => {
     const { getByRole, getByText,
-      getAllByText, container } = renderWithRouter(<App />);
+      getAllByText, container } = render(<MemoryRouter><App /></MemoryRouter>);
 
     const cardPokemon = container.querySelector('.pokemon');
 
@@ -22,5 +25,47 @@ describe('Test the <Pokemon /> component', () => {
     expect(cardPokemon).toContainElement(typePokemon);
     expect(cardPokemon).toContainElement(weightPokemon);
     expect(cardPokemon).toContainElement(imagePokemon);
+  });
+  it('Test if the Pokémon card contains a navigation link to view details', () => {
+    const { getByRole, container } = render(<MemoryRouter><App /></MemoryRouter>);
+
+    const linkToDetails = getByRole('link', {
+      name: /more details/i,
+    });
+    expect(linkToDetails).toHaveAttribute('href', '/pokemons/25');
+    const cardPokemon = container.querySelector('.pokemon');
+    expect(cardPokemon).toContainElement(linkToDetails);
+  });
+
+  it('Test if clicking on the link redirects the application to details page.', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const linkToDetails = getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(linkToDetails);
+    const path = history.location.pathname;
+    expect(path).toBe('/pokemons/25');
+    expect(getByRole('heading', {
+      name: /pikachu details/i,
+    })).toBeInTheDocument();
+  });
+
+  it('Test if there is a star icon on favorite Pokémon.', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const linkToDetails = getByRole('link', {
+      name: /more details/i,
+    });
+    userEvent.click(linkToDetails);
+    const checkboxFavorite = getByRole('checkbox', {
+      name: /pokémon favoritado\?/i,
+    });
+    userEvent.click(checkboxFavorite);
+    expect(checkboxFavorite).toBeChecked();
+    const iconFavorite = getByRole('img', {
+      name: /pikachu is marked as favorite/i,
+    });
+    expect(iconFavorite).toHaveAttribute('src', '/star-icon.svg');
+    expect(iconFavorite).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+    expect(iconFavorite).toBeInTheDocument();
   });
 });
