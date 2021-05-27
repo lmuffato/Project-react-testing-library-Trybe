ea76c9b30c1e9b97a162e83c5edf5a7aad9d319f
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-27 20:48:13 -0300

    feat: item 1 requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Componente PokemonDetails', () => {
+  test('Teste se as informações detalhadas'
+  + 'do Pokémon selecionado são mostradas na tela', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const linkMoreDatails = getByText('More details');
+    fireEvent.click(linkMoreDatails);
+    expect(linkMoreDatails).not.toBeInTheDocument();
+    const title = getByText(`${pokemons[0].name} Details`);
+    expect(title).toBeInTheDocument();
+  });
+});
