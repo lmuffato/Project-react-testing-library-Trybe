53e7077a0b7008611f676c903e84f6e539c19ca0
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-27 15:57:14 -0300

    Requisito 1 sem mutantes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,41 +1,63 @@
-import userEvent from '@testing-library/user-event';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+// import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 
 describe('Test App', () => {
   test('shows the Pokédex when the route is `/`', () => {
     const { getByText } = renderWithRouter(<App />);
+
     const home = getByText('Encountered pokémons');
+
     expect(home).toBeInTheDocument();
   });
+
   test('testing Home link', () => {
-    const { getByRole } = renderWithRouter(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
+    const { getByRole, history: { location: { pathname } } } = renderWithRouter(<App />);
 
     const homeLink = getByRole('link', {
       name: /home/i,
     });
     userEvent.click(homeLink);
+    expect(pathname).toBe('/');
   });
 
   test('testing About link', () => {
-    const { getByRole } = renderWithRouter(<App />);
+    const { getByRole, history } = renderWithRouter(<App />);
+
     const aboutLink = getByRole('link', {
       name: /about/i,
     });
+
     userEvent.click(aboutLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/about');
   });
 
   test('testing Favorite link', () => {
-    const { getByRole } = renderWithRouter(<App />);
+    const { getByRole, history } = renderWithRouter(<App />);
+
     const favLink = getByRole('link', {
       name: /favorite/i,
     });
+
     userEvent.click(favLink);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('testing Page not found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/route-not-exists');
+
+    const notExists = getByRole('heading', {
+      name: /page requested not found/i,
+    });
+
+    expect(notExists).toBeInTheDocument();
   });
 });
