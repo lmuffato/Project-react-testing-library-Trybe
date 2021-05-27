54ede96af2da69f9ac9b1c1375728b914622fd50
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-27 13:37:21 -0400

    feat: About page Tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { MemoryRouter as BrowserRouter } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
 
@@ -12,7 +9,20 @@ describe('About.test.js', () => {
     const { getByRole, getByText } = renderAboutWithRouter();
     const AboutTitle = getByRole('heading', { name: /about pokédex/i, level: 2 });
     expect(AboutTitle).toBeInTheDocument();
-    const paragraphers = getByText(/Pokémons/i);
-    console.log(paragraphers);
+    const paragraphers = [];
+    let text1 = 'This application simulates a Pokédex,';
+    text1 += ' a digital encyclopedia containing all Pokémons';
+    let text2 = 'One can filter Pokémons by type,';
+    text2 += ' and see more details for each one of them';
+    paragraphers.push(getByText(text1));
+    paragraphers.push(getByText(text2));
+    paragraphers.forEach((paragraph) => expect(paragraph).toBeInTheDocument());
+    expect(paragraphers.length).toBe(2);
+  });
+  test('Verfica se há uma imagem que tem como fonte um Link definido', () => {
+    const { getByRole } = renderAboutWithRouter();
+    const aboutImageSrc = getByRole('img').getAttribute('src');
+    const imageLink = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(aboutImageSrc).toBe(imageLink);
   });
 });
