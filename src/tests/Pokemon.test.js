7f6badd03aeec5343eeffbf75da97203b647664e
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-05-26 21:53:01 -0300

    fix: lint refactor requirement03

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,12 +4,23 @@ import App from '../App';
 
 describe('Test the <Pokemon /> component', () => {
   it('Test if a card is rendered with the information of a certain Pokémon.', () => {
-    const { getByRole, getByText, getAllByText, debug } = renderWithRouter(<App />);
+    const { getByRole, getByText,
+      getAllByText, container } = renderWithRouter(<App />);
 
-    expect(getByText(/pikachu/i)).toBeInTheDocument();
+    const cardPokemon = container.querySelector('.pokemon');
 
-    expect(getAllByText(/electric/i)[0]).toBeInTheDocument();
+    const namePokemon = getByText(/pikachu/i);
+    const typePokemon = getAllByText(/electric/i)[0];
+    const weightPokemon = getByText(/average weight: 6\.0 kg/i);
+    const imagePokemon = getByRole('img', {
+      name: /pikachu sprite/i,
+    });
+    expect(imagePokemon).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(imagePokemon).toHaveAttribute('alt', 'Pikachu sprite');
 
-    expect(getByText(/average weight: 6\.0 kg/i)).toBeInTheDocument();
+    expect(cardPokemon).toContainElement(namePokemon);
+    expect(cardPokemon).toContainElement(typePokemon);
+    expect(cardPokemon).toContainElement(weightPokemon);
+    expect(cardPokemon).toContainElement(imagePokemon);
   });
 });
