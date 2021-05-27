de9e33a8bee4d550c833e0b821b3f5c5a0f79e00
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-27 17:21:30 -0300

    refactor(Pokedex.test.js): Refatora testes do componente para ficar mais organizado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,61 +2,168 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
-import App from '../App';
 import Pokedex from '../components/Pokedex';
 
-describe('', () => {
-  it('renders a heading with the text `Encountered pokémons`', () => {
-    renderWithRouter(<App />);
+const testModel = (type) => [{
+  id: 25,
+  name: 'Pikachu',
+  type,
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+},
+{
+  id: 4,
+  name: 'Charmander',
+  type,
+  averageWeight: {
+    value: '8.5',
+    measurementUnit: 'kg',
+  },
+}];
+
+const testModelOriginal = () => [{
+  id: 25,
+  name: 'Pikachu',
+  type: 'Electric',
+  averageWeight: {
+    value: '6.0',
+    measurementUnit: 'kg',
+  },
+},
+{
+  id: 4,
+  name: 'Charmander',
+  type: 'Fire',
+  averageWeight: {
+    value: '8.5',
+    measurementUnit: 'kg',
+  },
+}];
+
+const renderPokedex = (model) => {
+  renderWithRouter(<Pokedex
+    pokemons={ model }
+    isPokemonFavoriteById={ { 4: true, 25: false } }
+  />);
+};
+
+const pokemonTypeText = 'pokemon-type';
+
+const testButton = (buttonName) => {
+  const type = new RegExp(buttonName, 'i');
+  let pokemonType = screen.getByTestId(pokemonTypeText);
+  expect(pokemonType.textContent).toMatch(type);
+  const nextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+  userEvent.click(nextPokemon);
+  const pokemon = screen.getByText(/Charmander/i);
+  expect(pokemon).toBeInTheDocument();
+  pokemonType = screen.getByTestId(pokemonTypeText);
+  expect(pokemonType.textContent).toMatch(type);
+};
+describe('header and nextPokemon button', () => {
+  it('render heading with text Encountered Pokemons', () => {
+    renderPokedex(testModelOriginal());
     const heading = screen
       .getByRole('heading', { level: 2, name: /Encountered Pokémons/i });
     expect(heading).toBeInTheDocument();
   });
+
   it('rendering next pokemon, when clicking on the button', () => {
-    renderWithRouter(<App />);
+    renderPokedex(testModelOriginal());
     const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
     userEvent.click(nextPokemonButton);
     let pokemon = screen.getByText(/Charmander/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Caterpie/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Ekans/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Alakazam/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Mew/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Rapidash/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Snorlax/i);
-    expect(pokemon).toBeInTheDocument();
-    userEvent.click(nextPokemonButton);
-    pokemon = screen.getByText(/Dragonair/i);
     expect(pokemon).toBeInTheDocument();
     userEvent.click(nextPokemonButton);
     pokemon = screen.getByText(/Pikachu/i);
     expect(pokemon).toBeInTheDocument();
   });
 
-  it('test button Electric', () => {
-    renderWithRouter(<App />);
+  it('Disables next pokemon button when there is only one pokemon.', () => {
+    renderPokedex(testModelOriginal());
     const button = screen.getByRole('button', { name: /electric/i });
     userEvent.click(button);
-    const type = screen.getByTestId('pokemon-type');
-    expect(type).toBeInTheDocument();
+    const nextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+    expect(nextPokemon).toBeDisabled();
+  });
+});
+
+describe('pokemon type buttons tests', () => {
+  it('test button Electric', () => {
+    renderPokedex(testModel('Electric'));
+    const button = screen.getByRole('button', { name: /electric/i });
+    userEvent.click(button);
+    testButton('electric');
   });
 
-  it('test button Electric', () => {
-    renderWithRouter(<App />);
-    const button = screen.getByRole('button', { name: /electric/i });
+  it('test button Fire', () => {
+    renderPokedex(testModel('Fire'));
+    const button = screen.getByRole('button', { name: /fire/i });
     userEvent.click(button);
-    const type = screen.getByTestId('pokemon-type');
-    expect(type).toBeInTheDocument();
+    testButton('fire');
+  });
+
+  it('test button Bug', () => {
+    renderPokedex(testModel('Bug'));
+
+    const button = screen.getByRole('button', { name: /bug/i });
+    userEvent.click(button);
+    testButton('bug');
+  });
+
+  it('test button Poison', () => {
+    renderPokedex(testModel('Poison'));
+
+    const button = screen.getByRole('button', { name: /poison/i });
+    userEvent.click(button);
+    testButton('poison');
+  });
+
+  it('test button Psychic', () => {
+    renderPokedex(testModel('Psychic'));
+
+    const button = screen.getByRole('button', { name: /psychic/i });
+    userEvent.click(button);
+    testButton('psychic');
+  });
+
+  it('test button Normal', () => {
+    renderPokedex(testModel('Normal'));
+
+    const button = screen.getByRole('button', { name: /normal/i });
+    userEvent.click(button);
+    testButton('normal');
+  });
+
+  it('test button Dragon', () => {
+    renderPokedex(testModel('Dragon'));
+
+    const button = screen.getByRole('button', { name: /dragon/i });
+    userEvent.click(button);
+    testButton('dragon');
+  });
+
+  it('test button All', () => {
+    renderWithRouter(<Pokedex
+      pokemons={ testModelOriginal() }
+      isPokemonFavoriteById={ { 4: true, 25: false } }
+    />);
+    const button = screen.getByRole('button', { name: /all/i });
+    userEvent.click(button);
+    let pokemonType = screen.getByTestId(pokemonTypeText);
+    expect(pokemonType.textContent).toMatch(/electric/i);
+    const nextPokemon = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(nextPokemon);
+    const pokemon = screen.getByText(/Charmander/i);
+    expect(pokemon).toBeInTheDocument();
+    pokemonType = screen.getByTestId(pokemonTypeText);
+    expect(pokemonType.textContent).toMatch(/fire/i);
+  });
+
+  it('dinamic buttons test ', () => {
+    renderPokedex(testModelOriginal());
+    expect(screen.getAllByTestId('pokemon-type-button')).toHaveLength(2);
   });
 });
