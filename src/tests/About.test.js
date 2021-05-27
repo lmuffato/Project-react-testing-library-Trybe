6468513bbf4003d215f6b81a218d23bf78580cc1
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-27 20:10:28 -0300

    testa se retorna imagem correta

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
 import { render } from '@testing-library/react';
-import App from '../App';
+import About from '../components/About';
 
 test('tests return of application details', () => {
   const { getByText } = render(
     <MemoryRouter initialEntries={ ['/about'] }>
-      <App />
+      <About />
     </MemoryRouter>,
   );
   const heading = getByText(/About Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+
+test('render image in "/About"', () => {
+  const { getByRole } = render(
+    <MemoryRouter initialEntries={ ['/about'] }>
+      <About />
+    </MemoryRouter>,
+  );
+  const img = getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
+  expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
 });
