8fde191b2f9103b94c68aec12a8c6c6d49780917
guidugaich <guidugaich.dev@gmail.com>
2021-05-27 11:56:45 -0300

    estrutura para testes do Pokemon.js

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+describe('5. Testar componente Pokemon', () => {
+  test('é renderizado um card com as informações de determinado pokémon.', () => {});
+  test('o card do Pokémon contém um link para exibir mais detalhes', () => {});
+  test('ao clicar no link, é feito o redirecionamento para a página de detalhes', () => {});
+  test('a URL exibida no navegador muda para `/pokemon/<id>`', () => {});
+  test('existe um ícone de estrela nos Pokémons favoritados.', () => {});
+});
