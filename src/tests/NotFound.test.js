2cf5c48e639522f3875241752c17534bc1a01473
hheloisa <hhackenhaar@gmail.com>
2021-05-26 21:03:48 -0300

    Inicia testes de <NotFound /> via url alterada

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente <NotFound />', () => {
+  test('testa se com url desconhecida, acessa a pagina notFound', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/xablau');
+    const notFound = screen.getByText(/Page requested not found/);
+    expect(notFound).toBeInTheDocument();
+  });
+});
