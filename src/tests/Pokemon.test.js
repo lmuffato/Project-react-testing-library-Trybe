a0638ca568fed60c49ef855e06a2995da89b0be0
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-27 18:22:54 -0300

    feat: Teste se existe um ícone de estrela nos Pokémons favoritados

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 import pokemons from '../data';
@@ -34,5 +35,33 @@ describe('Componente Pokemon', () => {
     expect(img).toBeInTheDocument();
     expect(img.src).toContain(pokemons[0].image);
     expect(img.alt).toContain(`${pokemons[0].name} sprite`);
+  });
+
+  test('Teste se o link de navegação do Pokémon, redireciona para seus detalhes', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const url = `pokemons/${pokemons[0].id}`;
+    const link = getAllByRole('link', { name: 'More details' });
+    const linkMoreDatails = link[0];
+    expect(linkMoreDatails).toBeInTheDocument();
+    expect(linkMoreDatails.href).toContain(url);
+  });
+
+  test('Teste se existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getAllByRole, getByRole, getByAltText } = renderWithRouter(<App />);
+    const imgs = getAllByRole('img');
+    expect(imgs.length).toBe(1);
+    const link = getAllByRole('link', { name: 'More details' });
+    const linkMoreDatails = link[0];
+    expect(linkMoreDatails).toBeInTheDocument();
+    fireEvent.click(linkMoreDatails);
+    expect(linkMoreDatails).not.toBeInTheDocument();
+    const check = getByRole('checkbox');
+    expect(check).toBeInTheDocument();
+    expect(check.checked).toEqual(false);
+    fireEvent.click(check);
+    expect(check.checked).toEqual(true);
+    const favoriteImg = getByAltText(`${pokemons[0].name} is marked as favorite`);
+    expect(favoriteImg).toBeInTheDocument();
+    expect(favoriteImg.src).toContain('/star-icon.svg');
   });
 });
