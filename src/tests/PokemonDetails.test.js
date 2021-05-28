eaca2e8dd4dcedb0b594ab9b56af4437d134ec27
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-28 15:18:42 -0300

    feat(PokemonDetails.test.js): verifica se nome do pokemon e renderizado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('renders pokemon info', () => {
+  it('render pokemon name', () => {
+    renderWithRouter(<App />);
+    const pokemonDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(pokemonDetails);
+    const name = screen.getByTestId('pokemon-name');
+    expect(name.textContent).toBe('Pikachu');
+  });
+});
