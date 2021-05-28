3a0e197513c9c3ca1f1810f8ec953478ea371975
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-05-28 10:49:23 -0300

    NotFound done

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('NotFound.js tests', () => {
+  test('se tem um headind lvl 2', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    // uso da doc https://testing-library.com/docs/queries/byrole
+    const headingText = getByRole('heading', {
+      name: /Page requested not found/,
+      level: 2,
+    });
+    expect(headingText).toBeInTheDocument();
+  });
+
+  test('verificar img', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const altIs = getByAltText('Pikachu crying because the page requested was not found');
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(altIs.getAttribute('src')).toBe(src);
+  });
+});
