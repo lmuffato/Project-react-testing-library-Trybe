45abe6949279b75d1bbe7838a69dfc80411d5e14
Nathalia Zebral <natzebral@gmail.com>
2021-05-28 18:51:10 -0300

    requisito 3 - componente FavoritePokemosn passando no stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('have no favorite Pokémons', () => {
+  it('text No favorite pokemons Found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
