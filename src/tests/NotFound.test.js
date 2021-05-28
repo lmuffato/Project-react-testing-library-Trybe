efb3cc7b6e33ec8bbf7a9ec7b7bb440bed5cea35
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-28 13:02:59 -0400

    feat: notFound tests

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+const renderAppWithRouter = () => renderWithRouter(<App />);
+
+describe('NotFound.test.js', () => {
+  test('Exibe um h2 com o texto "Page requested not found"', () => {
+    const { getByRole, history } = renderAppWithRouter();
+    history.push('lets-test');
+    const notFoundText = getByRole('heading', {
+      name: 'Page requested not found Crying emoji',
+      level: 2 });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Exibe uma imagem com um link pré definido', () => {
+    const { getByAltText, history } = renderAppWithRouter();
+    history.push('another-test');
+    const altText = /Pikachu crying because the page requested was not found/i;
+    const imageNotFound = getByAltText(altText);
+    expect(imageNotFound).toBeInTheDocument();
+  });
+});
