eb8c27b623d48624984b5ddd2b8bbd6f72c729f7
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-05-28 19:24:42 -0300

    feat:

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,6 @@
 // import { render } from '@testing-library/react';
+// import { getByText } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import Pokedex from '../components/Pokedex';
 import pokemons from '../data';
@@ -16,5 +18,51 @@ describe('testando component pokedex', () => {
     />);
     const h2 = getByRole('heading', { level: 2, name: 'Encountered pokémons' });
     expect(h2).toBeInTheDocument();
+  });
+  test('testando se apos clicar no botao aparece o pokemon', () => {
+    const { getByRole, getByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+    const button = getByRole('button', { name: 'Próximo pokémon' });
+    expect(button).toBeInTheDocument();
+    pokemons.forEach((pokemon) => {
+      const result = getByText(pokemon.name);
+      expect(result).toBeInTheDocument();
+      userEvent.click(button);
+    });
+    expect(getByText(pokemons[0].name)).toBeInTheDocument();
+  });
+  test('testando filtros', () => {
+    const { getByRole, queryByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+    const button = getByRole('button', { name: 'Electric' });
+    expect(button).toBeInTheDocument();
+    const electric = pokemons.filter(({ type }) => type === 'Electric');
+    electric.forEach((pokemon) => {
+      expect(queryByText(pokemon.name)).toBeInTheDocument();
+    });
+  });
+  test('testando filtros', () => {
+    const { getByRole, queryByText } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+    const button = getByRole('button', { name: 'All' });
+    expect(button).toBeInTheDocument();
+    userEvent.click(button);
+    expect(queryByText(pokemons[0].name)).toBeInTheDocument();
+  });
+  test('Se é criado um botão de tipo dinamicamente', () => {
+    const { queryByText, getAllByTestId } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+    const pokeType = queryByText('Poison');
+    expect(pokeType).toBeInTheDocument();
+    const pokeTestId = getAllByTestId('pokemon-type-button');
+    expect(pokeTestId[1]).toBeInTheDocument();
   });
 });
