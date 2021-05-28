c61ccbb1d504af09c43b184bd34b48975d03dbb6
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-05-28 13:21:57 -0300

    Erros lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,11 +1,20 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import React from 'react';
 import Pokedex from '../components/Pokedex';
+import pokemons from '../data';
+import renderWithRouter from './renderWithRouter';
+// Ajuda do Murilo Gonçalvez;
+const favoritePokemons = pokemons.reduce(
+  (acc, current) => ({ ...acc, [current.id]: false }), {},
+);
 
 describe('testando component pokedex', () => {
   test('testando se contem um h2 com o texto correto', () => {
-    const { getByText } = render(<Pokedex />);
-    const h2 = getByText('heading', { level: 2, name: 'Encountered pokémons' });
-
+    const { getByRole } = renderWithRouter(<Pokedex
+      pokemons={ pokemons }
+      isPokemonFavoriteById={ favoritePokemons }
+    />);
+    const h2 = getByRole('heading', { level: 2, name: 'Encountered pokémons' });
+    expect(h2).toBeInTheDocument();
   });
 });
