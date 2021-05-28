ef15d7eab110c37345c3684aaebac4e6519f179b
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-27 21:58:40 -0300

    testa se retorna texto 'Page requested not found'

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+
+import { MemoryRouter } from 'react-router';
+import { render } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+
+test('render text `Page requested not found 😭`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/*'] }>
+      <NotFound />
+    </MemoryRouter>,
+  );
+  const text = getByText(/Page requested not found/i);
+  expect(text).toBeInTheDocument();
+});
