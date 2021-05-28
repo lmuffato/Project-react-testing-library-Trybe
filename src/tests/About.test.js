3bc08bd2cbc943fb05955dedda6b4730f5231e18
rogerio <rogerio.cl@gmail.com>
2021-05-28 18:53:21 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const screen = renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const screen = renderWithRouter(<About />);
+    const p1 = screen.getByText(/this application simulates/i);
+    const p2 = screen.getByText(/One can filter Pokémons by type/i);
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    const screen = renderWithRouter(<About />);
+    const img = screen.getByAltText('Pokédex');
+    expect(img).toBeInTheDocument();
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(src);
+  });
+});
