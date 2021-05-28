0803e0ec6b03ebea3cdbf3d1d67522acc20014f8
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-27 21:31:26 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+/* import { MemoryRouter } from 'react-router-dom'; */
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se página contém um h2 com texto "Page requested not found"', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const h2Text = getByText(/Page requested not found/i);
+  expect(h2Text).toBeInTheDocument();
+});
+
+test('Teste se página mostra uma imagem', () => {
+  const { getAllByRole } = renderWithRouter(<NotFound />);
+  const tagImg = getAllByRole('img');
+  const linkImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  expect(tagImg[1].src).toBe(linkImg);
+  expect(tagImg[1]).toBeInTheDocument();
+});
