1874e1071fc687760fd589119370b86672c0f3b5
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-28 14:23:18 -0300

    feat(Pokemon.test.js): verifica se renderiza o peso do pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,15 +4,21 @@ import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 describe('render pokemon card itens', () => {
-  it('pokemon name', () => {
+  it('render pokemon name', () => {
     renderWithRouter(<App />);
     const pokemonName = screen.getByText(/pikachu/i);
     expect(pokemonName).toBeInTheDocument();
   });
 
-  it('pokemon name', () => {
+  it('render pokemon type', () => {
     renderWithRouter(<App />);
     const pokemonType = screen.getByTestId('pokemon-type');
     expect(pokemonType.textContent).toMatch(/electric/i);
+  });
+
+  it('render pokemon weight', () => {
+    renderWithRouter(<App />);
+    const pokemonWeight = screen.getByText(/average weight: 6\.0 kg/i);
+    expect(pokemonWeight).toBeInTheDocument();
   });
 });
