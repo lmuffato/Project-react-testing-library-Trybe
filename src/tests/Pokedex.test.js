e3168621be86c50dc8f123d2aca74a96867c4721
GetulioGutemberg <getulio.dev@gmail.com>
2021-05-27 22:35:34 -0300

    testa se retorna texto "Encountered pokémons"

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+
+import App from '../App';
+
+test('render text "Encountered pokémons"', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+  const text = getByText(/Encountered pokémons/i);
+  expect(text).toBeInTheDocument();
+});
