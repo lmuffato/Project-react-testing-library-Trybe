2bd6703f1f99fdb7d0a0d224b80dab0e122b54cb
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-27 23:06:09 -0300

    Requisito 5 Parte 1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+/* import Pokedex from '../components/Pokedex'; */
+import App from '../App';
+
+test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const tagH2 = getByRole('heading', {
+    level: 2,
+    name: /encountered pokémons/i,
+  });
+  expect(tagH2).toBeInTheDocument();
+});
+
+test('Testa se é exibido o próximo Pokémon quando o clica no botão', () => {
+  const { getByRole, getByTestId } = renderWithRouter(<App />);
+  const pokeButton = getByRole('button', { name: /próximo pokémon/i });
+  userEvent.click(pokeButton);
+  const idPoke = getByTestId('pokemon-name');
+  expect(idPoke.innerHTML).toBe('Charmander');
+
+  userEvent.click(pokeButton);
+  userEvent.click(pokeButton);
+  userEvent.click(pokeButton);
+  userEvent.click(pokeButton);
+
+  expect(idPoke.innerHTML).toBe('Mew');
+});
