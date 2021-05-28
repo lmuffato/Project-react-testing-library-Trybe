ecd3867cec93155a31eeb036e37976ddacfa379d
Nathalia Zebral <natzebral@gmail.com>
2021-05-28 19:28:05 -0300

    requisito 4 - componente NotFound  passando no stryker

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('test component Not Found', () => {
+  it('heading h2 have Text Not Found', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const textNotFound = getByText(/page requested not found/i);
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  it('have especific gif link on src', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imageNotFound = getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
