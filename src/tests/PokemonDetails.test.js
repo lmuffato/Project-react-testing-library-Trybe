65c3b50feac1f7021b54acc536a89cd7d169ef1f
Marcus C. H. Filho <mchusemann@gmail.com>
2021-05-28 15:27:58 -0300

    feat(PokemonDetails.test.js): verifica se o header do pokemon details e renderizado

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -5,5 +5,11 @@ import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
 
 describe('renders pokemon info', () => {
-
+  it('render h2 with text Pikachu Details', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const heading = screen.getByRole('heading', { level: 2, name: /pikachu details/i });
+    expect(heading).toBeInTheDocument();
+  });
 });
