74e3266548fc5462cc9aa2d12e16c6e4ecf4b0aa
guilhermemd <guilherme.md@gmail.com>
2021-05-28 01:31:48 -0300

    test: :white_check_mark: requisito 06 pela metade
    push feito para continuar o teste 6 amanha
    
    none

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,14 +1,29 @@
-// import React from 'react';
-// // import userEvent from '@testing-library/user-event';
-// import renderWithRouter from './renderWithRouter';
-// import Pokedex from '../components/Pokedex';
+import React from 'react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import App from '../App';
+import pokemons from '../data';
 
-// describe('testing all screen application of the Pokedex', () => {
-//   it('testing screen h2 "Encountered pokémons"', () => {
-//     const { getByRole } = renderWithRouter(<Pokedex />);
-//     const h2 = getByRole('heading', {
-//       name: /Encountered pokémons/i,
-//       level: 2 });
-//     expect(h2).toBeInTheDocument();
-//   });
-// });
+describe('Testing Component Pokemon', () => {
+  it('testing informations of pokemon card', () => {
+    const { getByTestId, getByAltText } = renderWithRouter(<App />);
+    const pokemonName = getByTestId('pokemon-name');
+    expect(pokemonName.innerHTML).toBe('Pikachu');
+    const pokemonType = getByTestId('pokemon-type');
+    expect(pokemonType.innerHTML).toBe('Electric');
+    const pokemonWeight = getByTestId('pokemon-Weight');
+    expect(pokemonWeight.innerHTML).toBe('Average weight: 6.0 kg');
+    const pokemonImg = getByAltText('Pikachu sprite');
+    expect(pokemonImg.src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  it('Checking Pathname of pikachu card', () => {
+    const { getByRole, history } = renderWithRouter(<Pokemon pokemon={ pokemons[0] } />);
+    const detailsBtn = getByRole('button', {
+      nome: /more details/i,
+    });
+    userEvent.click(detailsBtn);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+});
