509d81e2cb6b81bf9215e1190e861d80c38be185
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-29 11:52:45 -0300

    test: testing if About page contain a heading h2 - requirement 02

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testa o componente "About"', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByText, getByRole } = render(<About />);
+
+    const about = getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/i,
+    });
+    expect(about).toBeInTheDocument();
+  });
+});
