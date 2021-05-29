1905456f26581cecbf03124b484c11e70f6eda30
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-05-29 20:25:34 -0300

    feat: requirement 5 - refactored

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,36 +1,56 @@
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import React from 'react';
-import Pokedex from '../components/Pokedex';
+import App from '../App';
 import renderWithRouter from '../services/renderWithRouter';
 import pokemons from '../data';
 
 describe('Test component <Pokedex />', () => {
-  test('Test heading', () => {
-    const { getByRole } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ {} }
-    />);
-    const heading = getByRole('heading', {
-      name: /Encountered pokémons/i,
+  test('Test page heading', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/');
+    const encounteredPkms = getByRole('heading', {
+      name: 'Encountered pokémons',
       level: 2,
     });
-    expect(heading).toBeInTheDocument();
+    expect(encounteredPkms).toBeInTheDocument();
   });
-  test('Test next button', () => {
-    const { getByRole } = renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ {} }
-    />);
-    const btn = getByRole('button', {
-      name: /Próximo pokémon/i,
+
+  test('Test only one pokemon and next button', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/');
+    expect(screen.getByText('Próximo pokémon')).toBeInTheDocument();
+    const pkmName = screen.getByTestId('pokemon-name');
+    expect(pkmName).toBeInTheDocument();
+    expect(pkmName.innerHTML).toBe(pokemons[0].name);
+    const pokemonName = screen.getAllByTestId('pokemon-name');
+    expect(pokemonName).toHaveLength(1);
+    const pkmType = screen.getByTestId('pokemon-type');
+    expect(pkmType).toBeInTheDocument();
+    const pokemonType = screen.getAllByTestId('pokemon-type');
+    expect(pokemonType).toHaveLength(1);
+    const pkmWeight = screen.getByTestId('pokemon-weight');
+    expect(pkmWeight).toBeInTheDocument();
+    const pokemonWeight = screen.getAllByTestId('pokemon-weight');
+    expect(pokemonWeight).toHaveLength(1);
+    userEvent.click(screen.getByTestId('next-pokemon'));
+    expect(pkmName.innerHTML).toBe(pokemons[1].name);
+  });
+
+  test('Test filter buttons', () => {
+    const { history, getByRole } = renderWithRouter(<App />);
+    history.push('/');
+    const pkmListOfTypes = [...new Set(pokemons.map(({ type }) => type))];
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    expect(buttons).toHaveLength(pkmListOfTypes.length);
+    pkmListOfTypes.forEach((type, index) => {
+      expect(buttons[index]).toHaveTextContent(type);
     });
-    expect(btn).toBeInTheDocument();
-  });
-  test('Test if just one pokemon is on screen', () => {
-    renderWithRouter(<Pokedex
-      pokemons={ pokemons }
-      isPokemonFavoriteById={ {} }
-    />);
-    const poke = document.getElementsByClassName('pokemon-overview');
-    expect(poke.length).toBe(1);
+    const reset = getByRole('button', {
+      name: /all/i,
+    });
+    expect(reset).toBeInTheDocument();
   });
 });
+
+// Testes elaborados com base no PR da aluna Ana Ventura (https://github.com/tryber/sd-010-a-project-react-testing-library/pull/3/files)
