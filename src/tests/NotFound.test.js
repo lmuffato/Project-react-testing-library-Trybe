477da9ddc92505dba9b57425cf97d05f095312f2
rogerio <rogerio.cl@gmail.com>
2021-05-28 22:06:52 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 '
+  + 'com o texto Page requested not found 😭', () => {
+    const screen = renderWithRouter(<NotFound />);
+    const regexNotFound = /Page requested not found/i;
+    const heading = screen.getByRole('heading', { name: regexNotFound, level: 2 });
+    expect(heading).toBeInTheDocument();
+    const emoji = screen.getByLabelText('Crying emoji');
+    expect(emoji).toBeInTheDocument();
+  });
+  it('Teste se página mostra a imagem '
+  + 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    const screen = renderWithRouter(<NotFound />);
+    const altTextPikachu = 'Pikachu crying because the page requested was not found';
+    const pikachuImage = screen.getByAltText(altTextPikachu);
+    expect(pikachuImage).toBeInTheDocument();
+    const src = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(pikachuImage.src).toBe(src);
+  });
+});
