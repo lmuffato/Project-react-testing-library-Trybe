5f3a95f98ab7d511048bf1b8ae494b68b64f5933
William <w.willers@outlook.com>
2021-05-29 16:18:05 -0400

    App tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -21,4 +23,56 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  // https://testing-library.com/docs/example-react-router/#reducing-boilerplate
+  render(<App />, { wrapper: MemoryRouter });
+
+  const home = screen.getByText(/Home/);
+  expect(home).toBeInTheDocument();
+
+  const about = screen.getByText(/About/);
+  expect(about).toBeInTheDocument();
+
+  const favoritePokes = screen.getByText(/Favorite Pokémons/);
+  expect(favoritePokes).toBeInTheDocument();
+});
+
+// Utilizando getByRole ao invés do screen, funciona das duas formas
+test('should redirect to / when click on Home', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /Home/ }));
+
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/');
+});
+
+test('should redirect to /about when click on About', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /About/ }));
+
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/about');
+});
+
+test('should redirect to /favorites when click on Favorites Pokémons', () => {
+  const { getByRole, history } = renderWithRouter(<App />);
+
+  userEvent.click(getByRole('link', { name: /Favorite Pokémons/ }));
+
+  const { pathname } = history.location;
+
+  expect(pathname).toBe('/favorites');
+});
+
+test('should redirect to /favorites when click on Favorites Pokémons', () => {
+  const { history } = renderWithRouter(<App />);
+
+  history.push('/page/not-found');
+  expect(screen.getByText(/page requested not found/i)).toBeInTheDocument();
 });
