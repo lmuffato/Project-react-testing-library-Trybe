858cafd0f6a9f9602b09763eb58fc0e60cadc79b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-29 15:36:11 -0300

    test: looking for if has a img with alt espefic linked with src - requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente "NotFound"', () => {
+  it(`Teste se página contém um heading h2 com 
+    o texto Page requested not found 😭`, () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe');
+
+    const notFound = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    const { getByAltText, history } = renderWithRouter(<App />);
+    const textAlt = 'Pikachu crying because the page requested was not found';
+
+    history.push('/not-found');
+
+    const imageAlt = getByAltText(textAlt);
+
+    expect(imageAlt).toBeInTheDocument();
+  });
+});
