4ca62f56eaf6e21f14cad5a1cf60025555b93f47
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-29 12:40:51 -0300

    test: testing if a message show up on none pokemons is selected - requirement 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Testa o componente "FavoritePokemons"', () => {
+  it(`Teste se é exibido na tela a mensagem No favorite pokemon found, 
+    se a pessoa não tiver pokémons favoritos.`, () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const messageDefault = getByText('No favorite pokemon found');
+
+    expect(messageDefault).toBeInTheDocument();
+  });
+});
