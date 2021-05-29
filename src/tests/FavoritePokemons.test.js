0fd60493e3a6351dc9e06475824a07ba0776f56b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-29 14:52:23 -0300

    test: testing with mock if render the page with information - requirement 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,13 +1,84 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
 
 describe('Testa o componente "FavoritePokemons"', () => {
   it(`Teste se é exibido na tela a mensagem No favorite pokemon found, 
     se a pessoa não tiver pokémons favoritos.`, () => {
-    const { getByText } = render(<FavoritePokemons />);
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
     const messageDefault = getByText('No favorite pokemon found');
 
     expect(messageDefault).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const pokemons = [
+      {
+        id: 25,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Kanto Viridian Forest',
+            map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+          },
+          {
+            location: 'Kanto Power Plant',
+            map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+          },
+        ],
+        summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+      },
+      {
+        id: 4,
+        name: 'Charmander',
+        type: 'Fire',
+        averageWeight: {
+          value: '8.5',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Alola Route 3',
+            map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+          },
+          {
+            location: 'Kanto Route 3',
+            map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+          },
+          {
+            location: 'Kanto Route 4',
+            map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+          },
+          {
+            location: 'Kanto Rock Tunnel',
+            map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+          },
+        ],
+        summary: 'The flame on its tail shows the strength of its life force. If it is weak, the flame also burns weakly.',
+      },
+    ];
+
+    const { getAllByText } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const onlySelectedPokemons = getAllByText('More details');
+
+    expect(onlySelectedPokemons.length).toBe(pokemons.length);
+  });
+
+  it('Teste se nenhum card de pokémon é exibido, se ele não estiver favoritado.', () => {
+    const pokemons = [];
+
+    const { queryByText } = renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    const onlySelectedPokemons = queryByText('More details');
+
+    expect(onlySelectedPokemons).not.toBeInTheDocument();
   });
 });
