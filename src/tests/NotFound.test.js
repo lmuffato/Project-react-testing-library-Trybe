33a03979c84ef080cac7a745c90c6e45f606f8a7
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-05-29 19:06:09 -0300

    feat: requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Test component <NotFound />', () => {
+  test('Test heading', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const heading = getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('Test img', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const img = getByRole('img', { 
+      name: /Pikachu crying because the page requested was not found/i,
+    });
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
