8376477097db1da517fcba08c570cad385716439
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-29 14:35:48 -0300

    Requisito Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../services/renderWithRouter';
+
+describe('Test App', () => {
+  test('shows the Pokédex title', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const title = getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+
+    expect(title).toBeInTheDocument();
+  });
+  test('shows the next pokemon when the button is clicked', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+
+    const pokemon1 = getByText(/pikachu/i);
+    expect(pokemon1).toBeInTheDocument();
+
+    const nextButton = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+
+    userEvent.click(nextButton);
+
+
+    const pokemon2 = getByText(/charmander/i);
+    expect(pokemon2).toBeInTheDocument();
+  });
+});
