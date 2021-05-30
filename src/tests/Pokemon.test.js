2e17d6f091ef61df5defc055aa6ae26d987ef0fb
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-30 14:44:46 -0300

    test: Create testing for Pokemon component if render the informations with props - requirement 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import { userEvent } from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+
+const pokemonMock = {
+  id: 10,
+  name: 'Caterpie',
+  type: 'Bug',
+  averageWeight: {
+    value: '2.9',
+    measurementUnit: 'kg',
+  },
+  image: 'https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png',
+  moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Caterpie_(Pok%C3%A9mon)',
+  foundAt: [
+    {
+      location: 'Johto Route 30',
+      map: 'https://cdn2.bulbagarden.net/upload/7/76/Johto_Route_30_Map.png',
+    },
+    {
+      location: 'Johto Route 31',
+      map: 'https://cdn2.bulbagarden.net/upload/2/2b/Johto_Route_31_Map.png',
+    },
+    {
+      location: 'Ilex Forest',
+      map: 'https://cdn2.bulbagarden.net/upload/a/ae/Johto_Ilex_Forest_Map.png',
+    },
+    {
+      location: 'Johto National Park',
+      map: 'https://cdn2.bulbagarden.net/upload/4/4e/Johto_National_Park_Map.png',
+    },
+  ],
+  summary: `For protection, it releases a horrible stench from 
+    the antennae on its head to drive away enemies.`,
+};
+
+describe('Testa o componente "Pokemon"', () => {
+  it(`Teste se é renderizado um card com 
+  as informações de determinado pokémon`, () => {
+    const { getByText, getByRole } = renderWithRouter(
+      <Pokemon pokemon={ pokemonMock } isFavorite={ false } />,
+    );
+
+    const pokeName = getByText('Caterpie');
+    const pokeType = getByText('Bug');
+    const pokeWeight = getByText('Average weight: 2.9 kg');
+    const pokeImage = getByRole('img');
+
+    expect(pokeName).toBeInTheDocument();
+    expect(pokeType).toBeInTheDocument();
+    expect(pokeWeight).toBeInTheDocument();
+    expect(pokeImage.src).toBe('https://cdn2.bulbagarden.net/upload/8/83/Spr_5b_010.png');
+    expect(pokeImage.alt).toBe('Caterpie sprite');
+  });
+});
