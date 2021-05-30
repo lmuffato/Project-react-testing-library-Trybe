821a90f999c97d16cec3fd0c409608a639128737
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-05-30 16:09:05 -0300

    3rd requirement completed

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import { FavoritePokemons } from '../components';
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const heading = getByText(/No favorite pokemon found/i);
+  expect(heading).toBeInTheDocument();
+});
