60a2e213bbc93badf2d70c5656f7d5417c0ded96
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-30 16:06:15 -0300

    test: testing if infos about pokemon is correct when click on details - requirement 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+// import React from 'react';
+// import Pokedex from '../components/Pokedex';
+// import renderWithRouter from './renderWithRouter';
+
+// describe('Testa o componente "Pokedex".', () => {
+//   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+//     const { getByRole } = renderWithRouter(<Pokedex />);
+//   });
+// });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente "PokemonDetails"', () => {
+  it(`Teste se as informações detalhadas do Pokémon 
+    selecionado são mostradas na tela.`, () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const details = getByText('More details');
+    const pokeSummary = 'This intelligent Pokémon roasts hard berries'
+      + ' with electricity to make them tender enough to eat.';
+
+    userEvent.click(details);
+    const mainText = getByText('Pikachu Details');
+    const summary = getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+    const paragraph = getByText(pokeSummary);
+
+    expect(mainText).toBeInTheDocument();
+    expect(details).not.toBeInTheDocument();
+    expect(summary).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+  });
+});
