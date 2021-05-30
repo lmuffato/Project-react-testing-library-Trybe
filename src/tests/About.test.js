da69e67c972e16ced2daa671bf83b9c2e582f215
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-05-30 14:30:19 -0300

    Requisito 2 Testa o componente <About.js /.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import { About } from '../components';
+
+describe('Testa o componente <About.js /.', () => {
+  it('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const linkAbout = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+
+    const elementosP = screen.getAllByRole('p');
+    expect(elementosP.length).toBe(2);
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const img = screen.getByRole('img');
+    const srcImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(img.src).toBe(srcImg);
+  });
+});
