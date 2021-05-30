c414309bf49c2b670b4180f8eb4b59316fc6511c
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-05-30 16:18:58 -0300

    4th requirement completed

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import renderWithRouter from '../RenderWithRouter';
 import { FavoritePokemons } from '../components';
 
-test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+test('Teste se é exibido na tela a mensagem ', () => {
   const { getByText } = renderWithRouter(<FavoritePokemons />);
   const heading = getByText(/No favorite pokemon found/i);
   expect(heading).toBeInTheDocument();
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../RenderWithRouter';
+import { NotFound } from '../components';
+
+test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const heading = getByText(/Page requested not found/i);
+  expect(heading).toBeInTheDocument();
+});
+
+test('Teste se página mostra a imagem', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const text = 'Pikachu crying because the page requested was not found';
+  const pikachuImage = getByAltText(text);
+  expect(pikachuImage.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
