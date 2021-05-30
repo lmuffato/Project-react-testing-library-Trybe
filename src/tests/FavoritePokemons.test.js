c39169e020f88d88804ed8559330c0119e5a12a4
igson <igson660@gmail.com>
2021-05-30 10:08:58 -0500

    quesito 03 concluido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithHistory';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
-describe('testes do component About.js', () => {
+describe('testes do component FavoritePkemons', () => {
   it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons />);
     expect(getByText(/No favorite pokemon/i)).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados', () => {
+    const { history, getByRole, queryByText,
+      getByText } = renderWithRouter(<App />);
+
+    const moreDetailsLink = getByRole('link', {
+      name: /more details/i,
+    });
+
+    userEvent.click(moreDetailsLink);
+    expect(history.location.pathname).toBe('/pokemons/25');
+
+    const checkboxFavorite = getByRole('checkbox', {
+      name: /pokémon favoritado?/i,
+    });
+
+    userEvent.click(checkboxFavorite);
+
+    expect(checkboxFavorite).toBeChecked();
+
+    history.push('/favorites');
+    expect(queryByText(/no favorite pokemon found/i)).not.toBeInTheDocument();
+
+    expect(getByText(/pikachu/i)).toBeInTheDocument();
   });
 });
