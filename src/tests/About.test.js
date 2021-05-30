3d455623b5c25669ebb9613241a90f7612ffa8be
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-05-30 15:37:05 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,20 +4,6 @@ import renderWithRouter from './RenderWithRouter';
 import { About } from '../components';
 
 describe('Testa o componente <About.js /.', () => {
-  it('Teste se a página contém as informações sobre a Pokédex.', () => {
-    renderWithRouter(<About />);
-
-    const paragrafoUm = screen.getByText('This application simulates a Pokédex, '
-    + 'a digital encyclopedia containing all Pokémons');
-
-    expect(paragrafoUm).toBeInTheDocument();
-
-    const paragrafoDois = screen.getByText('One can filter Pokémons by type, '
-    + 'and see more details for each one of them');
-
-    expect(paragrafoDois).toBeInTheDocument();
-  });
-
   it('Testa se a página contém um heading h2 com o texto About Pokédex.', () => {
     renderWithRouter(<About />);
 
@@ -31,14 +17,10 @@ describe('Testa o componente <About.js /.', () => {
   it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
     renderWithRouter(<About />);
 
-    // const elementosP = screen.getAllByRole('p');
-    // expect(elementosP.length).toBe(2);
-
     const paragrafoUm = screen.getByText('This application simulates a Pokédex, '
     + 'a digital encyclopedia containing all Pokémons');
 
     expect(paragrafoUm).toBeInTheDocument();
-
     const paragrafoDois = screen.getByText('One can filter Pokémons by type, '
     + 'and see more details for each one of them');
 
