e240cd914343e836656ba54b6321634745543504
Robertson <robertson.nunes@gmail.com>
2021-05-31 12:56:22 -0300

    Requisito 1 - Favorite Pokemon Test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -10,7 +10,7 @@ it('shows the Pokédex when the route is `/`', () => {
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
 });
 
-describe('Teste o componente <App.js />', () => {
+describe('1. Teste o componente <App.js />.', () => {
   it('Testa se o primeiro link possui o texto Home', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const linkHome = getByText(/Home/i);
@@ -20,12 +20,21 @@ describe('Teste o componente <App.js />', () => {
     expect(history.location.pathname).toBe('/');
   });
 
-  it('Testa se o segundo link possui o texto About', () => {
+  it('Testa se o segundo link possui o texto About.', () => {
     const { getByText, history } = renderWithRouter(<App />);
     const linkAbout = getByText(/About/i);
     expect(linkAbout).toBeInTheDocument();
 
     fireEvent.click(linkAbout);
     expect(history.location.pathname).toBe('/about');
+  });
+
+  it('Testa se o terceiro link possui o texto Favorite Pokémons.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkFavorites = getByText(/Favorite Pokémon/i);
+    expect(linkFavorites).toBeInTheDocument();
+
+    fireEvent.click(linkFavorites);
+    expect(history.location.pathname).toBe('/favorites');
   });
 });
