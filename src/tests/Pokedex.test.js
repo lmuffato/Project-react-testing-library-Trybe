29116d67ed9437588267047f04214af64f6e3ced
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-31 08:15:53 -0300

    test: testing if has a h2 element with the text "Encountered pokemons" - requirement 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,9 +1,16 @@
-// import React from 'react';
-// import Pokedex from '../components/Pokedex';
-// import renderWithRouter from './renderWithRouter';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-// describe('Testa o componente "Pokedex".', () => {
-//   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
-//     const { getByRole } = renderWithRouter(<Pokedex />);
-//   });
-// });
+describe('Testa o componente "Pokedex".', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const pokedexTitle = getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    });
+
+    expect(pokedexTitle).toBeInTheDocument();
+  });
+});
