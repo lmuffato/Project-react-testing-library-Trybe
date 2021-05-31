05c9b19157463f87d90cc67605a7995fd11a66ef
Robertson <robertson.nunes@gmail.com>
2021-05-31 15:49:53 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from '../components/renderWithRouter';
+import Pokemons from '../data';
 
-describe('Teste o componente <FavoritePokemons.js />', () => {
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
   it('Testa se é exibido erro, caso a pessoa não tiver pokémons favoritos.', () => {
     const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
     const error = getByText('No favorite pokemon found');
 
     expect(error).toBeInTheDocument();
+  });
+
+  it('Testa se é exibido todos os cards de pokémons favoritados.', () => {
+    const { getAllByText } = renderWithRouter(<FavoritePokemons pokemons={ Pokemons } />);
+    const details = getAllByText(/More Details/i);
+
+    expect(details).toBeDefined();
   });
 });
