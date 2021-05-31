71e0def533bcd968a0b65fe955b84224e2974928
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-31 08:31:50 -0300

    test: testing the "Proximo pokemon" button - requirement 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 describe('Testa o componente "Pokedex".', () => {
+  const pokeIdCardName = 'pokemon-name';
+
   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
     const { getByRole } = renderWithRouter(<App />);
 
@@ -12,5 +15,32 @@ describe('Testa o componente "Pokedex".', () => {
     });
 
     expect(pokedexTitle).toBeInTheDocument();
+  });
+
+  it(`Teste se é exibido o próximo Pokémon da lista 
+    quando o botão Próximo pokémon é clicado.`, () => {
+    const { getByText, getByTestId } = renderWithRouter(<App />);
+
+    const nextPokemon = getByText('Próximo pokémon');
+
+    userEvent.click(nextPokemon);
+    let newPokemon = getByTestId(pokeIdCardName);
+    expect(newPokemon.textContent).toBe('Charmander');
+
+    userEvent.click(nextPokemon);
+    newPokemon = getByTestId(pokeIdCardName);
+    expect(newPokemon.textContent).toBe('Caterpie');
+
+    userEvent.click(nextPokemon);
+    userEvent.click(nextPokemon);
+    userEvent.click(nextPokemon);
+    userEvent.click(nextPokemon);
+    userEvent.click(nextPokemon);
+    userEvent.click(nextPokemon);
+    userEvent.click(nextPokemon);
+    newPokemon = getByTestId(pokeIdCardName);
+    expect(newPokemon.textContent).toBe('Pikachu');
+
+    expect(nextPokemon).toBeInTheDocument();
   });
 });
