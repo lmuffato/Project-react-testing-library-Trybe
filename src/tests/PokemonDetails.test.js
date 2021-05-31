84dbc873c7749da1424cc76b31ff70c4315aab1c
Vinicios <soicinivt@gmail.com>
2021-05-31 09:39:54 -0300

    Test appear the map of pokemon location correctly.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -20,7 +20,19 @@ describe('Tests in PokemonDetails.js', () => {
     const pkmParagraphTheReturnOfTheText = ' enough to eat.';
     const pkmParagraphText = `${pkmParagraphPart1}${pkmParagraphTheReturnOfTheText}`;
     const pkmParagraph = screen.getByText(pkmParagraphText).outerHTML;
-    console.log(pkmParagraph);
     expect(pkmParagraph).toBe(`<p>${pkmParagraphText}</p>`);
+  });
+  it('Contein a map with pokemon location', () => {
+    /* const { history } =  */renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    fireEvent.click(moreDetailsLink);
+    const pkmName = screen.getByTestId('pokemon-name').innerHTML;
+    const headingTxt = `Game Locations of ${pkmName}`;
+    const LocationHeading = screen.getByRole('heading', { level: 2, name: headingTxt });
+    expect(LocationHeading).toBeInTheDocument();
+    const pkmLocations = screen.getAllByAltText(`${pkmName} location`);
+    expect(pkmLocations).toHaveLength(2);
+    expect(pkmLocations[0]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+    expect(pkmLocations[1]).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
   });
 });
