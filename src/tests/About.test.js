9abd588386cbf713fb18ec7cac8d81e5888c0eeb
Robertson <robertson.nunes@gmail.com>
2021-05-31 13:45:55 -0300

    Requisito 2 - Test Infos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('2. Teste o componente <About.js /.', () => {
+  it('Testa se a página contém as informações sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const pokeInfo = getByText(/This application simulates a Pokedex/i);
+
+    expect(pokeInfo).toBeInTheDocument();
+  });
+});
