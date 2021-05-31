227ef0f625922bb05da724572a42cbb3fd15213d
Robertson <robertson.nunes@gmail.com>
2021-05-31 14:03:34 -0300

    Requisto 2 - Test Headings

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from '../components/renderWithRouter';
 
 describe('2. Teste o componente <About.js /.', () => {
   it('Testa se a página contém as informações sobre a Pokédex.', () => {
     const { getByText } = renderWithRouter(<About />);
-    const pokeInfo = getByText(/This application simulates a Pokedex/i);
+    const pokeInfo = getByText(/This application simulates a Pokédex/i);
 
     expect(pokeInfo).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByRole } = render(
+      <About />,
+    );
+    const h2Test = getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+
+    expect(h2Test).toBeInTheDocument();
   });
 });
