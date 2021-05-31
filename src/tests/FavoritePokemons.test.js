f88406b45fe01ccf56f481969133727e7f578b0e
Robertson <robertson.nunes@gmail.com>
2021-05-31 15:16:18 -0300

    Requisito 3 - Test Error Message

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Testa se é exibido erro, caso a pessoa não tiver pokémons favoritos.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const error = getByText('No favorite pokemon found');
+
+    expect(error).toBeInTheDocument();
+  });
+});
