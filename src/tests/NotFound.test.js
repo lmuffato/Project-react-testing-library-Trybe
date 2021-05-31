fa4c21c010e33b14a13e2d037fd519e6a05f1ba4
Debora Passos <deborinhapassos@gmail.com>
2021-05-31 19:40:39 -0300

    Requisito 4 - amem

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+// import { getByRole } from '@testing-library/dom';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('renderizar o componente NotFound', () => {
+  it('deve verificar o h2 contém a mensagem "Page requested not found"', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+
+    const h2 = getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/i,
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  it('deve verificar se possui a imagem "Crying emoji"', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imagemAlt = getByAltText(/Pikachu crying because/);
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imagemAlt).toHaveAttribute('src', imgSrc);
+  });
+});
