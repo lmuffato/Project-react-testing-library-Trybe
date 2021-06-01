25b2ef3265c9f9c587d0fc6aba11520c44fbd645
renancosta21 <renancosta21@gmail.com>
2021-06-01 17:12:57 -0300

    req1 - 1.1, 1.2.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
+  const { getByRole } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-  const heading = getByText(/Pokédex/i);
+
+  const heading = getByRole('heading', {
+    name: /Pokédex/i,
+    level: 1,
+  });
   expect(heading).toBeInTheDocument();
+});
+
+describe('tests navigation links', () => {
+  test('home link', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const linkHome = getByText(/home/i);
+    userEvent.click(linkHome);
+  });
 });
