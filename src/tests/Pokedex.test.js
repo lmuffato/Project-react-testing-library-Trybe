81e6666cc187a16013f7d783d0b4539a75161e6d
Robertson <robertson.nunes@gmail.com>
2021-06-01 19:33:53 -0300

    Requisito 5 - H2 Test

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('5. Teste o componente <Pokedex.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const h2Test = getByRole('heading', {
+      level: 2,
+    });
+    expect(h2Test).toHaveTextContent('Encountered pokémons');
+  });
+});
