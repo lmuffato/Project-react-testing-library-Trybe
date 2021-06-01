6731b3466575d95d8574d9c280af644a36ca134f
renancosta21 <renancosta21@gmail.com>
2021-06-01 18:29:11 -0300

    req1 finished

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,6 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByRole } = render(
@@ -20,32 +21,56 @@ test('renders a reading with the text `Pokédex`', () => {
 
 describe('tests navigation links', () => {
   test('home link', () => {
-    const { getByText } = render(
+    const { getByText, getByRole } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
     const linkHome = getByText(/home/i);
     userEvent.click(linkHome);
+    const linkHomeText = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(linkHomeText).toBeInTheDocument();
   });
 
   test('about link', () => {
-    const { getByText } = render(
+    const { getByText, getByRole } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
     const linkAbout = getByText(/about/i);
     userEvent.click(linkAbout);
+    const textAboutPage = getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(textAboutPage).toBeInTheDocument();
   });
 
   test('favorite pokémons link', () => {
-    const { getByText } = render(
+    const { getByText, getByRole } = render(
       <MemoryRouter>
         <App />
       </MemoryRouter>,
     );
     const favoritePokemonsLink = getByText(/favorite pokémon/i);
     userEvent.click(favoritePokemonsLink);
+    const favoritePokemonsPage = getByRole('heading', {
+      name: /Favorite pokémons/i,
+      level: 2,
+    });
+    expect(favoritePokemonsPage).toBeInTheDocument();
+  });
+
+  test('tests page Not Found', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/not-found-route');
+    const pageNotFound = getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(pageNotFound).toBeInTheDocument();
   });
 });
