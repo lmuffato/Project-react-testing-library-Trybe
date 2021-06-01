f0eb24dd27896735c770fc389dd7b5256dccd78f
Robertson <robertson.nunes@gmail.com>
2021-06-01 19:19:12 -0300

    Requisto 4 - H2 Test

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  it('Testa se página contém um heading h2 com o texto Page requested not found.', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const h2Test = getByRole('heading', {
+      level: 2,
+    });
+    expect(h2Test).toHaveTextContent('Page requested not found');
+  });
+});
