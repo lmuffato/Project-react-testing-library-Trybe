b57198700f22c89dbd4a5cb59889e56bdabb52fa
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-02 13:29:24 -0300

    Testando botão next Pokémon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
-// import Pokedex from '../components/Pokedex';
 import App from '../App';
+import pokemons from '../data';
 
 describe('Testes sobre componente Pokedex.', () => {
   it('Testa a existência de uma tag H2 com texto: "Encountered pokémons".', () => {
@@ -16,5 +17,25 @@ describe('Testes sobre componente Pokedex.', () => {
       name: /próximo pokémon/i,
     });
     expect(botao).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido o próximo Pokémon da lista quando o botão é clicado.', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const botao = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    userEvent.click(botao);
+    const charmander = getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+    for (let i = 0; i < (pokemons.length - 1); i += 1) {
+      userEvent.click(botao);
+    }
+    const pikachu = getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+  });
+
+  it('Teste se é mostrado apenas um Pokémon por vez.', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    expect(getAllByTestId('pokemon-name').length).toBe(1);
   });
 });
