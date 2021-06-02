7f8c4af4bd7f9fe029eabe089d6718d58ad33136
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-01 22:33:33 -0300

    Componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Componente NotFound', () => {
+  test('Testa a existência de um texto h2 com emoji.', () => {
+    const { getByRole } = render(<NotFound />);
+    const emoji = getByRole('heading', {
+      name: /page requested not found/i });
+    expect(emoji).toBeInTheDocument();
+  });
+
+  test('Teste se a imagem é renderizada.', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const imagem = getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(imagem).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
