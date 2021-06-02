261dacd887f793ac54b1bf02af3b14497c3685ab
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-01 21:03:15 -0300

    Teste do componente FavoritePokemon

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+import Pokemons from '../data';
+
+describe('Testando o componete', () => {
+  it('A mensagem "No favorite pokemon found" deve aparecer na tela.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const paragrafoDeErro = getByText(/no favorite pokemon found/i);
+    expect(paragrafoDeErro).toBeInTheDocument();
+  });
+
+  it('Verifica se os cards são renderizado na tela.', () => {
+    const { getAllByText } = renderWithRouter(<FavoritePokemons pokemons={ Pokemons } />);
+    const detailsLink = getAllByText(/average weight:/i);
+    expect(detailsLink).toBeDefined();
+  });
+});
