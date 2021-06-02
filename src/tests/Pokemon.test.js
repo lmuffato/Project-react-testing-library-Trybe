91fbbecb477ce77366fb9579a95bab8a6195b175
Robertson <robertson.nunes@gmail.com>
2021-06-01 22:07:10 -0300

    Requisito 6 - Favorite Icon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -24,12 +24,34 @@ describe('6. Teste o componente <Pokemon.js />', () => {
 
   it('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação', () => {
     const { getByRole, history } = renderWithRouter(<App />);
-    const details = getByRole('link', {
+    const detail = getByRole('link', {
       name: /more details/i,
     });
-    expect(details).toBeInTheDocument();
-    fireEvent.click(details);
+
+    expect(detail).toBeInTheDocument();
+
+    /* Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento da aplicação para a página de detalhes de Pokémon. */
+    fireEvent.click(detail);
     const { pathname } = history.location;
+
+    /* Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes deste Pokémon. O link deve possuir a URL /pokemons/<id>, onde <id> é o id do Pokémon exibido; */
+
+    /* Teste também se a URL exibida no navegador muda para /pokemon/<id>, onde <id> é o id do Pokémon cujos detalhes se deseja ver; */
     expect(pathname).toBe('/pokemons/25');
+  });
+
+  it('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    const { getByRole, getByText, getByAltText } = renderWithRouter(<App />);
+    const detail = getByRole('link', {
+      name: /more details/i,
+    });
+    fireEvent.click(detail);
+    fireEvent.click(getByText('Pokémon favoritado?'));
+    /* A imagem deve ter o atributo alt igual a <pokemon> is marked as favorite, onde <pokemon> é o nome do Pokémon exibido. */
+    const startIcon = getByAltText('Pikachu is marked as favorite');
+
+    /* O ícone deve ser uma imagem com o atributo src contendo o caminho /star-icon.svg; */
+    expect(startIcon).toBeInTheDocument();
+    expect(startIcon.src).toBe('http://localhost/star-icon.svg');
   });
 });
