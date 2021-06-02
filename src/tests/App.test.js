9254b5cd82eaa9b7e7ff118aaaf372d9cd3ab84a
jlfagundes <jlfagundes@gmail.com>
2021-06-02 16:02:56 -0300

    Feat: (req1) test of route  /

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = screen.getByRole('heading', {
-    name: /Pokédex/i
+describe('Tests of Requeriment 1', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = screen.getByRole('heading', {
+      name: /Pokédex/i,
+    });
+    expect(heading).toBeInTheDocument();
   });
-  expect(heading).toBeInTheDocument();
+
+  test('testando se rota esta em /', () => {
+    const history = createMemoryHistory();
+    history.push('/');
+
+    const { container } = render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    expect(container.innerHTML).toMatch(/Pokédex/i);
+  });
 });
