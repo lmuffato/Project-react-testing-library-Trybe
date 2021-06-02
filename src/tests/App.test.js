d36a6c2d8a734f2ed0811fa38251ea6db81284db
Tiago Santos <tiago881@gmail.com>
2021-06-01 23:36:11 -0300

    1. Teste o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createBrowserHistory } from 'history';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -13,12 +15,55 @@ test('renders a reading with the text `Pokédex`', () => {
   expect(heading).toBeInTheDocument();
 });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
+test('testa conjunto de link', () => {
+  render(
+    <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
+  const homeLink = screen.getByRole('link', {
+    name: 'Home',
+  });
+  const aboutLink = screen.getByRole('link', {
+    name: 'About',
+  });
+  const favoriteLink = screen.getByRole('link', {
+    name: 'Favorite Pokémons',
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoriteLink).toBeInTheDocument();
+});
+
+test('testa o redirecionamento de links', () => {
+  const historyMock = createBrowserHistory();
+  render(
+    <Router history={ historyMock }>
+      <App />
+    </Router>,
+  );
+  const homeLink = screen.getByRole('link', {
+    name: 'Home',
+  });
+  const aboutLink = screen.getByRole('link', {
+    name: 'About',
+  });
+  const favoriteLink = screen.getByRole('link', {
+    name: 'Favorite Pokémons',
+  });
+
+  const funcLink = (link) => {
+    userEvent.click(link);
+    return historyMock.location.pathname;
+  };
+
+  const aboutPathname = funcLink(aboutLink);
+  expect(aboutPathname).toEqual('/about');
+
+  const favoritePathname = funcLink(favoriteLink);
+  expect(favoritePathname).toEqual('/favorites');
+
+  const homePathname = funcLink(homeLink);
+  expect(homePathname).toEqual('/');
 });
