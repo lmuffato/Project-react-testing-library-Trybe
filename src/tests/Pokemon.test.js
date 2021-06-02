939eb23576204ef366049d0ce6d22ffa96605275
Robertson <robertson.nunes@gmail.com>
2021-06-01 21:59:56 -0300

    Requsito 6 - Detail Link

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,10 +1,11 @@
+import { fireEvent } from '@testing-library/dom';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
 
 describe('6. Teste o componente <Pokemon.js />', () => {
   it('Teste se é renderizado um card com as informações de determinado pokémon.', () => {
-    const { getByText, getAllByText } = renderWithRouter(<App />);
+    const { getByText, getAllByText, getByAltText } = renderWithRouter(<App />);
 
     /* O nome correto do Pokémon deve ser mostrado na tela; */
     expect(getByText('Pikachu')).toBeInTheDocument();
@@ -19,5 +20,16 @@ describe('6. Teste o componente <Pokemon.js />', () => {
 
     /* A imagem do Pokémon deve ser exibida. Ela deve conter um atributo src com a URL da imagem e um atributo alt com o texto <name> sprite, onde <name> é o nome do pokémon; */
     expect(pokeImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    const details = getByRole('link', {
+      name: /more details/i,
+    });
+    expect(details).toBeInTheDocument();
+    fireEvent.click(details);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
   });
 });
