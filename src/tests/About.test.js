14373a95008dc30d114c129a346f07605f2dc83c
root <root@DESKTOP-74CRLKE.localdomain>
2021-06-02 10:26:29 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test if Pokedex page', () => {
+  it('contains a heading with the text: About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  it('contains two paragraphs', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const firstParagraph = getByText('This application simulates a Pokédex,'
+    + ' a digital encyclopedia containing all Pokémons');
+    expect(firstParagraph).toBeInTheDocument();
+    const secondParagraph = getByText('One can filter Pokémons by type,'
+    + ' and see more details for each one of them');
+    expect(secondParagraph).toBeInTheDocument();
+  });
+  it('contains an image with the url requested', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const image = getByAltText(/pokédex/i);
+    expect(image).toBeInTheDocument();
+    expect(image.src)
+      .toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
