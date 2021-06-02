956c2c90e54e9cd37ba2b3e46bd687edb77ae60d
Jorge-William <jorge_william_cardoso@yahoo.com.br>
2021-06-02 12:33:36 -0300

    Testa tag H2

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+// import Pokedex from '../components/Pokedex';
+import App from '../App';
+
+describe('Testes sobre componente Pokedex.', () => {
+  it('Testa a existência de uma tag H2 com texto: "Encountered pokémons".', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const h2Tag = getByText(/Encountered pokémons/i);
+    expect(h2Tag).toBeInTheDocument();
+  });
+});
