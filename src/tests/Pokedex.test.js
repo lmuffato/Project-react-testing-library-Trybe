515076fe867d462ce17cfef1271b2ff41f27754f
Robertson <robertson.nunes@gmail.com>
2021-06-01 21:00:29 -0300

    Requisito 5 - Button Test

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
 
 describe('5. Teste o componente <Pokedex.js />', () => {
   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
@@ -8,6 +10,25 @@ describe('5. Teste o componente <Pokedex.js />', () => {
     const h2Test = getByRole('heading', {
       level: 2,
     });
+
     expect(h2Test).toHaveTextContent('Encountered pokémons');
+  });
+
+  it('Teste se é exibido o próximo Pokémon da lista quando o botão é clicado.', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    /* O botão deve conter o texto Próximo pokémon; */
+    const nextPoke = getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    fireEvent.click(nextPoke);
+
+    /* Os próximos Pokémons da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão; */
+    expect(getByText('Charmander')).toBeInTheDocument();
+    for (let index = 0; index < (pokemons.length - 1); index += 1) {
+      fireEvent.click(nextPoke);
+    }
+
+    /* O primeiro Pokémon da lista deve ser mostrado ao clicar no botão, se estiver no último Pokémon da lista; */
+    expect(getByText('Pikachu')).toBeInTheDocument();
   });
 });
