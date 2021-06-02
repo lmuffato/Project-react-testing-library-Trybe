3052df77cccee8755bdabae22ff027ac48c7b9fc
root <root@DESKTOP-74CRLKE.localdomain>
2021-06-02 10:28:20 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Test if FavoritePokemon component', () => {
+  test('renders No favorite pokemon found, if the user havent favorite pokemons', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const noPokemonsMessage = getByText(/no favorite pokemon found/i);
+    expect(noPokemonsMessage).toBeInTheDocument();
+  });
+});
