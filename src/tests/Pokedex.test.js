1804e8423cbe024ebc7970375499de0665387fd2
Nathalia Zebral <natzebral@gmail.com>
2021-06-02 08:54:42 -0300

    requisito 6 finalizado-passando no jest e stryker

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,15 +1,20 @@
-// import React from 'react';
-// import UserEvent from '@testing-library/user-event';
-// import renderWithRouter from '../renderWithRouter';
-// import App from '../App';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
-// describe('Testing Pokedex component', () => {
-//   it('have a h2 with text "Encountered Pokémons"', () => {
-//     const { getByRole } = renderWithRouter(<App />);
-//     const heading = getByRole('heading', {
-//       name: /Encauntered pokémons/i,
-//       level: 2,
-//     });
-//     expect(heading).toBeInTheDocument();
-//   });
-// });
+describe('Testing Pokedex component', () => {
+  it('have a h2 with text Encountered Pokémons', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const heading2 = getAllByRole('heading', {
+      Name: /Encauntered pokémons/i,
+    });
+    expect(heading2[1]).toHaveTextContent('Encountered pokémons');
+  });
+  it('when click the button the next Pokémon is shown', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const buttonNextPokemon = getByTestId('next-pokemon');
+    userEvent.click(buttonNextPokemon);
+    expect(buttonNextPokemon).toHaveTextContent('Próximo pokémon');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 import { Pokemon } from '../components';
@@ -28,7 +29,16 @@ describe('test component Pokémon', () => {
     expect(pokemonImage.alt).toContain('Pikachu sprite');
     expect(pokemonImage.src).toContain('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
-
+  it('Have a link to More Details about pokémon', () => {
+    const { getByText, history } = renderWithRouter(<Pokemon
+      pokemon={ pokemons[0] }
+    />);
+    const detailsLink = getByText(/More Details/i);
+    expect(detailsLink).toBeInTheDocument();
+    userEvent.click(detailsLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
   it('Have a Star of Favorite Pokemons', () => {
     const { getByAltText } = renderWithRouter(<Pokemon
       pokemon={ pokemons[0] }
