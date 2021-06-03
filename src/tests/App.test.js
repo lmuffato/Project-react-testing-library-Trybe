9bb0b8832cfbd3705fa8388d297d115170e51da5
Samuel Melo <samuelmmjunior@gmail.com>
2021-06-03 10:57:51 -0300

    requisito 3

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,52 +3,54 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helper/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
-
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = renderWithRouter(<App />);
-  const text = getByText('Encountered pokémons');
-  expect(text).toBeInTheDocument();
-});
-
-test('Teste se a página principal é renderizada no caminho de URL /', () => {
-  const { history } = renderWithRouter(<App />);
-  const { pathname } = history.location;
-  expect(pathname).toBe('/');
-});
-
-test('Teste se a página principal é renderizada no caminho de URL /about', () => {
-  const { history } = renderWithRouter(<App />);
-  history.push('/about');
-  const { pathname } = history.location;
-  expect(pathname).toBe('/about');
-});
-
-test('Teste se a página principal é renderizada no caminho de URL /favorites', () => {
-  const { history } = renderWithRouter(<App />);
-  history.push('/favorites');
-  const { pathname } = history.location;
-  expect(pathname).toBe('/favorites');
-});
-
-test('Teste o redirecionando para as rotas', () => {
-  const { getByRole } = renderWithRouter(<App />);
-
-  const linkHome = getByRole('link', {
-    name: /home/i,
+describe('Teste o componente <App.js />, () =>', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
   });
-  userEvent.click(linkHome);
 
-  const linkAbout = getByRole('link', {
-    name: /about/i,
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const text = getByText('Encountered pokémons');
+    expect(text).toBeInTheDocument();
   });
-  const linkFavorite = getByRole('link', {
-    name: /favorite/i,
+
+  it('Teste se a página principal é renderizada no caminho de URL /', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
   });
-  expect(linkAbout).toBeInTheDocument();
-  expect(linkFavorite).toBeInTheDocument();
+
+  it('Teste se a página principal é renderizada no caminho de URL /about', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Teste se a página principal é renderizada no caminho de URL /favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Teste o redirecionando para as rotas', () => {
+    const { getByRole } = renderWithRouter(<App />);
+
+    const linkHome = getByRole('link', {
+      name: /home/i,
+    });
+    userEvent.click(linkHome);
+
+    const linkAbout = getByRole('link', {
+      name: /about/i,
+    });
+    const linkFavorite = getByRole('link', {
+      name: /favorite/i,
+    });
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const favorite = getByText('No favorite pokemon found');
+    expect(favorite).toBeInTheDocument();
+  });
+});
