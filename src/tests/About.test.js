b1982b476fa8dc75b3c48559c74688f9a0b0b033
diegoCorreia <diegomethal@gmail.com>
2021-06-03 17:20:42 -0300

    requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,24 @@
-test('', () => {});
-// comentário para o stage!
+import React from 'react';
+// import { screen } from '@testing-library/dom';
+import renderWithRouter from './renderWithRouter';
+import { About } from '../components';
+
+test('Test 2.0', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const info1 = getByText(/This application simulates a Pokédex/i);
+  expect(info1).toBeInTheDocument();
+  const info2 = getByText(/One can filter Pokémons by type/i);
+  expect(info2).toBeInTheDocument();
+});
+test('Test 2.1', () => {
+  const { getByText } = renderWithRouter(<About />);
+  const aboutText = getByText(/About Pokédex/i);
+  expect(aboutText).toBeInTheDocument();
+});
+test('Test 2.2', () => {
+  const { getByRole } = renderWithRouter(<About />);
+  const image = getByRole('img');
+  expect(image).toBeInTheDocument();
+  const path = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(image.src).toBe(path);
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -11,4 +13,34 @@ test('renders a reading with the text `Pokédex`', () => {
   );
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
+});
+test('Test 1.1', () => {
+  const { history: { location: { pathname } } } = renderWithRouter(<App />);
+  expect(pathname).toBe('/');
+});
+test('Test 1.2', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const navigation = getByRole('navigation');
+  console.log(navigation.children[0].href);
+  expect(navigation.children[0].innerHTML).toBe('Home');
+  expect(navigation.children[1].innerHTML).toBe('About');
+  expect(navigation.children[2].innerHTML).toBe('Favorite Pokémons');
+});
+test('Test 1.3', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const homeText = getByText('Home');
+  userEvent.click(homeText);
+  expect(homeText).toBeInTheDocument();
+});
+test('Test 1.4', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const aboutText = getByText('About');
+  userEvent.click(aboutText);
+  expect(aboutText).toBeInTheDocument();
+});
+test('Test 1.5', () => {
+  const { getByText } = renderWithRouter(<App />);
+  const favoriteText = getByText('Favorite Pokémons');
+  userEvent.click(favoriteText);
+  expect(favoriteText).toBeInTheDocument();
 });
