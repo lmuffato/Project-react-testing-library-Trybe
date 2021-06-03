518dce01a0b230f5926ec21f4e25c3ea0349e128
Samuel Melo <samuelmmjunior@gmail.com>
2021-06-03 10:42:27 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helper/renderWithRouter';
+import About from '../components/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex', () => {
+
+  it('Teste se a página contém um heading h2 com o texto', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const headingH2 = getByText('About Pokédex');
+    expect(headingH2).toBeInTheDocument();
+  });
+
+  // it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  //   const { getByText } = renderWithRouter(<About />);
+  //   pokedexInfos.forEach((info) => {
+  //     const paragraphInfo = getByText(info);
+  //     expect(paragraphInfo).toBeInTheDocument();
+  //   });
+  // });
+
+  it('test if the component About have image', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const imageAlt = getByAltText('Pokédex');
+    const imagem = 'https://cdn2.bulbagarden.net/upload/thumb/'
+    + '8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(imageAlt).toHaveAttribute('src', imagem);
+  });
+});
