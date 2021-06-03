94483080efdd5bf9fb8d9da792afe63a3e2a4f14
ThalitaC <thalitaceci@gmail.com>
2021-06-03 18:40:53 -0300

    fix: lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import FavoritePokemons from "../components/FavoritePokemons";
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './RenderWithRouter';
 
 describe('tests `FavoritePokemons` component ', () => {
-  test('shows `No favorite pokemon found` if dont have any favorite pokemon', () => {
+  test('show `No favorite pokemon found` if dont have any favorite pokemon', () => {
     renderWithRouter((<FavoritePokemons />));
     const noFavorite = screen.getByText('No favorite pokemon found');
     expect(noFavorite).toBeInTheDocument();
   });
-})
+});
