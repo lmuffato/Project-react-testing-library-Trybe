cea4c91937be7ee75c5823d3b7825042cfdbb816
ThalitaC <thalitaceci@gmail.com>
2021-06-03 18:36:54 -0300

    Requisito 3: Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import FavoritePokemons from "../components/FavoritePokemons";
+import { render, screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+
+describe('tests `FavoritePokemons` component ', () => {
+  test('shows `No favorite pokemon found` if dont have any favorite pokemon', () => {
+    renderWithRouter((<FavoritePokemons />));
+    const noFavorite = screen.getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+})
