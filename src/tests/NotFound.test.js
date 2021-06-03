649a87fb0f63f0198c444a9c8051aa8a477f73fa
ThalitaC <thalitaceci@gmail.com>
2021-06-03 19:25:38 -0300

    Requisito 4: Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import NotFound from '../components/NotFound';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+
+
+describe('tests `NotFound` component', () => {
+  test('contains an <h2> element whit the text `Page requested not found`', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('contains a `not found` image', () => {
+    renderWithRouter((<NotFound />));
+    const image = screen.getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
