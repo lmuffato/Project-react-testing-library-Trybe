1e552cbb9fccfe4408132c27b643bfc1069e600f
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-06-03 18:52:35 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes do requisito 4', () => {
+  it('A pagina "NotFound" contém uma heade "h2" com texto específico', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe');
+    const noMatch = getByText('Page requested not found');
+
+    const content = noMatch.textContent;
+    const heading2 = noMatch.nodeName;
+
+    expect(heading2).toBe('H2');
+    expect(content).toBe('Page requested not found 😭');
+  });
+
+  it('A pagina contém a imagem determinada', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe');
+    const noMatch = getByText('Page requested not found');
+
+    const img = noMatch.nextSibling.nodeName;
+    const href = noMatch.nextSibling.src;
+
+    expect(img).toBe('IMG');
+    expect(href).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
