607607077ee312988ccb4ecc80a839c97004ecb9
lmuffato <lmiremuffato@gmail.com>
2021-06-04 14:43:31 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+// import { getByRole } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('Testa os elementos da componente About', () => {
+  it('Teste se a página contém um heading h2 com o texto "About Pokédex"', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const textH2 = getByRole('heading', {
+      name: 'About Pokédex',
+      level: 2,
+    });
+    expect(textH2).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const paragraph01 = getByText(/This application simulates a Pokédex, a digital ency/);
+    expect(paragraph01).toBeInTheDocument();
+    const paragraph02 = getByText(/One can filter Pokémons by type, and see more detail/);
+    expect(paragraph02).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém a imagem de uma pokedex.', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const pokedexImg = getByRole('img');
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -61,28 +61,3 @@ describe('Testa a página Not Found em URLs desconhecidas.', () => {
     expect(messageInNotFoundPage).toBeInTheDocument();
   });
 });
-
-// import React from 'react';
-// import { MemoryRouter } from 'react-router-dom';
-// import { render } from '@testing-library/react';
-// import App from '../App';
-
-// test('renders a reading with the text `Pokédex`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter>
-//       <App />
-//     </MemoryRouter>,
-//   );
-//   const heading = getByText(/Pokédex/i);
-//   expect(heading).toBeInTheDocument();
-// });
-
-// test('shows the Pokédex when the route is `/`', () => {
-//   const { getByText } = render(
-//     <MemoryRouter initialEntries={ ['/'] }>
-//       <App />
-//     </MemoryRouter>,
-//   );
-
-//   expect(getByText('Encountered pokémons')).toBeInTheDocument();
-// });
