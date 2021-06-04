69c1124fa6735b0248cfc3b4f59ca7a9e48260c0
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-06-04 02:15:45 -0300

    requisito 6 - correçao 2

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import { fireEvent } from '@testing-library/react';
-import renderWithRouter from './renderWithRouter';
-import App from '../App';
-
-describe('The requirement 7 tests', () => {
-  it('All informations about the Pokemon selected is on screen', () => {
-    const { getByText, getByRole, getByTestId } = renderWithRouter(<App />);
-  });
-
-  it('There are maps on screen indicating the Pokemon localization', () => {
-    const { getByText, getByRole, getByTestId } = renderWithRouter(<App />);
-  });
-
-  it('The user can selecting the Pokemon like favorite', () => {
-    const { getByText, getByRole, getByTestId } = renderWithRouter(<App />);
-  });
-});
+test('', () => {});
