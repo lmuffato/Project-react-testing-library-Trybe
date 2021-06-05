0f0960d83461334f4fe2514b7e61d4541ab3894b
diegoCorreia <diegomethal@gmail.com>
2021-06-05 17:44:07 -0300

    requisitos 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+test('Test 3.0', () => {
+  const { getByText } = renderWithRouter(<FavoritePokemons />);
+  const aboutText = getByText(/No favorite pokemon found/i);
+  expect(aboutText).toBeInTheDocument();
+});
+test('Test 3.1', () => {
+  const { getByText, getByAltText } = renderWithRouter(<App />);
+  const infoPokemon = getByText(/More details/i);
+  userEvent.click(infoPokemon);
+  const isFavorited = getByText(/Pokémon favoritado?/i);
+  userEvent.click(isFavorited);
+  const markedPokemon = getByAltText(/is marked as favorite/i);
+  expect(markedPokemon).toBeInTheDocument();
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+test('Test 4.0', () => {
+  const { getByText } = renderWithRouter(<NotFound />);
+  const NotFoundText = getByText(/Page requested not found/i);
+  expect(NotFoundText).toBeInTheDocument();
+});
+test('Test 4.1', () => {
+  const { getByAltText } = renderWithRouter(<NotFound />);
+  const image = getByAltText(/Pikachu crying because the page requested was not found/i);
+  expect(image).toBeInTheDocument();
+  const imgUrl = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(image.src).toBe(imgUrl);
+});
