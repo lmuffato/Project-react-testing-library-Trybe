88f8bc3c6ab22f262e8f7e650960d0e8d4c56f14
William <w.willers@outlook.com>
2021-06-05 16:07:03 -0400

    About tests

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+test('about page should contains info about Pokédex', () => {
+  render(<About />);
+  const firstParagraph = screen.getByText(/This application simulates a Pokédex/i);
+  const secondParagraph = screen.getByText(/One can filter Pokémons by type/i);
+
+  expect(firstParagraph).toBeInTheDocument();
+  expect(secondParagraph).toBeInTheDocument();
+});
+
+test('should about heading contains H2 About Pokédex text', () => {
+  render(<About />);
+  const heading = screen.getByRole('heading', { level: 2 });
+
+  expect(heading.textContent).toBe('About Pokédex');
+});
+
+test('should About page contains Pokédex image', () => {
+  render(<About />);
+  const getImage = screen.getByAltText(/Pokédex/);
+  const imageSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+  expect(getImage.src).toContain(imageSrc);
+});
