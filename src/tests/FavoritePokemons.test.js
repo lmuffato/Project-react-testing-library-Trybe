4f2cc50c934614fac7e70c26048fc57290faf3f8
renancosta21 <renancosta21@gmail.com>
2021-06-06 12:26:58 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('tests FavoritePokemons.js', () => {
+  test('verifies if the page contains the headinf "Favorite pokémons"', () => {
+    const { getByRole } = renderWithRouter(<FavoritePokemons />);
+    const expectedHeading = getByRole('heading', {
+      name: 'Favorite pokémons',
+      level: 2,
+    });
+    expect(expectedHeading).toBeInTheDocument();
+  });
+
+  test('verifies if the message "No favorite pokemon found appears"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const expectedMessage = getByText(/No favorite pokemon found/);
+    expect(expectedMessage).toBeInTheDocument();
+  });
+});
