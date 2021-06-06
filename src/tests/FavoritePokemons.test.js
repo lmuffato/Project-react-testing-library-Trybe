29de966fdc17f2c630100c0ceea0917b040c1f28
biazidioti <bzferreira.bio@gmail.com>
2021-06-06 11:53:09 -0300

    Add req 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+// import { screen } from '@testing-library/dom';
+import RenderWithRouter from './RenderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem "No favorite pokemon found"', () => {
+    const { getByText } = RenderWithRouter(<FavoritePokemons />);
+    const noFavorite = getByText(/no favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  test('Testa se a lista de favoritos aparece', () => {
+    const { history } = RenderWithRouter(<FavoritePokemons />);
+    const about = screen.getByRole('link', {
+      name: /about/i,
+    });
+    userEvent.click(about);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Testa componente NoutFoud', () => {
+  test('Teste se contém um heading h2 com o texto Page requested not found', () => {
+    RenderWithRouter(<NotFound />);
+    const notFoudPage = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(notFoudPage).toBeInTheDocument();
+  });
+
+  // Ref: https://github.com/tryber/sd-010-a-project-react-testing-library/pull/112/files
+  test('Teste se a página contém a imagem correta de uma Pokédex:', () => {
+    RenderWithRouter(<NotFound />);
+    const imagemSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundImage = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/);
+
+    expect(notFoundImage.src).toContain(imagemSrc);
+  });
+});
