ab3ef3ab79ee9dfc9cfb9a7b817b7fc45e9c6a9d
renancosta21 <renancosta21@gmail.com>
2021-06-06 13:19:45 -0300

    req5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('tests Pokedex.js', () => {
+  test('verifies if the page renders the heading "Encoutered pokémons"', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const expectedHeading = getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    });
+    expect(expectedHeading).toBeInTheDocument();
+  });
+
+  test('verifies if the button "Proximo pokémon" is redered', () => {
+    const { getByTestId } = renderWithRouter(<App />);
+    const btnText = getByTestId('next-pokemon');
+    expect(btnText).toHaveTextContent(/Próximo pokémon/);
+  });
+
+  test('verifies if there are filter buttons', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const btnType = getAllByTestId('pokemon-type-button');
+    expect(btnType[1]).toHaveTextContent(/Fire/);
+  });
+
+  test('verifies if there is a reset button', () => {
+    const { getByRole } = renderWithRouter(<App />);
+    const resetBtn = getByRole('button', {
+      name: /All/i,
+    });
+    userEvent.click(resetBtn);
+    expect(resetBtn).toBeInTheDocument();
+  });
+});
