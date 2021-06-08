71ff88dad76f4d6d43c85a649cfd45082ec71177
carlos25xrm <carlos25xrm@gmail.com>
2021-06-08 10:52:45 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+// Seletores: https://testing-library.com/docs/react-testing-library/cheatsheet
+// Exemplos de uso: https://testing-library.com/docs/react-testing-library/example-intro/
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  // Sobre o teste abaixo: Fiz similar ao teste do componente About.js
+  it('Testa se a TAG H2 possui o texto Page requested not found', () => {
+    const { getByRole } = renderWithRouter(<NotFound />);
+    const titulo = getByRole('heading', { level: 2 });
+    expect(titulo).toBeInTheDocument();
+    expect(titulo).toHaveTextContent('Page requested not found 😭');
+  });
+  it('Testa se existe uma imagem no componente pelo link da imagem', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    // ALT copiado da imagem através da função "Inspecionar" do Google Chrome
+    const alt = getByAltText('Pikachu crying because the page requested was not found');
+    expect(alt)
+      .toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
