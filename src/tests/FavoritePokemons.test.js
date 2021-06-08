8d5fae280f8c73c9ea30a0b6e796acb1e2b2844c
carlos25xrm <carlos25xrm@gmail.com>
2021-06-08 09:26:15 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+// Seletores: https://testing-library.com/docs/react-testing-library/cheatsheet
+// Exemplos de uso: https://testing-library.com/docs/react-testing-library/example-intro/
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  it('Testa se exibe "No favorite pokemon found" sem Pokémons favoritos,', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const titulo = getByText(/No favorite pokemon found/i);
+    expect(titulo).toBeInTheDocument();
+  });
+});
