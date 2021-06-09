65508a6a2e3890fb199c8a85e6d34f0dec4f41e1
Fernando Castello Branco <fernandoresende@MacBook-Pro-de-Fernando.local>
2021-06-09 16:58:36 -0300

    requisito 03 feito

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import renderWithRouter from './renderWithRouter.test';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
 import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
 
-describe('testing App components', () => {
-  it('renders a reading with the text `about pokédex`', () => {
-    const { getByText } = renderWithRouter(<FavoritePokemons />);
-    const text = getByText(/No favorite pokemon found/i, {
-    });
-    expect(text).toBeInTheDocument();
+describe('Componente Pokemon favorito', () => {
+  test('testa se a messagem eh mostrada"'
++ ' se nada eh selecionado', () => {
+    const { getByText, history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/favorites');
+    const paragraph = getByText(/No favorite pokemon found/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph.tagName).toBe('P');
+  });
+
+  test('testa se todos os cartoes de pokemons favoritos sao mostrados', () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
+    const fakePokemon = '/pokemons/25';
+    history.push(`${fakePokemon}`);
+    const favcheckbox = getByRole('checkbox');
+    userEvent.click(favcheckbox);
+    history.push('/favorites');
+    const paragraph = getByText(/Pikachu/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph.tagName).toBe('P');
   });
 });
