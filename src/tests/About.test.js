b78c732dfaf03327c1fb7a4ec712b79f1aabef2b
Fernando Castello Branco <fernandoresende@MacBook-Pro-de-Fernando.local>
2021-06-09 15:34:25 -0300

    requisito 02 feito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente about', () => {
+  test('teste se a página sobre contém um h2 chamado `About Pokédex`', () => {
+    const { getByRole, history } = renderWithRouter(<About />);
+    history.push('/about');
+    const heading = getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('teste se a página sobre contém dois parágrafos sobre o `Pokédex`', () => {
+    const { getByText, history } = renderWithRouter(<About />);
+    history.push('/about');
+    const paragraph = getByText(/This application simulates a Pokédex/i);
+    const paragraph2 = getByText(/One can filter Pokémons by type/i);
+    expect(paragraph).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+    expect(paragraph.tagName).toBe('P');
+  });
+  test('teste se a página sobre contém uma imagem Pokédex', () => {
+    const { getByAltText, history } = renderWithRouter(<About />);
+    history.push('/about');
+    const image = getByAltText('Pokédex');
+    const imagesrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(imagesrc);
+  });
+});
