15e7e7624710608f35fcfbe6facff59d60edcce6
Fernando Castello Branco <fernandoresende@MacBook-Pro-de-Fernando.local>
2021-06-10 13:59:23 -0300

    requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Componente not found', () => {
+  test('tteste se a página exibe um cabeçalho h2'
+  + ' com o texto `Página solicitada não encontrada`', () => {
+    const { getByRole, history } = renderWithRouter(<NotFound />);
+    history.push('/dumbpage');
+    const heading = getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+  test('testa se a pagina nao encontrada contem imagem do pikachu', () => {
+    const { getByAltText, history } = renderWithRouter(<NotFound />);
+    history.push('/dumppage');
+    const image = getByAltText('Pikachu crying because the page requested was not found');
+    const imagesrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image).toBeInTheDocument();
+    expect(image.src).toBe(imagesrc);
+  });
+});
