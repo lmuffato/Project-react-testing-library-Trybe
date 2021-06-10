2e1f12251fd441aa777b96264048834c6695ad66
Luiz <luizfvaccarif@gmail.com>
2021-06-10 23:12:35 +0100

    feat: Requirement 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+// import { render, screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
+/* test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
     <MemoryRouter>
       <App />
@@ -21,4 +22,30 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+}); */
+
+describe('Requirement 1', () => {
+  test('If home page renders', () => {
+    const { getByText, history } = renderWithRouter(
+      <App />,
+    );
+    const encounteredPokemons = getByText(/encountered pokémons/i);
+    const { pathname } = history.location;
+    expect(encounteredPokemons).toBeInTheDocument();
+    expect(pathname).toBe('/');
+  });
+
+  test('If the links on the top are rendering', () => {
+    const { getByRole } = renderWithRouter(
+      <App />,
+    );
+    const homeLink = getByRole('link', { name: /home/i });
+    const aboutLink = getByRole('link', { name: /about/i });
+    const favoritePokemonsLink = getByRole('link', { name: /favorite Pokémons/i });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritePokemonsLink).toBeInTheDocument();
+  });
 });
+
+// Caso não seja utilizado screen o getByRole deve ser declarado.
