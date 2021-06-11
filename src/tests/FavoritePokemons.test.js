f1b9dd5132f08fee03eda87b28147f9e2ce8c352
Luiz <luizfvaccarif@gmail.com>
2021-06-11 12:19:04 +0100

    feat: Requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Requirement 3', () => {
+  test('If the message with the text "No favorite pokemon found" is rendered.', () => {
+    const { getByText } = renderWithRouter(
+      <FavoritePokemons />,
+    );
+    const noFavorite = getByText(/no favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
