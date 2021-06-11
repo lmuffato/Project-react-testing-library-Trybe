58f6a78956790dfb64fc52628f7e6514bd241fe4
Junior Portela <portelajr14@gmail.com>
2021-06-11 12:09:17 -0300

    feat:req2- tests on About.js

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,58 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { About } from '../components';
+
+describe('Testing the component <About />', () => {
+  test('text information about pokédex', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const containingText = screen.getByText(/encyclopedia containing/i);
+
+    expect(containingText).toBeInTheDocument();
+  });
+
+  test('h2 with /about pokedex/ text', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const aboutTitle = screen.getByRole('heading',
+      {
+        name: /about pokédex/i,
+        level: 2,
+      });
+
+    expect(aboutTitle).toBeInTheDocument();
+  });
+
+  test('render information text into two <p> tags', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const containingWord = screen.getAllByText(/pokémons/i);
+
+    expect(containingWord.length).toBe(2);
+  });
+
+  test('renders the pokédex image', () => {
+    render(
+      <BrowserRouter>
+        <About />
+      </BrowserRouter>,
+    );
+
+    const altText = screen.getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(altText.src).toBe(src);
+  });
+});
