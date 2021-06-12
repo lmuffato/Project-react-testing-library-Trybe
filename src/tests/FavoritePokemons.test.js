3d8063e086ce4985db5498e64c0bde7ee47ae523
Tiago Santos <tiago881@gmail.com>
2021-06-11 23:26:41 -0300

    3. Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('test mesagem no favorite pokemon ', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/favorites');
+  const noFavoriteMsg = screen.getByText('No favorite pokemon found');
+
+  expect(noFavoriteMsg).toBeInTheDocument();
+});
+
+test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+  const { history } = renderWithRouter(<App />);
+  history.push('/pokemons/25');
+  const input = screen.getByLabelText('Pokémon favoritado?');
+
+  expect(input).toBeInTheDocument();
+  userEvent.click(input);
+  history.push('/favorites');
+
+  const pokemonName = screen.getByTestId('pokemon-name');
+  expect(pokemonName).toBeInTheDocument();
+});
