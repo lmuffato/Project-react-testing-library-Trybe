d83f1fd52f268b375196875433a003e2195dcd4b
Tiago Santos <tiago881@gmail.com>
2021-06-13 15:16:35 -0300

    Referencias e agradecimentos

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,12 @@ export { default as NotFound } from './NotFound';
 export { default as Pokedex } from './Pokedex';
 export { default as Pokemon } from './Pokemon';
 export { default as PokemonDetails } from './PokemonDetails';
+
+/* 
+Agradeço meus colegas da Turma10-Tybo A por dedicar um tempo me auxiliando no 
+entendimento dos conceitos que me levaram a concluir 100% deste projeto. 
+
+Leonardo Mallmann | https://github.com/mallmann02
+Victor Canto | https://github.com/victorcanto
+Anderson Silva(Andy) | https://github.com/AndersonSilva94
+*/
