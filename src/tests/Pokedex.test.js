b0de90b84645aacd8a1a3768c113b0dc151ab0a5
biazidioti <bzferreira.bio@gmail.com>
2021-06-13 17:39:33 -0300

    Procurando nemo no heading ...

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+// import Pokedex from '../components/Pokedex';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('Teste se contém um heading h2 com o texto Encountered pokémons', () => {
+    RenderWithRouter(<App />);
+    const heading = screen.getByRole('heading', {
+      name: /Encountered pokémons/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
