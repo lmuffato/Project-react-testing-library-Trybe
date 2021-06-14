94fc56654ec67cbfad44beb4e0ae422c80055487
Renata Andrade <reapminas@gmail.com>
2021-06-14 01:47:21 -0300

    Requisito 1 - Teste o componente <App.js />

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+import { fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+// Acessar os elementos da sua tela
+// Interagir com eles (se houver necessidade)
+// Fazer seu teste
+describe('Testa links de navegação do componente App', () => {
+  it('Testa se a URL / renderiza o componente Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+    const home = getByText(/Pokédex/);
+    expect(home).toBeInTheDocument();
+  });
+  it('Testa se a URL /about renderiza o componente About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/About/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+    const about = getByText(/About Pokédex/);
+    expect(about).toBeInTheDocument();
+  });
+  it('Testa se a URL /favorites renderiza o componente Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+    const about = getByText(/Favorite pokémons/);
+    expect(about).toBeInTheDocument();
+  });
 });
