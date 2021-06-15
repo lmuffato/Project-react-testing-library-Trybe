09a98a693ca9d0cac87b3609cd535567ce0c5bd2
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-06-15 12:00:56 -0300

    req 05 incompleto

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
+
+describe('Pokedex.js tests', () => {
+  it('verify h2 text', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const pokeDex = getByText('Encountered pokémons');
+    expect(pokeDex).toBeInTheDocument();
+    expect(pokeDex.tagName).toBe('H2');
+  });
+
+  it('verify next pokemon button', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const nextPokemon = getByRole('button', { name: 'Próximo pokémon' });
+    expect(nextPokemon).toBeInTheDocument();
+    const pikachu = getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+    userEvent.click(nextPokemon);
+    const charmander = getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+    userEvent.click(nextPokemon);
+    const caterpie = getByText('Caterpie');
+    expect(caterpie).toBeInTheDocument();
+  });
+});
