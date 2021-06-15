0b44e162493a797b22b238536f24a290d98a9656
biazidioti <bzferreira.bio@gmail.com>
2021-06-14 21:35:54 -0300

    Olha eu aqui ... tentando.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -15,46 +15,38 @@ describe('Teste o componente <Pokedex.js />', () => {
     expect(heading).toBeInTheDocument();
   });
 
-  // Ref: https://github.com/tryber/sd-010-a-project-react-testing-library/pull/15/files
-  test('Teste se é exibido o próximo Pok da lista qnq o botão Próx poké é clic', () => {
+  test('O botão deve conter o texto Próximo pokémon', () => {
     RenderWithRouter(<App />);
-    const nextButton = screen.getByRole('button', {
+    const proxPok = screen.getByTestId('next-pokemon');
+    expect(proxPok).toHaveTextContent('Próximo pokémon');
+  });
+
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    const { getByRole, getByText } = RenderWithRouter(<App />);
+
+    const proximoPokemon = getByRole('button', {
       name: /Próximo pokémon/i,
     });
-    userEvent.click(nextButton);
+
+    fireEvent.click(proximoPokemon);
+    const secondPok = getByText('Charmander');
+    expect(secondPok).toBeInTheDocument();
   });
 
-  const pokName = screen.getByTestId('pokemon-name');
-  console.log(pokName.innerHTML);
-  console.log(data[1].pokName);
-  expect(pokName.innerHTML).toBe(data[1].pokName);
-});
-
-test('Teste se é mostrado apenas um Pokémon por vez', () => {
-  const { getByRole, getByText } = RenderWithRouter(<App />);
-
-  const proximoPokemon = getByRole('button', {
-    name: /Próximo pokémon/i,
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const { getByRole } = RenderWithRouter(<App />);
+    const resetBtn = getByRole('button', {
+      name: /All/i,
+    });
+    userEvent.type(resetBtn);
+    expect(resetBtn).toBeInTheDocument();
   });
 
-  fireEvent.click(proximoPokemon);
-  const secondPok = getByText('Charmander');
-  expect(secondPok).toBeInTheDocument();
-});
+  test('deve verificar se é criado, um botão de filtro para cada tipo de Pokémon', () => {
+    const { getAllByTestId } = RenderWithRouter(<App />);
 
-test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
-  const { getByRole } = RenderWithRouter(<App />);
-  const resetBtn = getByRole('button', {
-    name: /All/i,
+    const btnAllPokemons = getAllByTestId('pokemon-type-button');
+    userEvent.type(btnAllPokemons);
+    expect(btnAllPokemons[0]).toHaveTextContent('Electric');
   });
-  userEvent.type(resetBtn);
-  expect(resetBtn).toBeInTheDocument();
-});
-
-test('deve verificar se é criado, um botão de filtro para cada tipo de Pokémon', () => {
-  const { getAllByTestId } = RenderWithRouter(<App />);
-
-  const btnAllPokemons = getAllByTestId('pokemon-type-button');
-  userEvent.type(btnAllPokemons);
-  expect(btnAllPokemons[0]).toHaveTextContent('Electric');
 });
