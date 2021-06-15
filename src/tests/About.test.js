bb9ac05adff16da9ce764d660693fbcc50c8e3c0
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-06-15 01:27:22 -0300

    req 01 e 02 refeitos.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('About.js tests', () => {
+  it('renders "About" page', () => {
+    const { getByText } = renderWithRouter(<About />);
+    const about = getByText('About Pokédex');
+    expect(about).toBeInTheDocument();
+  });
+
+  it('verify h2 text', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const titleContent = getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(titleContent).toBeInTheDocument();
+  });
+
+  it('verify paragraphs', () => {
+    const { container } = renderWithRouter(<About />);
+    // referência: https://stackoverflow.com/questions/54234515/get-by-html-element-with-react-testing-library
+    const paragraphs = container.querySelectorAll('p');
+    expect(paragraphs).toHaveLength(2);
+  });
+
+  it('verify image', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const image = getByRole('img');
+    expect(image).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    // referência: https://dev.to/raphaelchaula/a-simple-image-test-in-react-3p6f
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,69 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js tests', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders homepage in the "/" path ', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const homePage = getByText('Encountered pokémons');
+    expect(homePage).toBeInTheDocument();
+  });
+
+  it('nav bar have 3 links', () => {
+    const { getAllByRole, getByRole } = renderWithRouter(<App />);
+    const navBar = getAllByRole('navigation');
+    expect(navBar).toHaveLength(1);
+
+    const home = getByRole('link', { name: 'Home' });
+    // 'modelo: para usar o getByRole(expectedRole, { name: 'The name' })'
+    // referência: https://testing-library.com/docs/queries/byrole/
+    expect(home).toBeInTheDocument();
+
+    const about = getByRole('link', { name: 'About' });
+    expect(about).toBeInTheDocument();
+
+    const favoritePokemons = getByRole('link', { name: 'About' });
+    expect(favoritePokemons).toBeInTheDocument();
+  });
+
+  it('click in "Home" and redirect to "/"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText('Home'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('click in "About" and redirect to "/about"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText('About'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('click in "About" and redirect to "/about"', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    userEvent.click(getByText('Favorite Pokémons'));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('not found page', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/someRandomPage');
+    const noMatch = getByText('Page requested not found');
+    expect(noMatch).toBeInTheDocument();
+  });
 });
