1efaad7473d03233d9d3fcab94cbe03e721e2714
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-06-15 13:46:50 -0300

    req 05 finalizado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -21,8 +21,37 @@ describe('Pokedex.js tests', () => {
     userEvent.click(nextPokemon);
     const charmander = getByText('Charmander');
     expect(charmander).toBeInTheDocument();
-    userEvent.click(nextPokemon);
-    const caterpie = getByText('Caterpie');
-    expect(caterpie).toBeInTheDocument();
+    // Testar a lista toda
+    for (let index = 1; index < pokemons.length; index += 1) {
+      const currentPokemon = getByText(pokemons[index].name);
+      expect(currentPokemon).toBeInTheDocument();
+      userEvent.click(nextPokemon);
+    }
+  });
+
+  it('show each pokemon once time', () => {
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const showOncePokemon = getAllByTestId('pokemon-name');
+    expect(showOncePokemon.length).toBe(1);
+  });
+
+  it('verify button of Pokemon types', () => {
+    const allTypes = ['Electric', 'Fire', 'Bug', 'Poison', 'Psychic', 'Normal', 'Dragon'];
+    const { getAllByTestId, getByRole } = renderWithRouter(<App />);
+    allTypes.forEach((currentType, index) => {
+      const pokemonType = getAllByTestId('pokemon-type-button')[index];
+      expect(pokemonType).toBeInTheDocument();
+      const current = getByRole('button', { name: currentType });
+      expect(current).toBeInTheDocument();
+    });
+  });
+
+  it('reset filter', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const btnAll = getByText('All');
+    expect(btnAll).toBeInTheDocument();
+    userEvent.click(btnAll);
+    const defaultPokemon = getByText('Pikachu');
+    expect(defaultPokemon).toBeInTheDocument();
   });
 });
