e3cd3fa82190e36c35e36b089fcaf6ec28e52098
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-06-15 09:09:06 -0300

    req 03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('FavoritePokemons.js tests', () => {
+  it('verify "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons pokemons={ [] } />);
+    const noFavorite = getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
