47205181a30f008f08bb1efb502cf5188af7f3d7
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-15 16:03:24 -0300

    add req1 and component renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/dom';
+import renderWithRouter from '../components/renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('tests for the App component', () => {
+  it('Tests whether the Pokédex main page is rendered in the URL path /', () => {
+    const { getByText } = renderWithRouter(<App />);
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  it('test redirect with home link', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkHome = getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+
+    fireEvent.click(linkHome);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('test redirect with about link', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkAbout = getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+
+    fireEvent.click(linkAbout);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('test redirect with favorite link', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const linkFavorite = getByText(/Favorite Pokémon/i);
+    expect(linkFavorite).toBeInTheDocument();
+
+    fireEvent.click(linkFavorite);
+    expect(history.location.pathname).toBe('/favorites');
+  });
 });
