bed0d0d4d945bbd4d6833ab68562618ce081421f
Renata Andrade <reapminas@gmail.com>
2021-06-15 10:15:44 -0300

    Requisito 3 - Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o component Favorite Pokemons ', () => {
+  it('Testa se a página exibe a mensagem: No favorite pokemon found', () => {
+    const { getByText, history } = renderWithRouter(<FavoritePokemons />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = getByText(/No favorite pokemon found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+});
