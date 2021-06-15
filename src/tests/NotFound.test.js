d331e99abf989a6e43143ad017911850fe922fdb
Renata Andrade <reapminas@gmail.com>
2021-06-15 08:04:25 -0300

    Requisito 4 - Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o component Not Found', () => {
+  it('Testa se a página tem um h2 com texto: Page requested not found', () => {
+    const { getByText } = renderWithRouter(<NotFound />);
+    const textTitle = getByText('Page requested not found');
+    expect(textTitle).toBeInTheDocument();
+  });
+  it('Testa se a página tem o endereço da imagem do Pikachu chorando', () => {
+    const { getByAltText } = renderWithRouter(<NotFound />);
+    const image = getByAltText(/Pikachu/i);
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
