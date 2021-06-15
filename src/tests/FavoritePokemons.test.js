71e594727ec327a16d7cd7717c0d872bdcd5f56a
Marcos Albuquerque <mpdsa20@gmail.com>
2021-06-14 22:09:18 -0300

    test NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { MemoryRouter } from 'react-router';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import Favorite from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test(`Teste se é exibido na tela a mensagem 
+        No favorite pokemon found, se a pessoa
+        não tiver pokémons favoritos.`, () => {
+    render(<Favorite />, { wrapper: MemoryRouter });
+    screen.getByText(/No favorite pokemon found/i);
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { MemoryRouter } from 'react-router';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import NotFound from '../components/NotFound';
+
+const text1 = 'Pikachu crying because the page requested was not found';
+
+describe('Teste o componente <NotFound.js />', () => {
+  render(<NotFound />, { wrapper: MemoryRouter });
+  const imageAlt = screen.getByAltText(text1);
+
+  test(`Teste se página contém um heading h2 
+        com o texto Page requested not found 😭`, () => {
+    const pageNotFound = screen.getByText(/Page requested not found/i);
+    expect(pageNotFound).toBeInTheDocument();
+  });
+
+  test(`Teste se página mostra a imagem
+        https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif`, () => {
+    expect(imageAlt.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
