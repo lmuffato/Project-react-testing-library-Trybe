199c32859143dc25779c3b1a15301ae288ae235d
jlfagundes <jlfagundes@gmail.com>
2021-06-15 10:36:48 -0300

    feat: (renderWithRouter) create component renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+import { render } from "react-dom";
+import { About } from "../components";
+
+describe('bloco de testes de about', () => {
+  test('teste se a pagina About contem h2', () => {
+    render(<About />)
+  });
+});
