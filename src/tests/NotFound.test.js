54f59fa2045fdd6de6e72cfd20dd99c4dd5e403b
jlfagundes <jlfagundes@gmail.com>
2021-06-16 16:13:33 -0300

    feat: (req4) test not found component

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Teste do componente not found', () => {
+  test('teste heading', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFounds = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(notFounds).toBeInTheDocument();
+  });
+
+  test('testando imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getAllByRole('img');
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img[1].src).toBe(imgSrc);
+  });
+});
