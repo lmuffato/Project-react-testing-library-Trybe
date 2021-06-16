e76bef7e76853a3459ca5be9fb75b35ceafac902
jlfagundes <jlfagundes@gmail.com>
2021-06-16 15:55:09 -0300

    fix: (req3) implementation teste reder component

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,7 +5,39 @@ import renderWithRouter from './RenderWithRouter';
 
 describe('Testes pagina favorite pokemons', () => {
   test('teste se exibe testo padrao', () => {
-    renderWithRouter(<FavoritePokemons />);
+    renderWithRouter(<FavoritePokemons
+      pokemons={ [{
+        id: 25,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+        moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+        foundAt: [
+          {
+            location: 'Kanto Viridian Forest',
+            map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+          },
+          {
+            location: 'Kanto Power Plant',
+            map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+          },
+        ],
+        summary: 'This intelligent Pokémon roasts hard'
+        + 'berries with electricity to make them tender enough to eat.',
+      }] }
+    />);
+
+    const renderPokemon = screen.getByText(/Pikachu/i);
+
+    expect(renderPokemon).toBeInTheDocument();
+  });
+
+  test('teste no found', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ [] } />);
 
     const noPokemon = screen.getByText(/no favorite pokemon found/i);
 
