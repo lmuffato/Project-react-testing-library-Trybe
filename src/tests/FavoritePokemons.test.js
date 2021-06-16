42d09c3e3a08352526c68e0592bc26d206d7a816
jlfagundes <jlfagundes@gmail.com>
2021-06-16 15:27:29 -0300

    feat: (req3) test component favorite pokemon done

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Testes pagina favorite pokemons', () => {
+  test('teste se exibe testo padrao', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noPokemon = screen.getByText(/no favorite pokemon found/i);
+
+    expect(noPokemon).toBeInTheDocument();
+  });
+});
