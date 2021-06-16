e812854b1debbc3535444cc127612c9922688d12
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-06-16 02:41:49 -0300

    requesito 1, tentativa 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -15,6 +15,43 @@ describe('App component', () => {
     expect(heading).toBeInTheDocument();
   });
 
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+
+  test('shows the Navigation menu', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/');
+
+    const home = getByText(/home/i);
+    expect(home).toBeInTheDocument();
+
+    const about = getByText(/about/i);
+    expect(about).toBeInTheDocument();
+
+    const FavoritePokemon = getByText(/favorite pokémons/i);
+    expect(FavoritePokemon).toBeInTheDocument();
+  });
+
+  test('full app rendering/navigating', async () => {
+    const {
+      getByText,
+      history,
+    } = renderWithRouter(<App />);
+    history.push('/about');
+
+    const navigateAbout = getByText(/About Pokédex/i);
+    expect(navigateAbout).toBeInTheDocument();
+
+    await history.push('/');
+
+    const navigateHome = getByText(/Encountered pokémons/i);
+    expect(navigateHome).toBeInTheDocument();
+  });
+
   test('Heading is rendered with text Pokédex', () => {
     const { getByRole, history } = renderWithRouter(<App />);
     history.push('/');
