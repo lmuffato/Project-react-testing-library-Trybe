87c10d5c725fab5b11c316bf36759f171dd89b1e
Luiz Otavio <luiz.otavio273@gmail.com>
2021-06-16 19:45:20 -0300

    requisito 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../components/renderWithRouter';
+
+import App from '../App';
+
+describe('Requisito 2 testa o componente About', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const { getByRole, getByText } = renderWithRouter(<App />);
+    const about = getByText('About');
+    userEvent.click(about);
+    const elementh2 = getByRole('heading', { name: 'About Pokédex', level: 2 });
+    const paragraph1 = getByText(/This application simulates a Pokédex/i);
+    const paragraph2 = getByText(/One can filter Pokémons by type/i);
+    const img = getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+    expect(elementh2).toBeInTheDocument();
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+    expect(img.src).toBe(src);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,22 +3,23 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+describe('Requisito 1. Teste o componente `<App.js />`', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+    const home = getByText(/Home/i);
+    expect(home).toBeInTheDocument();
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+    const about = getByText(/About/i);
+    expect(about).toBeInTheDocument();
+
+    const favorite = getByText(/Favorite Pokémons/i);
+    expect(favorite).toBeInTheDocument();
+  });
 });
