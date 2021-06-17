841dbdc3aac09ccd3b887379ff20834f87440dcd
Renata Andrade <reapminas@gmail.com>
2021-06-16 23:36:56 -0300

    Requisito 7 - Teste o componente <PokemonDetails.js />

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,93 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+const myRota = 'pokemons/25';
+
+describe('Testa o component PokemonDetails', () => {
+  it('Testa se o texto (<name> Details) esta na tela', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByRole('heading', {
+      name: /Pikachu Details/i,
+      level: 2,
+    });
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se o texto Summary esta na tela', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByRole('heading', {
+      name: /Summary/i,
+      level: 2,
+    });
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se exite uma descrição do Pokemon esta na tela', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByText('This intelligent Pokémon roasts hard berries with'
+    + ' electricity to make them tender enough to eat.');
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se o texto Game Locations of Pikachu esta na tela', () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByRole('heading', {
+      name: /Game Locations of Pikachu/i,
+      level: 2,
+    });
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se o texto Kanto Viridian Forest esta na tela', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByText('Kanto Viridian Forest');
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se o texto Kanto Power Plant esta na tela', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByText('Kanto Power Plant');
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se o texto Pokémon favoritado? esta na tela', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push(myRota);
+
+    const myText = getByText('Pokémon favoritado?');
+    expect(myText).toBeInTheDocument();
+  });
+
+  it('Testa se a imagem do mapa esta na tela', () => {
+    const { getAllByAltText, history } = renderWithRouter(<App />);
+
+    history.push('/pokemons/25');
+
+    const myImage = getAllByAltText('Pikachu location');
+
+    expect(myImage[0].src).toContain('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
+});
+
+// Referência:
+// Repositório https://github.com/tryber/sd-010-a-project-react-testing-library/blob/VRodriguera-react-testing/src/tests/PokemonDetails.test.js
