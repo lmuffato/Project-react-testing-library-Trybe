24498e69e53ce1bfb97ead4a00087d851a96cd4f
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-16 22:09:07 -0300

    Req03

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkToFavPokemon = screen.getByRole('link', { name: /Favorite Pokémons/i });
+  userEvent.click(linkToFavPokemon);
+  const favPokemonText = screen.getByText('No favorite pokemon found');
+  expect(favPokemonText).toBeInTheDocument();
+});
