8b1454463af6c94894deef501de17741d980b8fb
pauloeliezerg <pauloeliezerg@gmail.com>
2021-06-16 21:21:45 -0300

    Req01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 test('renders a reading with the text `Pokédex`', () => {
@@ -21,4 +21,40 @@ test('shows the Pokédex when the route is `/`', () => {
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('O primeiro link deve possuir o texto Home', () => {
+  render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkToHome = screen.getByRole('link', { name: /Home/i });
+
+  expect(linkToHome).toBeInTheDocument();
+});
+
+test('O segundo link deve possuir o texto About', () => {
+  render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkToHome = screen.getByRole('link', { name: /About/i });
+
+  expect(linkToHome).toBeInTheDocument();
+});
+
+test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+  render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  const linkToHome = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+  expect(linkToHome).toBeInTheDocument();
 });
