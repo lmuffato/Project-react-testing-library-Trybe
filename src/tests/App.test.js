19f5b4e4804e19d6b70ad2a5fcbec63185cc76b4
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-06-17 12:00:11 -0300

    corrige lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
