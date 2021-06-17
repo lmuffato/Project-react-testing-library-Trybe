31952e1a77dac714aa9aa601a2d08cf7ef5ade7c
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-06-17 08:52:10 -0300

    add req4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('tests for the NotFound component', () => {
+  it('Test if page contains a heading with the text Page requested not found', () => {
+    render(<NotFound />);
+    const heading = screen.getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('test if it contains an image', () => {
+    render(<NotFound />);
+    const image = screen.getAllByRole('img');
+    const imageAdress = image[1].src;
+    const adress = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imageAdress).toEqual(adress);
+  });
+});
