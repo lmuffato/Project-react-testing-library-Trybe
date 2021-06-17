504ce71a55e5627b60bcbdde8efbc2982a9c29f3
biazidioti <bzferreira.bio@gmail.com>
2021-06-16 21:21:41 -0300

    Base para terminar o ultimo requisito ... forças icone.

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+/* import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import RenderWithRouter from './RenderWithRouter'; */
+
+describe('Teste o componente PokemonDetails.js', () => {
+  test('as infos detalhadas do Pokémon selec são mostradas na tela', () => {
+
+  });
+
+  test('existe na pág uma seção com os mapas contendo as loc do pokémon', () => {
+
+  });
+
+  test('suário pode favoritar um pokémon através da página de detalhes', () => {
+
+  });
+
+  test('usuário pode favoritar um pokémon através da página de detalhes', () => {
+
+  });
+});
