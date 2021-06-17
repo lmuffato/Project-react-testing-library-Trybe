d3af45a53679d67375ab072ab9724cecdfd74b26
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-06-17 11:49:46 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+test('', () => {
+  render(<NotFound />);
+  const notFoundTextEl = screen.getByRole('heading', {
+    level: 2,
+    name: /Page requested not found/i,
+  });
+
+  const notFoundImageEl = screen.getAllByRole('img');
+  const correctImagePath = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+  expect(notFoundTextEl).toBeInTheDocument();
+  expect(notFoundImageEl[1].src).toEqual(correctImagePath);
+});
