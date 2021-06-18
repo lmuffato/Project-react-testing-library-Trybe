33c6ba18af60cc9763e519704b44826bfade4c15
Wendrick <wendrick.b.c1@gmail.com>
2021-06-18 00:40:57 -0300

    Req 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import About from '../components/About'
+import renderWithRouter from '../render/renderWithRouter';
+
+describe('Verify if the page has the informations', () => {
+  test('Test if the page has a heading h2 with the text', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const headingText = getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(headingText).toBeInTheDocument();
+  })
+
+  test('Test if the page has 2 p with text about pokedex', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+    const paragQuant = getAllByText(/Pokémons/);
+    expect(paragQuant.length).toBe(2);
+  });
+
+  test('Test if has image.', () => {
+    const { getByAltText } = renderWithRouter(<About />);
+    const pokedexImage = getByAltText('Pokédex');
+    expect(pokedexImage).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  })
+})
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../render/renderWithRouter';
 
 test('renders a reading with the text `Pokédex`', () => {
   const { getByText } = render(
@@ -15,10 +16,30 @@ test('renders a reading with the text `Pokédex`', () => {
 
 test('shows the Pokédex when the route is `/`', () => {
   const { getByText } = render(
-    <MemoryRouter initialEntries={['/']}>
+    <MemoryRouter initialEntries={ ['/'] }>
       <App />
     </MemoryRouter>,
   );
 
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('Verify the links:', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Home')).toBeInTheDocument();
+  expect(getByText('About')).toBeInTheDocument();
+  expect(getByText(/Favorite Pokémons/i)).toBeInTheDocument();
+});
+
+test('Verify the click in the links', () => {
+  const { getByText, history } = renderWithRouter(<App />);
+
+  fireEvent.click(getByText(/About/i));
+  expect(history.location.pathname).toBe('/about');
+  expect(getByText(/About Pokédex/)).toBeInTheDocument();
 });
