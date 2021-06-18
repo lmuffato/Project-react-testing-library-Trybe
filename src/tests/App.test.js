1166695bbffc80fb0d18c7430f95e0e1a4a0681a
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-06-18 08:12:41 -0300

    Requisito 01: Adicionando testes.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('Exemplos de teste.', () => {
+  test('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 });
 
-// teste inicial provido pelo proprio repositório.
+describe('Testes de redirecionamento.', () => {
+  it('Testa se o app é redirecionado à página inicial ao clicar no link Home;', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/home/i));
+  });
 
-test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
+  it('Testa se o app é redirecionado à página about ao clicar no link about;', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/about/i));
+  });
 
-  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  it('Testa se o app é redirecionado à página Favorite Pokémons;', () => {
+    const { getByText } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Favorite Pokémons/i));
+  });
+
+  it('Testa se o app é redirecionado à página Not Found.', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/inexistente/');
+    const noFound = getByText(/Not Found/i);
+    expect(noFound).toBeInTheDocument();
+  });
 });
