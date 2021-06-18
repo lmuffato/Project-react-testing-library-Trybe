ddf044866e2204e1171e2ea16f917e45b85e8582
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-06-18 09:37:56 -0300

    Requisito 03: Adicionando testes.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('testa a pagina de pokemonsfavoritos.', () => {
+  it('testa se existem pokemons favoritos.', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    const notFound = getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
