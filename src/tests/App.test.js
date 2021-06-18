d2ddb4d4a031d54ae34d570a0cb714c657c1c14d
Arlesson Moura <arlessonmss@gmail.com>
2021-06-18 06:38:22 -0300

    Starting in desperate mode

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,24 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('testes do componente App.js', () => {
+  it('renders a reading with the text `Pokédex`', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(getByText('Encountered pokémons')).toBeInTheDocument();
+  });
 });
