6fb03bab278ce580c71d9469c5b848627dd952b9
Arlesson Moura <arlessonmss@gmail.com>
2021-06-18 13:54:24 -0300

    desperate delivery

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render, fireEvent, userEvent } from '@testing-library/react';
-import App from '../App';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../Render/renderWithRouter';
+import App from '../App';
 
-/// Com ajuda de colega
-
-describe('Requisito Pokedex', () => {
-  test('Teste se contem um h2', () => {
-    const { getByRole } = render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
-    );
-    const title = getByRole('heading', { level: 2, name: 'Encountered pokémons' });
-    expect(title).toBeInTheDocument();
+describe('Testes do componente pokedex.', () => {
+  it('Testa se existe uma tag h2 com texto `Encountered Pokémons`', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const heading = getAllByRole('heading', {
+      Name: /Encauntered pokémons/i,
+    });
+    expect(heading[1]).toHaveTextContent('Encountered pokémons');
   });
-  it('Ação botão proximo.', () => {
+
+  it('Testa se ao clicar no botão o proximo pokemon é exibido.', () => {
     const { getByTestId } = renderWithRouter(<App />);
-    const btnNext = getByTestId('next-pokemon');
-    fireEvent.click(btnNext);
-    expect(btnNext).toHaveTextContent('Próximo pokémon');
+    const buttonNextPokemon = getByTestId('next-pokemon');
+    userEvent.click(buttonNextPokemon);
+    expect(buttonNextPokemon).toHaveTextContent('Próximo pokémon');
   });
+
   it('Testa se há um botão que resete o tipo de pokemon.', () => {
     const { getByRole } = renderWithRouter(<App />);
     const resetButton = getByRole('button', {
       name: /All/i,
     });
-    fireEvent.click(resetButton);
+    userEvent.click(resetButton);
     expect(resetButton).toBeInTheDocument();
   });
+
   it('Testa se existe botões para cada tipo de pokemon.', () => {
     const { getAllByTestId } = renderWithRouter(<App />);
     const buttonForType = getAllByTestId('pokemon-type-button');
