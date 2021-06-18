cc2aab95b9886a51b016425aed6c8d9e9fa19622
Wendrick <wendrick.b.c1@gmail.com>
2021-06-18 01:11:32 -0300

    Req 6 with linter

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,51 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../render/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+import { Pokemon } from '../components';
+
+describe('Test component Pokemon', () => {
+  test('Tests if a card is rendered', () => {
+    renderWithRouter(<App />);
+
+    pokemons.forEach(({ name, type, averageWeight:
+      { value, measurementUnit }, image }) => {
+      const pokemonName = screen.getByTestId('pokemon-name');
+      const pokemonType = screen.getByTestId('pokemon-type');
+      const pokemonWeight = screen.getByTestId('pokemon-weight');
+      const pokemonImage = screen.getByRole('img');
+      const moreInfoLink = screen.getByRole('link', { name: /More details/i });
+
+      expect(pokemonName).toHaveTextContent(name);
+      expect(pokemonType).toHaveTextContent(type);
+      expect(pokemonWeight).toHaveTextContent(
+        `Average weight: ${value} ${measurementUnit}`,
+      );
+      expect(pokemonImage).toHaveAttribute('src', image);
+      expect(pokemonImage).toHaveAttribute('alt', `${name} sprite`);
+      expect(moreInfoLink).toBeInTheDocument();
+
+      const nextButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+      userEvent.click(nextButton);
+    });
+  });
+
+  test('If card contains a "more information" Link', () => {
+    const { history } = renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
+    const { id } = pokemons[0];
+    const infoLink = screen.getByRole('link', { name: /More details/i });
+    expect(infoLink).toBeInTheDocument();
+    userEvent.click(infoLink);
+    const { pathname } = history.location;
+    expect(pathname).toBe(`/pokemons/${id}`);
+  });
+
+  test('If has a favorite icon', () => {
+    renderWithRouter(<Pokemon pokemon={ pokemons[0] } isFavorite />);
+    const { name } = pokemons[0];
+    const favoriteIcon = screen.getByAltText(`${name} is marked as favorite`);
+    expect(favoriteIcon).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
