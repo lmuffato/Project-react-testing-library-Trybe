e1ed98ed68c74775a53a971cb00fcdb8b09feac9
johnpligeiro <johnpligeiro@gmail.com>
2021-06-18 13:07:26 -0300

    About fix

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,43 @@
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
-import { render } from '@testing-library/react';
 import About from '../components/About';
+import App from '../App';
 
-test('testando', () => {
-  const { getByText } = render(<About />);
-  const tit = getByText('About Pokédex');
+const { MemoryRouter } = require('react-router');
 
-  expect(tit).toBeInTheDocument();
-});
+describe('testa pagina sobre a pokedex', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
 
-test('Verificando src img', () => {
-  const { getByAltText } = render(<About />);
-  const img = getByAltText('Pokédex');
-  const pathImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
-  expect(img).toHaveAttribute('src', pathImg);
+    const about = getByRole('link', { name: /about/i });
+    fireEvent.click(about);
+
+    const title = getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const paragraph = container.getElementsByTagName('p');
+    expect(paragraph[0]).toBeInTheDocument();
+    expect(paragraph[1]).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém imagem do link,com uma Pokédex', () => {
+    const { getByAltText } = render(
+      <MemoryRouter>
+        <About />
+      </MemoryRouter>,
+    );
+    const img = getByAltText('Pokédex');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
