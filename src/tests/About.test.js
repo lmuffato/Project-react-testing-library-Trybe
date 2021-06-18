bcc62c00869dc8259f1a65484a5b35c4885d468c
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-06-18 09:13:43 -0300

    Requisito 02: adicionando testes de H2 e imagem

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+describe('Testes do componente About.', () => {
+  it('Teste se a página contém um heading h2 com o texto `About Pokédex`', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const heading = getByRole('heading', {
+      name: /about pokédex/i,
+      level: 2,
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém link de imagem de uma Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const imagePokedex = getByRole('img');
+    expect(imagePokedex).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
