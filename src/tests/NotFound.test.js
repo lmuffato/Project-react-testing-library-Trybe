c9d777b47269c903fb94c05ccd027a522d736889
Wendrick <wendrick.b.c1@gmail.com>
2021-06-18 00:59:31 -0300

    Req 5

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import renderWithRouter from '../render/renderWithRouter';
-
 import NotFound from '../components/NotFound';
 
 describe('Test page notFound', () => {
