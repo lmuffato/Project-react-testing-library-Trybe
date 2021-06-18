773f5a4060a67114ac5204fa24114d7e0380b391
johnpligeiro <johnpligeiro@gmail.com>
2021-06-18 12:58:34 -0300

    feat favoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Verifica mensagem se No favorite pokemon found é exibido', () => {
+  const { getByText } = render(<FavoritePokemons />);
+  const noPokemons = getByText('No favorite pokemon found');
+
+  expect(noPokemons).toBeInTheDocument();
+});
