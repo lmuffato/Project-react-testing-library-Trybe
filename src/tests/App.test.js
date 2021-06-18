923174847a5ff507836d1d6036349e020cb74b41
johnpligeiro <johnpligeiro@gmail.com>
2021-06-18 12:47:41 -0300

    App.test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -12,3 +12,28 @@ test('renders a reading with the text `Pokédex`', () => {
   const heading = getByText(/Pokédex/i);
   expect(heading).toBeInTheDocument();
 });
+
+test('shows the Pokédex when the route is `/`', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={ ['/'] }>
+      <App />
+    </MemoryRouter>,
+  );
+
+  expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+test('search buttons nav', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  const home = getByText('Home');
+  const about = getByText('About');
+  const favorites = getByText('Favorite Pokémons');
+
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(favorites).toBeInTheDocument();
+});
