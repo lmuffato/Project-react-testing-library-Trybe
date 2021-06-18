832f46893a7434f5351bc99c4a00094f3ec91cfa
johnpligeiro <johnpligeiro@gmail.com>
2021-06-18 13:00:16 -0300

    feat NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+test('Verifica texto h2', () => {
+  const { getByRole, getByText } = render(<NotFound />);
+  const h2 = getByRole('heading');
+  const textH2 = getByText('Page requested not found');
+
+  expect(h2).toBeInTheDocument();
+  expect(textH2).toBeInTheDocument();
+});
+
+test('Verificando src img', () => {
+  const { getByAltText } = render(<NotFound />);
+  const alt = 'Pikachu crying because the page requested was not found';
+  const img = getByAltText(alt);
+  const pathImg = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  expect(img).toHaveAttribute('src', pathImg);
+});
