c2f1ff617ae2eecccd602b578500ecfc0e57ac6e
johnpligeiro <johnpligeiro@gmail.com>
2021-06-18 12:50:21 -0300

    About.test

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import About from '../components/About';
+
+test('testando', () => {
+  const { getByText } = render(<About />);
+  const tit = getByText('About Pokédex');
+
+  expect(tit).toBeInTheDocument();
+});
+
+test('Verificando src img', () => {
+  const { getByAltText } = render(<About />);
+  const img = getByAltText('Pokédex');
+  const pathImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(img).toHaveAttribute('src', pathImg);
+});
