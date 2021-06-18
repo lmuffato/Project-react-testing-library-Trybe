612bd593d2a786d77a9c8c81388a3b0104b3b6b9
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-06-17 21:36:57 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createBrowserHistory } from 'history';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+// npx stryker run ./stryker/App.conf.json
+
+// test('renders a reading with the text `Pokédex`', () => {
+//     const heading = getByText(/Pokédex/);
+//     expect(heading).toBeInTheDocument();
+//   });
+describe('requisito 1', () => {
+  test('testando o conjunto dos links', () => {
+      render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
+      const homeLink = screen.getByRole('link', {
+        name: 'Home',
+      });
+      const aboutLink = screen.getByRole('link', {
+        name: 'About',
+      });
+      const favoriteLink = screen.getByRole('link', {
+        name: 'Favorite Pokémons',
+      });
+    
+      expect(homeLink).toBeInTheDocument();
+      expect(aboutLink).toBeInTheDocument();
+      expect(favoriteLink).toBeInTheDocument();
+    });
+    
+    test('testando o redirecionamento dos links', () => {
+      const historyMock = createBrowserHistory();
+      render(
+        <Router history={ historyMock }>
+          <App />
+        </Router>,
+      );
+      const home = screen.getByRole('link', {
+        name: 'Home',
+      });
+      const about = screen.getByRole('link', {
+        name: 'About',
+      });
+      const favorito = screen.getByRole('link', {
+        name: 'Favorite Pokémons',
+      });
+    
+      const funcLink = (link) => {
+        userEvent.click(link);
+        return historyMock.location.pathname;
+      };
+
+      const homePathname = funcLink(home);
+      expect(homePathname).toEqual('/');
+    
+      const aboutPathname = funcLink(about);
+      expect(aboutPathname).toEqual('/about');
+    
+      const favoritePathname = funcLink(favorito);
+      expect(favoritePathname).toEqual('/favorites');
+    
+    });
+})
